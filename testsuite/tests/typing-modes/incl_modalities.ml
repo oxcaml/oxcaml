294067ebf7fd3140c8c7bb7e120511bed75f2b34
Zesen Qian <github@riaqn.org>
2025-06-17 15:27:08 +0100

    don't know wtf is going on

--- testsuite/tests/typing-modes/incl_modalities.ml
+++ testsuite/tests/typing-modes/incl_modalities.ml
@@ -192,7 +192,7 @@ module M :
     module type Foo = sig val foo : 'a -> 'a end
     module type Foo' = Foo
     module type S = sig module N : Foo' end
-  end @@ portable
+  end @@ stateless
 module type S' = sig module N : M.Foo' @@ portable end
 |}]
 
