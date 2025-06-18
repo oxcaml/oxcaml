a488743837f5bf77c700e229c8b4e01bff2a0f70
Guillaume Bury <guillaume.bury@gmail.com>
2025-06-18 16:43:37 +0200

    better order of match

--- middle_end/flambda2/from_lambda/lambda_to_lambda_transforms.ml
+++ middle_end/flambda2/from_lambda/lambda_to_lambda_transforms.ml
@@ -54,40 +54,38 @@ let share_expr ~kind ~expr k =
 
 let switch_for_if_then_else ~loc ~cond ~ifso ~ifnot ~kind =
   let rec aux ~loc ~kind ~cond ~ifso ~ifnot =
-    match[@warning "-4"] ifso, ifnot with
-    | L.Lconst (Const_base (Const_int 1)), L.Lconst (Const_base (Const_int 0))
-      ->
-      cond
-    | L.Lconst (Const_base (Const_int 0)), L.Lconst (Const_base (Const_int 1))
-      ->
-      L.Lprim (Pnot, [cond], loc)
+    match[@warning "-4"] cond with
+    | L.Lconst (Const_base (Const_int 1)) -> ifso
+    | L.Lconst (Const_base (Const_int 0)) -> ifnot
+    (* CR gbury: should we try to use the locs here, or is it better to keep
+       using the locs from each individual condition ? *)
+    | L.Lprim (Psequand, [a; b], loc) ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifso ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lifthenelse (a, b, Lconst (Const_base (Const_int 0)), _) ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifso ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lprim (Psequor, [a; b], loc) ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifnot ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lifthenelse (a, Lconst (Const_base (Const_int 1)), b, _) ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifnot ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lprim (Pnot, [c], loc) -> aux ~loc ~kind ~cond:c ~ifso:ifnot ~ifnot:ifso
+    | L.Lifthenelse (cond, inner_ifso, inner_ifnot, _) ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:inner_ifso ~ifso ~ifnot)
+        (fun new_ifso ->
+          share_expr ~kind ~expr:(aux ~loc ~kind ~cond:inner_ifnot ~ifso ~ifnot)
+            (fun new_ifnot ->
+              aux ~loc ~kind ~cond ~ifso:new_ifso ~ifnot:new_ifnot))
     | _ -> (
-      match[@warning "-4"] cond with
-      | L.Lconst (Const_base (Const_int 1)) -> ifso
-      | L.Lconst (Const_base (Const_int 0)) -> ifnot
-      (* CR gbury: should we try to use the locs here, or is it better to keep
-         using the locs from each individual condition ? *)
-      | L.Lprim (Psequand, [a; b], loc) ->
-        share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot)
-          (fun ifso -> aux ~loc ~kind ~cond:a ~ifso ~ifnot)
-      | L.Lifthenelse (a, b, Lconst (Const_base (Const_int 0)), _) ->
-        share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot)
-          (fun ifso -> aux ~loc ~kind ~cond:a ~ifso ~ifnot)
-      | L.Lprim (Psequor, [a; b], loc) ->
-        share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot)
-          (fun ifnot -> aux ~loc ~kind ~cond:a ~ifso ~ifnot)
-      | L.Lifthenelse (a, Lconst (Const_base (Const_int 1)), b, _) ->
-        share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot)
-          (fun ifnot -> aux ~loc ~kind ~cond:a ~ifso ~ifnot)
-      | L.Lprim (Pnot, [c], loc) ->
-        aux ~loc ~kind ~cond:c ~ifso:ifnot ~ifnot:ifso
-      | L.Lifthenelse (cond, inner_ifso, inner_ifnot, _) ->
-        share_expr ~kind ~expr:(aux ~loc ~kind ~cond:inner_ifso ~ifso ~ifnot)
-          (fun new_ifso ->
-            share_expr ~kind
-              ~expr:(aux ~loc ~kind ~cond:inner_ifnot ~ifso ~ifnot)
-              (fun new_ifnot ->
-                aux ~loc ~kind ~cond ~ifso:new_ifso ~ifnot:new_ifnot))
+      match[@warning "-4"] ifso, ifnot with
+      | L.Lconst (Const_base (Const_int 1)), L.Lconst (Const_base (Const_int 0))
+        ->
+        cond
+      | L.Lconst (Const_base (Const_int 0)), L.Lconst (Const_base (Const_int 1))
+        ->
+        L.Lprim (Pnot, [cond], loc)
       | _ -> mk_switch ~cond ~ifso ~ifnot ~kind)
   in
   let res =
