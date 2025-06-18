4d25759d9e16d6b35aae6e165f5385d8d66a4d83
Guillaume Bury <guillaume.bury@gmail.com>
2025-06-18 15:51:47 +0200

    More optimizations

--- middle_end/flambda2/from_lambda/lambda_to_lambda_transforms.ml
+++ middle_end/flambda2/from_lambda/lambda_to_lambda_transforms.ml
@@ -49,34 +49,60 @@ let share_expr ~kind ~expr k =
     | _ -> false
   in
   match[@warning "-4"] (expr : L.lambda) with
+  | _ when is_simple_duplicable expr -> k expr
   | Lstaticraise (_, args) when List.for_all is_simple_duplicable args -> k expr
   | _ ->
     let cont = L.next_raise_count () in
     let jump = L.Lstaticraise (cont, []) in
     L.Lstaticcatch (k jump, (cont, []), expr, Same_region, kind)
 
-let optimize_boolean_sequop ~cond ~ifso ~ifnot ~kind =
-  let rec aux ~kind ~cond ~ifso ~ifnot =
+let switch_for_if_then_else ~loc ~cond ~ifso ~ifnot ~kind =
+  let rec aux ~loc ~kind ~cond ~ifso ~ifnot =
+    match[@warning "-4"] ifso, ifnot with
+    | L.Lconst (Const_base (Const_int 1)),
+      L.Lconst (Const_base (Const_int 0)) -> cond
+    | L.Lconst (Const_base (Const_int 0)),
+      L.Lconst (Const_base (Const_int 1)) ->
+        L.Lprim (Pnot, [cond], loc)
+    | _ -> begin
     match[@warning "-4"] cond with
+    | L.Lconst (Const_base (Const_int 1)) -> ifso
+    | L.Lconst (Const_base (Const_int 0)) -> ifnot
     (* CR gbury: should we try to use the locs here, or is it better to keep
        using the locs from each individual condition ? *)
-    | L.Lprim (Psequand, [a; b], _loc) ->
-      share_expr ~kind ~expr:(aux ~kind ~cond:b ~ifso ~ifnot) (fun ifso ->
-          aux ~kind ~cond:a ~ifso ~ifnot)
-    | L.Lprim (Psequor, [a; b], _loc) ->
-      share_expr ~kind ~expr:(aux ~kind ~cond:b ~ifso ~ifnot) (fun ifnot ->
-          aux ~kind ~cond:a ~ifso ~ifnot)
-    | L.Lprim (Pnot, [c], _loc) -> aux ~kind ~cond:c ~ifso:ifnot ~ifnot:ifso
+    | L.Lprim (Psequand, [a; b], loc)
+      ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifso ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lifthenelse (a, b, Lconst (Const_base (Const_int 0)), _)
+      ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifso ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lprim (Psequor, [a; b], loc)
+      ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifnot ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lifthenelse (a, Lconst (Const_base (Const_int 1)), b, _)
+      ->
+      share_expr ~kind ~expr:(aux ~loc ~kind ~cond:b ~ifso ~ifnot) (fun ifnot ->
+          aux ~loc ~kind ~cond:a ~ifso ~ifnot)
+    | L.Lprim (Pnot, [c], loc) -> aux ~loc ~kind ~cond:c ~ifso:ifnot ~ifnot:ifso
+    | L.Lifthenelse (cond, inner_ifso, inner_ifnot, _) ->
+        share_expr ~kind ~expr:(aux ~loc ~kind ~cond:inner_ifso ~ifso ~ifnot) (fun new_ifso ->
+            share_expr ~kind ~expr:(aux ~loc ~kind ~cond:inner_ifnot ~ifso ~ifnot) (fun new_ifnot ->
+                aux ~loc ~kind ~cond ~ifso:new_ifso ~ifnot:new_ifnot))
     | _ -> mk_switch ~cond ~ifso ~ifnot ~kind
+  end
   in
-  share_expr ~kind ~expr:ifso (fun ifso ->
-      share_expr ~kind ~expr:ifnot (fun ifnot -> aux ~kind ~cond ~ifso ~ifnot))
-
-let switch_for_if_then_else ~cond ~ifso ~ifnot ~kind =
-  match[@warning "-4"] cond with
-  | L.Lprim ((Psequand | Psequor | Pnot), _, _) ->
-    optimize_boolean_sequop ~cond ~ifso ~ifnot ~kind
-  | _ -> mk_switch ~cond ~ifso ~ifnot ~kind
+  let res =
+    share_expr ~kind ~expr:ifso (fun ifso ->
+        share_expr ~kind ~expr:ifnot (fun ifnot -> aux ~loc ~kind ~cond ~ifso ~ifnot))
+      in
+  if Flambda_features.debug_flambda2 () then
+    Format.eprintf "SWITCH:@\n%a@\n->@\n%a@\n@."
+      Printlambda.lambda (L.Lifthenelse(cond,ifso,ifnot,kind))
+      Printlambda.lambda res;
+  res
 
 let rec_catch_for_while_loop env cond body =
   let cont = L.next_raise_count () in
@@ -668,12 +694,12 @@ let transform_primitive0 env (prim : L.primitive) args loc =
   | Psequor, [a; b] ->
     let const_true = L.Lconst (Const_base (Const_int 1)) in
     Transformed
-      (switch_for_if_then_else ~cond:a ~ifso:const_true ~ifnot:b
+      (switch_for_if_then_else ~loc ~cond:a ~ifso:const_true ~ifnot:b
          ~kind:Lambda.layout_int)
   | Psequand, [a; b] ->
     let const_false = L.Lconst (Const_base (Const_int 0)) in
     Transformed
-      (switch_for_if_then_else ~cond:a ~ifso:b ~ifnot:const_false
+      (switch_for_if_then_else ~loc ~cond:a ~ifso:b ~ifnot:const_false
          ~kind:Lambda.layout_int)
   | (Psequand | Psequor), _ ->
     Misc.fatal_error "Psequand / Psequor must have exactly two arguments"
