3c1ce5819585a72869f75c3e4dcc32fff32ae633
TheNumbat <max@thenumb.at>
2025-06-25 23:56:17 -0400

    edit

--- oxcaml/tests/simd/ops_int8x32.ml
+++ oxcaml/tests/simd/ops_int8x32.ml
@@ -1,18 +1,4 @@
-(* open Utils *)
-
-let eq lv hv l h =
-  if l <> lv
-  then Printf.printf "low: actual = 0x%016Lx <> 0x%016Lx = expected\n" lv l;
-  if h <> hv
-  then Printf.printf "high: actual = 0x%016Lx <> 0x%016Lx = expected\n" hv h;
-  if l <> lv || h <> hv then assert false
-
-let eqi lv hv l h =
-  if l <> lv
-  then Printf.printf "low:  expected = %016x <> %016x = actual\n" lv l;
-  if h <> hv
-  then Printf.printf "high: expected = %016x <> %016x = actual\n" hv h;
-  if l <> lv || h <> hv then assert false
+open Utils
 
 type t = int8x32
 
