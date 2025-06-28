28d4f688c071c17352c4facee6c2d088cdfe352c
TheNumbat <max@thenumb.at>
2025-06-27 22:00:59 -0400

    tests

--- testsuite/tests/lib-domain/cpu_relax_domain.ml
+++ testsuite/tests/lib-domain/cpu_relax_domain.ml
@@ -1,11 +1,17 @@
 (* TEST
+   flags += "-alert -do_not_spawn_domains -alert -unsafe_multidomain";
    include systhreads;
    hassysthreads;
+   multidomain;
    { bytecode; }
    { native; }
 *)
 
 let () =
+  (* create tick thread on initial domain *)
+  Thread.create (fun () -> ()) () |> Thread.join;
+  (* rely on ticks in another domain *)
+  Domain.spawn (fun () ->
    let flag = Atomic.make false in
    let thread = Thread.create (fun () ->
       Atomic.set flag true;
@@ -17,4 +23,5 @@ let () =
      Domain.cpu_relax ();
    done;
    Atomic.set flag false;
-   Thread.join thread
+   Thread.join thread)
+  |> Domain.join
