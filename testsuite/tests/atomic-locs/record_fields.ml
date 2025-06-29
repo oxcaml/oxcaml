d23bb4b2ce94232f5febff3c34c4f0cd228761a5
Aspen Smith <aspsmith@janestreet.com>
2025-06-29 13:12:15 -0400

    Add atomic prims that take a loc

--- testsuite/tests/atomic-locs/record_fields.ml
+++ testsuite/tests/atomic-locs/record_fields.ml
@@ -3,6 +3,94 @@
    expect;
 *)
 
+
+module Atomic = struct
+  module Loc = struct
+    type ('a : value_or_null) t = 'a atomic_loc
+
+    external compare_and_set
+      : ('a : value_or_null mod portable).
+         'a atomic_loc @ contended local
+      -> 'a
+      -> 'a
+      -> bool @@ portable
+      = "%atomic_cas_loc"
+  end
+end
+[%%expect{|
+(apply (field_imm 1 (global Toploop!)) "Atomic/286"
+  (let (Loc = (makeblock 0)) (makeblock 0 Loc)))
+module Atomic :
+  sig
+    module Loc :
+      sig
+        type 'a t = 'a atomic_loc
+        external compare_and_set :
+          ('a : value_or_null mod portable).
+            'a atomic_loc @ local contended -> 'a -> 'a -> bool
+          @@ portable = "%atomic_cas_loc"
+      end
+  end
+|}]
+
+(* Basic usage: redefine atomics. *)
+
+module Basic = struct
+  type 'a atomic = { mutable filler: unit; mutable x : 'a [@atomic] }
+
+  let get (type a) (r : a atomic) : a = r.x
+
+  let get_imm (r : int atomic) : int = r.x
+
+  let set (type a) (r : a atomic) (v : a) : unit = r.x <- v
+
+  let set_imm (r : int atomic) (v : int) : unit = r.x <- v
+
+  let cas (type a : value mod portable) (r : a atomic) oldv newv =
+    Atomic.Loc.compare_and_set [%atomic.loc r.x] oldv newv
+
+  let[@inline never] get_loc (type a) (r : a atomic) : a Atomic.Loc.t =
+    [%atomic.loc r.x]
+
+  let slow_cas (type a : value mod portable) (r : a atomic) oldv newv =
+    Atomic.Loc.compare_and_set (get_loc r) oldv newv
+end
+[%%expect{|
+(apply (field_imm 1 (global Toploop!)) "Basic/326"
+  (let
+    (get = (function {nlocal = 0} r (atomic_load_field_ptr r 1))
+     get_imm = (function {nlocal = 0} r : int (atomic_load_field_imm r 1))
+     set = (function {nlocal = 0} r v : int (atomic_set_field_ptr r 1 v))
+     set_imm =
+       (function {nlocal = 0} r v[int] : int (atomic_set_field_imm r 1 v))
+     cas =
+       (function {nlocal = 0} r oldv newv : int
+         (atomic_compare_set_field_ptr r 1 oldv newv))
+     get_loc =
+       (function {nlocal = 0} r never_inline (makeblock 0 (*,int) r 1))
+     slow_cas =
+       (function {nlocal = 0} r oldv newv : int
+         (let (atomic_arg = (apply get_loc r))
+           (atomic_compare_set_field_ptr (field_imm 0 atomic_arg)
+             (field_int 1 atomic_arg) oldv newv))))
+    (makeblock 0 get get_imm set set_imm cas get_loc slow_cas)))
+module Basic :
+  sig
+    type 'a atomic = {
+      mutable filler : unit;
+      mutable(<non-legacy>) x : 'a [@atomic];
+    }
+    val get : 'a atomic -> 'a
+    val get_imm : int atomic -> int
+    val set : 'a atomic -> 'a @ portable -> unit
+    val set_imm : int atomic -> int -> unit
+    val cas : ('a : value mod portable). 'a atomic -> 'a -> 'a -> bool
+    val get_loc : 'a atomic -> 'a Atomic.Loc.t
+    val slow_cas : ('a : value mod portable). 'a atomic -> 'a -> 'a -> bool
+  end
+|}];;
+
+
 (* Atomic fields must be mutable. *)
 module Error1 = struct
   type t = { x : int [@atomic] }
@@ -12,6 +100,32 @@ Line 2, characters 13-30:
 2 |   type t = { x : int [@atomic] }
                  ^^^^^^^^^^^^^^^^^
 Error: The label "x" must be mutable to be declared atomic.
+|}];;
+
+
+(* [%atomic.loc _] payload must be a record field access *)
+module Error2 = struct
+  type t = { mutable x : int [@atomic] }
+  let f t = [%atomic.loc t]
+end
+[%%expect{|
+Line 3, characters 12-27:
+3 |   let f t = [%atomic.loc t]
+                ^^^^^^^^^^^^^^^
+Error: Invalid "[%atomic.loc]" payload, a record field access is expected.
+|}];;
+
+
+(* [%atomic.loc _] only works on atomic fields *)
+module Error3 = struct
+  type t = { x : int }
+  let f t = [%atomic.loc t.x]
+end
+[%%expect{|
+Line 3, characters 12-29:
+3 |   let f t = [%atomic.loc t.x]
+                ^^^^^^^^^^^^^^^^^
+Error: The record field "x" is not atomic
 |}];;
 
 
@@ -33,15 +147,15 @@ Error: Signature mismatch:
        Modules do not match:
          sig type t = { mutable x : int; } end
        is not included in
-         sig type t = { mutable x : int [@atomic]; } end
+         sig type t = { mutable(<non-legacy>) x : int [@atomic]; } end
        Type declarations do not match:
          type t = { mutable x : int; }
        is not included in
-         type t = { mutable x : int [@atomic]; }
+         type t = { mutable(<non-legacy>) x : int [@atomic]; }
        Fields do not match:
          "mutable x : int;"
        is not the same as:
-         "mutable x : int [@atomic];"
+         "mutable(<non-legacy>) x : int [@atomic];"
        The second is atomic and the first is not.
 |}];;
 
@@ -58,15 +172,15 @@ Lines 1-3, characters 17-3:
 3 | end......
 Error: Signature mismatch:
        Modules do not match:
-         sig type t = { mutable x : int [@atomic]; } end
+         sig type t = { mutable(<non-legacy>) x : int [@atomic]; } end
        is not included in
          sig type t = { mutable x : int; } end
        Type declarations do not match:
-         type t = { mutable x : int [@atomic]; }
+         type t = { mutable(<non-legacy>) x : int [@atomic]; }
        is not included in
          type t = { mutable x : int; }
        Fields do not match:
-         "mutable x : int [@atomic];"
+         "mutable(<non-legacy>) x : int [@atomic];"
        is not the same as:
          "mutable x : int;"
        The first is atomic and the second is not.
@@ -78,9 +192,11 @@ end : sig
   type t = { mutable x : int [@atomic] }
 end)
 [%%expect{|
-(apply (field_imm 1 (global Toploop!)) "Ok/304" (makeblock 0))
-module Ok : sig type t = { mutable x : int [@atomic]; } end
+(apply (field_imm 1 (global Toploop!)) "Ok/361" (makeblock 0))
+module Ok : sig type t = { mutable(<non-legacy>) x : int [@atomic]; } end
 |}];;
+
+
 
 (* Inline records are supported, including in extensions. *)
 
@@ -90,11 +206,16 @@ module Inline_record = struct
   let test : t -> int = fun (A r) -> r.x
 end
 [%%expect{|
-(apply (field_imm 1 (global Toploop!)) "Inline_record/312"
-  (let (test = (function {nlocal = 0} param : int (field_int 0 param)))
+(apply (field_imm 1 (global Toploop!)) "Inline_record/369"
+  (let
+    (test =
+       (function {nlocal = 0} param : int (atomic_load_field_imm param 0)))
     (makeblock 0 test)))
 module Inline_record :
-  sig type t = A of { mutable x : int [@atomic]; } val test : t -> int end
+  sig
+    type t = A of { mutable(<non-legacy>) x : int [@atomic]; }
+    val test : t -> int
+  end
 |}];;
 
 module Extension_with_inline_record = struct
@@ -109,16 +230,15 @@ module Extension_with_inline_record = struct
 
   let () = assert (test (A { x = 42 }) = 42)
 end
-
 [%%expect{|
-(apply (field_imm 1 (global Toploop!)) "Extension_with_inline_record/320"
+(apply (field_imm 1 (global Toploop!)) "Extension_with_inline_record/377"
   (let
     (A =
        (makeblock_unique 248 "Extension_with_inline_record.A"
          (caml_fresh_oo_id 0))
      test =
        (function {nlocal = 0} param : int
-         (if (== (field_imm 0 param) A) (field_int 1 param) 0))
+         (if (== (field_imm 0 param) A) (atomic_load_field_imm param 1) 0))
      *match* =[int]
        (if (== (apply test (makemutable 0 (*,int) A 42)) 42) 0
          (raise (makeblock 0 (getpredef Assert_failure!!) [0: "" 11 11]))))
@@ -126,7 +246,32 @@ end
 module Extension_with_inline_record :
   sig
     type t = ..
-    type t += A of { mutable x : int [@atomic]; }
+    type t += A of { mutable(<non-legacy>) x : int [@atomic]; }
     val test : t -> int
   end
-|}]
+|}];;
+
+
+(* Marking a field [@atomic] in a float-only record disables the unboxing optimization. *)
+module Float_records = struct
+  type t = { x : float; mutable y : float [@atomic] }
+
+  (* one should see in the -dlambda output below that this creates a block of tag 0. *)
+  let mk_t x y : t = { x; y }
+  let get v = v.y
+end
+(* CR aspsmith: this is wrong? *)
+[%%expect{|
+(apply (field_imm 1 (global Toploop!)) "Float_records/394"
+  (let
+    (mk_t =
+       (function {nlocal = 0} x[float] y[float] (makefloatblock Mutable x y))
+     get = (function {nlocal = 0} v : float (floatfield_mut 1 v)))
+    (makeblock 0 mk_t get)))
+module Float_records :
+  sig
+    type t = { x : float; mutable(<non-legacy>) y : float [@atomic]; }
+    val mk_t : float -> float -> t
+    val get : t -> float
+  end
+|}];;
