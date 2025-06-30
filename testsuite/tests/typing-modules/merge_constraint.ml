c6ae1ea05fc49191bba1c3335dbe47310e5e90cd
Ryan Tjoa <rtjoa@janestreet.com>
2025-06-30 13:54:54 -0400

    Add regression test

--- testsuite/tests/typing-modules/merge_constraint.ml
+++ testsuite/tests/typing-modules/merge_constraint.ml
@@ -617,3 +617,20 @@ Lines 4-8, characters 18-26:
 Error: In this "with" constraint, replacing "X0" by "F(X)" would
        introduce an invalid alias at "X1"
 |}]
+
+(* #14117 *)
+module type S = sig
+  type ('a, 'b) t constraint 'a = 'l * 'r
+end
+type 'a t2 constraint 'a = 'l * 'r
+module type S2 = S with type ('a, _) t = 'a t2
+[%%expect{|
+module type S = sig type ('a, 'b) t constraint 'a = 'l * 'r end
+type 'a t2 constraint 'a = 'l * 'r
+Line 7, characters 17-46:
+7 | module type S2 = S with type ('a, _) t = 'a t2
+                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: Destructive substitutions are not supported for constrained
+       types (other than when replacing a type constructor with
+       a type constructor with the same arguments).
+|}]
