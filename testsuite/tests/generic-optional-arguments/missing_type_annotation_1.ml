d2bc2ca5dca2bf6d397bb871fb74c31fcc3125ea
zchenb <121908802+zchenb@users.noreply.github.com>
2025-07-07 11:10:59 -0400

    add test cases

---
+++ testsuite/tests/generic-optional-arguments/missing_type_annotation_1.ml
@@ -0,0 +1,11 @@
+(* Interface *)
+val concat : ?'sep:string -> string list -> string
+
+(* Implementation *)
+let concat ?'(sep  <- " ") xs =
+  String.concat sep xs
+
+(* Usage *)
+let default_concat ys = concat ys
+let comma_concat zs = concat ~sep:" " zs
+let chain_call ?'(sep : string option) arg = concat ?'sep arg
---
+++ testsuite/tests/generic-optional-arguments/missing_type_annotation_2.ml
@@ -0,0 +1,11 @@
+(* Interface *)
+val concat : ?'sep:string -> string list -> string
+
+(* Implementation *)
+let concat ?'(sep : string option <- " ") xs =
+  String.concat sep xs
+
+(* Usage *)
+let default_concat ys = concat ys
+let comma_concat zs = concat ~sep:" " zs
+let chain_call ?'(sep ) arg = concat ?'sep arg
