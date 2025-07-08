aaa97befbd9f580f5fb4c215ce94b34173ee4125
zchenb <121908802+zchenb@users.noreply.github.com>
2025-07-08 10:46:23 -0400

    annotate future failures

--- testsuite/tests/generic-optional-arguments/missing_type_annotation_1.ml
+++ testsuite/tests/generic-optional-arguments/missing_type_annotation_1.ml
@@ -1,13 +1,25 @@
-(* TEST *)
+(* TEST
+ flags = "-extension-universe alpha";
+
+ setup-ocamlc.byte-build-env;
+ (* This test should fail after type checking is implemented. *)
+ (*= ocamlc_byte_exit_status = "2"; *)
+ ocamlc.byte;
+ check-ocamlc.byte-output;
+*)
 
 (* Interface *)
-val concat : ?'sep:string -> string list -> string
+module type S = sig
+  val concat : ?'sep:string option -> string list -> string
+end
 
 (* Implementation *)
+module M : S = struct
 let concat ?'(sep  <- " ") xs =
   String.concat sep xs
+end
 
 (* Usage *)
-let default_concat ys = concat ys
-let comma_concat zs = concat ~sep:" " zs
-let chain_call ?'(sep : string option) arg = concat ?'sep arg
+let default_concat ys = M.concat ys
+let comma_concat zs = M.concat ~sep:(Some " ") zs
+let chain_call ?'(sep : string option) arg = M.concat ?'sep arg
--- testsuite/tests/generic-optional-arguments/missing_type_annotation_2.ml
+++ testsuite/tests/generic-optional-arguments/missing_type_annotation_2.ml
@@ -1,13 +1,25 @@
-(* TEST *)
+(* TEST
+ flags = "-extension-universe alpha";
+
+ setup-ocamlc.byte-build-env;
+ (* This test should fail after type checking is implemented. *)
+ (*= ocamlc_byte_exit_status = "2"; *)
+ ocamlc.byte;
+ check-ocamlc.byte-output;
+*)
 
 (* Interface *)
-val concat : ?'sep:string -> string list -> string
+module type S = sig
+  val concat : ?'sep:string option -> string list -> string
+end
 
 (* Implementation *)
-let concat ?'(sep : string option <- " ") xs =
+module M : S = struct
+let concat ?'(sep : string <- " ") xs =
   String.concat sep xs
+end
 
 (* Usage *)
-let default_concat ys = concat ys
-let comma_concat zs = concat ~sep:" " zs
-let chain_call ?'(sep ) arg = concat ?'sep arg
+let default_concat ys = M.concat ys
+let comma_concat zs = M.concat ~sep:(Some " ") zs
+let chain_call ?'(sep) arg = M.concat ?'sep arg
