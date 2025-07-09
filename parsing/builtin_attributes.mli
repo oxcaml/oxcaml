6336b712794860c94c2e9d20baa64bb394f8efa6
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-07-09 15:54:40 -0400

    variable jkind defaulting

--- parsing/builtin_attributes.mli
+++ parsing/builtin_attributes.mli
@@ -242,6 +242,17 @@ val jkind : Parsetree.attributes -> jkind_attribute Location.loc option
     ignored. **)
 val error_message_attr : Parsetree.attributes -> string option
 
+(** If the argument is a "variable" attribute, marks it as used,
+    and returns a list of variable name and jkind annotation pairs.
+    Returns an empty list otherwise.
+
+    Expected syntax:
+    [@@@variable: ('var : jkind)]
+    [@@@variable: ('var1 : jkind1) * ('var2 : jkind2) ...]
+*)
+val get_variable_attr :
+  Parsetree.attribute -> (string * Parsetree.jkind_annotation) list
+
 (** [get_int_payload] is a helper for working with attribute payloads.
     Given a payload that consist of a structure containing exactly
     {[
