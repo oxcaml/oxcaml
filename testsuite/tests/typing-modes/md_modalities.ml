e0b58418fea21c581d56a0540ef2be0c107cc14d
Zesen Qian <github@riaqn.org>
2025-07-09 12:11:09 +0100

    improve inclusion error message for module modes

--- testsuite/tests/typing-modes/md_modalities.ml
+++ testsuite/tests/typing-modes/md_modalities.ml
@@ -137,9 +137,9 @@ Error: Signature mismatch:
          S
        In module "M'":
        Modules do not match:
-         sig val foo : 'a -> 'a end (* in a structure at nonportable *)
+         sig val foo : 'a -> 'a end (* at nonportable *)
        is not included in
-         sig val foo : 'a -> 'a @@ portable end (* in a structure at nonportable *)
+         sig val foo : 'a -> 'a @@ portable end (* at nonportable *)
        In module "M'":
        Values do not match:
          val foo : 'a -> 'a (* in a structure at nonportable *)
