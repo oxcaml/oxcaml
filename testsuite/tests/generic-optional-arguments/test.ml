a865d4ba6274918a127c25fd7c4cda0666a65bc1
zchenb <121908802+zchenb@users.noreply.github.com>
2025-07-10 18:33:32 -0400

    save

--- testsuite/tests/generic-optional-arguments/test.ml
+++ testsuite/tests/generic-optional-arguments/test.ml
@@ -32,4 +32,7 @@ let () =
   print_endline (chain_call ~sep:"+" ["x"; "y"; "z"]);
   print_endline (chain_call ?sep:(Some "+") ["x"; "y"; "z"]);
   print_endline (chain_call_2 ?sep:(Some "+") ["x"; "y"; "z"]);
+  (* CR generic-optional : The following two don't work yet.*)
+  (*= print_endline (chain_call ~sep:("+" @ local) ["x"; "y"; "z"]);
+  print_endline (chain_call_2 ?sep:(Some "+" @ local) ["x"; "y"; "z"]); *)
   ()
