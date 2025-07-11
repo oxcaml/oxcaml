4074241ffbd0ec469a6eb226311b0efa9638dd91
Leo Lee <seojun.lee@oriel.ox.ac.uk>
2025-07-11 13:22:17 -0400

    address reviews

--- driver/jsmaindriver.ml
+++ driver/jsmaindriver.ml
@@ -17,6 +17,30 @@ module Options = Main_args.Make_jscomp_options (Main_args.Default.Jsmain)
 
 let main argv ppf =
   let program = "ocamlj" in
+  let columns =
+    match Sys.getenv "COLUMNS" with
+    | exception Not_found -> None
+    | columns -> ( try Some (int_of_string columns) with _ -> None)
+  in
+  (match columns with
+  | None -> ()
+  | Some columns ->
+    (* Avoid getting too close to the edge just in case we've mismeasured
+       the boxes for some reason. *)
+    let columns = columns - 5 in
+    let set_geometry ppf =
+      Format.pp_set_margin ppf columns;
+      (* Make sure the max indent is at least 3/4 of the total width. Without
+         this, output can be unreadable no matter how wide your screen is. Note
+         that [Format.pp_set_margin] already messes with the max indent
+         sometimes, so we want to check [Format.pp_get_max_indent] rather than
+         make assumptions. *)
+      let desired_max_indent = columns * 3 / 4 in
+      if Format.pp_get_max_indent ppf () < desired_max_indent
+      then Format.pp_set_max_indent ppf desired_max_indent
+    in
+    set_geometry Format.std_formatter;
+    set_geometry Format.err_formatter);
   Clflags.add_arguments __LOC__ Options.list;
   match
     Compenv.readenv ppf Before_args;
@@ -26,7 +50,7 @@ let main argv ppf =
     then Compenv.fatal "-plugin is only supported up to OCaml 4.08.0";
     try
       Compenv.process_deferred_actions
-        (ppf, Jscompile.implementation, Jscompile.interface, ".cmo", ".cma")
+        (ppf, Jscompile.implementation, Jscompile.interface, ".cmj", ".cmja")
     with Arg.Bad msg ->
       prerr_endline msg;
       Clflags.print_arguments program;
