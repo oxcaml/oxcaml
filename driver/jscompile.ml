ff62908daaf04a916d4e8ce1da1d80f7880da716
Leo Lee <seojun.lee@oriel.ox.ac.uk>
2025-07-11 10:15:31 -0400

    ocamlformat

--- driver/jscompile.ml
+++ driver/jscompile.ml
@@ -71,7 +71,6 @@ let to_jsir i Typedtree.{ structure; coercion; argument_interface; _ } =
   in
   (structure, coercion, argument_coercion)
   |> Profile.(record transl)
-       (Translmod.transl_implementation i.module_name ~style:Plain_block)
   |> raw_lambda_to_jsir i
 
 type starting_point =
