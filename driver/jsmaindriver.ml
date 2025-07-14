5f64071e5005c54598944855b6789dc99271d878
Leo Lee <seojun.lee@oriel.ox.ac.uk>
2025-07-14 10:19:52 -0400

    revert this_is_ocamlj flag

--- driver/jsmaindriver.ml
+++ driver/jsmaindriver.ml
@@ -16,7 +16,6 @@
 module Options = Main_args.Make_jscomp_options (Main_args.Default.Jsmain)
 
 let main argv ppf =
-  Symbol.this_is_ocamlj ();
   let program = "ocamlj" in
   let columns =
     match Sys.getenv "COLUMNS" with
