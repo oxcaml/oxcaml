b000232f88f4608a0d9707060e8d276dce0aeb9d
TheNumbat <max@thenumb.at>
2025-07-19 12:44:38 -0400

    arm

---
+++ oxcaml/tests/simd/amd64/utils256.ml
@@ -0,0 +1,438 @@
+[@@@ocaml.warning "-unused-module"]
+
+include Utils
+
+external int64x4_of_int64s : int64 -> int64 -> int64 -> int64 -> int64x4
+  = "" "vec256_of_int64s"
+  [@@noalloc] [@@unboxed]
+
+external int32x8_of_int64s : int64 -> int64 -> int64 -> int64 -> int32x8
+  = "" "vec256_of_int64s"
+  [@@noalloc] [@@unboxed]
+
+external int16x16_of_int64s : int64 -> int64 -> int64 -> int64 -> int16x16
+  = "" "vec256_of_int64s"
+  [@@noalloc] [@@unboxed]
+
+external int8x32_of_int64s : int64 -> int64 -> int64 -> int64 -> int8x32
+  = "" "vec256_of_int64s"
+  [@@noalloc] [@@unboxed]
+
+external float32x8_of_int64s : int64 -> int64 -> int64 -> int64 -> float32x8
+  = "" "vec256_of_int64s"
+  [@@noalloc] [@@unboxed]
+
+external float64x4_of_int64s : int64 -> int64 -> int64 -> int64 -> float64x4
+  = "" "vec256_of_int64s"
+  [@@noalloc] [@@unboxed]
+
+external int64x4_first_int64 : int64x4 -> int64 = "" "vec256_first_int64"
+  [@@noalloc] [@@unboxed]
+
+external int64x4_second_int64 : int64x4 -> int64 = "" "vec256_second_int64"
+  [@@noalloc] [@@unboxed]
+
+external int64x4_third_int64 : int64x4 -> int64 = "" "vec256_third_int64"
+  [@@noalloc] [@@unboxed]
+
+external int64x4_fourth_int64 : int64x4 -> int64 = "" "vec256_fourth_int64"
+  [@@noalloc] [@@unboxed]
+
+external int32x8_first_int64 : int32x8 -> int64 = "" "vec256_first_int64"
+  [@@noalloc] [@@unboxed]
+
+external int32x8_second_int64 : int32x8 -> int64 = "" "vec256_second_int64"
+  [@@noalloc] [@@unboxed]
+
+external int32x8_third_int64 : int32x8 -> int64 = "" "vec256_third_int64"
+  [@@noalloc] [@@unboxed]
+
+external int32x8_fourth_int64 : int32x8 -> int64 = "" "vec256_fourth_int64"
+  [@@noalloc] [@@unboxed]
+
+external float32x8_first_int64 : float32x8 -> int64 = "" "vec256_first_int64"
+  [@@noalloc] [@@unboxed]
+
+external float32x8_second_int64 : float32x8 -> int64 = "" "vec256_second_int64"
+  [@@noalloc] [@@unboxed]
+
+external float32x8_third_int64 : float32x8 -> int64 = "" "vec256_third_int64"
+  [@@noalloc] [@@unboxed]
+
+external float32x8_fourth_int64 : float32x8 -> int64 = "" "vec256_fourth_int64"
+  [@@noalloc] [@@unboxed]
+
+external float64x4_first_int64 : float64x4 -> int64 = "" "vec256_first_int64"
+  [@@noalloc] [@@unboxed]
+
+external float64x4_second_int64 : float64x4 -> int64 = "" "vec256_second_int64"
+  [@@noalloc] [@@unboxed]
+
+external float64x4_third_int64 : float64x4 -> int64 = "" "vec256_third_int64"
+  [@@noalloc] [@@unboxed]
+
+external float64x4_fourth_int64 : float64x4 -> int64 = "" "vec256_fourth_int64"
+  [@@noalloc] [@@unboxed]
+
+external int16x16_first_int64 : int16x16 -> int64 = "" "vec256_first_int64"
+  [@@noalloc] [@@unboxed]
+
+external int16x16_second_int64 : int16x16 -> int64 = "" "vec256_second_int64"
+  [@@noalloc] [@@unboxed]
+
+external int16x16_third_int64 : int16x16 -> int64 = "" "vec256_third_int64"
+  [@@noalloc] [@@unboxed]
+
+external int16x16_fourth_int64 : int16x16 -> int64 = "" "vec256_fourth_int64"
+  [@@noalloc] [@@unboxed]
+
+external int8x32_first_int64 : int8x32 -> int64 = "" "vec256_first_int64"
+  [@@noalloc] [@@unboxed]
+
+external int8x32_second_int64 : int8x32 -> int64 = "" "vec256_second_int64"
+  [@@noalloc] [@@unboxed]
+
+external int8x32_third_int64 : int8x32 -> int64 = "" "vec256_third_int64"
+  [@@noalloc] [@@unboxed]
+
+external int8x32_fourth_int64 : int8x32 -> int64 = "" "vec256_fourth_int64"
+  [@@noalloc] [@@unboxed]
+
+module Vector256_casts = struct
+  let eq a b c d e f g h =
+    if a <> e || b <> f || c <> g || d <> h
+    then Printf.printf "%Ld,%Ld,%Ld,%Ld <> %Ld,%Ld,%Ld,%Ld\n" a b c d e f g h
+
+  let int64x4_to_quadruple v =
+    ( int64x4_first_int64 v,
+      int64x4_second_int64 v,
+      int64x4_third_int64 v,
+      int64x4_fourth_int64 v )
+
+  external int64x4_of_int32x8 : int32x8 -> int64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int64x4_of_int16x16 : int16x16 -> int64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int64x4_of_int8x32 : int8x32 -> int64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int64x4_of_float32x8 : float32x8 -> int64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int64x4_of_float64x4 : float64x4 -> int64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  let () =
+    let _0 = int32x8_of_int64s 1L 2L 3L 4L in
+    let _1 = int16x16_of_int64s 5L 6L 7L 8L in
+    let _2 = int8x32_of_int64s 9L 10L 11L 12L in
+    let _3 = float32x8_of_int64s 13L 14L 15L 16L in
+    let _4 = float64x4_of_int64s 17L 18L 19L 20L in
+    let _0 = int64x4_of_int32x8 (Sys.opaque_identity _0) in
+    let _1 = int64x4_of_int16x16 (Sys.opaque_identity _1) in
+    let _2 = int64x4_of_int8x32 (Sys.opaque_identity _2) in
+    let _3 = int64x4_of_float32x8 (Sys.opaque_identity _3) in
+    let _4 = int64x4_of_float64x4 (Sys.opaque_identity _4) in
+    let a, b, c, d = int64x4_to_quadruple _0 in
+    eq a b c d 1L 2L 3L 4L;
+    let a, b, c, d = int64x4_to_quadruple _1 in
+    eq a b c d 5L 6L 7L 8L;
+    let a, b, c, d = int64x4_to_quadruple _2 in
+    eq a b c d 9L 10L 11L 12L;
+    let a, b, c, d = int64x4_to_quadruple _3 in
+    eq a b c d 13L 14L 15L 16L;
+    let a, b, c, d = int64x4_to_quadruple _4 in
+    eq a b c d 17L 18L 19L 20L
+
+  external int32x8_of_int64x4 : int64x4 -> int32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int32x8_of_int16x16 : int16x16 -> int32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int32x8_of_int8x32 : int8x32 -> int32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int32x8_of_float32x8 : float32x8 -> int32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int32x8_of_float64x4 : float64x4 -> int32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  let () =
+    let _0 = int64x4_of_int64s 1L 2L 3L 4L in
+    let _1 = int16x16_of_int64s 5L 6L 7L 8L in
+    let _2 = int8x32_of_int64s 9L 10L 11L 12L in
+    let _3 = float32x8_of_int64s 13L 14L 15L 16L in
+    let _4 = float64x4_of_int64s 17L 18L 19L 20L in
+    let _0 = int32x8_of_int64x4 (Sys.opaque_identity _0) in
+    let _1 = int32x8_of_int16x16 (Sys.opaque_identity _1) in
+    let _2 = int32x8_of_int8x32 (Sys.opaque_identity _2) in
+    let _3 = int32x8_of_float32x8 (Sys.opaque_identity _3) in
+    let _4 = int32x8_of_float64x4 (Sys.opaque_identity _4) in
+    let _0 = int64x4_of_int32x8 _0 in
+    let _1 = int64x4_of_int32x8 _1 in
+    let _2 = int64x4_of_int32x8 _2 in
+    let _3 = int64x4_of_int32x8 _3 in
+    let _4 = int64x4_of_int32x8 _4 in
+    let a, b, c, d = int64x4_to_quadruple _0 in
+    eq a b c d 1L 2L 3L 4L;
+    let a, b, c, d = int64x4_to_quadruple _1 in
+    eq a b c d 5L 6L 7L 8L;
+    let a, b, c, d = int64x4_to_quadruple _2 in
+    eq a b c d 9L 10L 11L 12L;
+    let a, b, c, d = int64x4_to_quadruple _3 in
+    eq a b c d 13L 14L 15L 16L;
+    let a, b, c, d = int64x4_to_quadruple _4 in
+    eq a b c d 17L 18L 19L 20L
+
+  external int16x16_of_int64x4 : int64x4 -> int16x16
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int16x16_of_int32x8 : int32x8 -> int16x16
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int16x16_of_int8x32 : int8x32 -> int16x16
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int16x16_of_float32x8 : float32x8 -> int16x16
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int16x16_of_float64x4 : float64x4 -> int16x16
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  let () =
+    let _0 = int64x4_of_int64s 1L 2L 3L 4L in
+    let _1 = int32x8_of_int64s 5L 6L 7L 8L in
+    let _2 = int8x32_of_int64s 9L 10L 11L 12L in
+    let _3 = float32x8_of_int64s 13L 14L 15L 16L in
+    let _4 = float64x4_of_int64s 17L 18L 19L 20L in
+    let _0 = int16x16_of_int64x4 (Sys.opaque_identity _0) in
+    let _1 = int16x16_of_int32x8 (Sys.opaque_identity _1) in
+    let _2 = int16x16_of_int8x32 (Sys.opaque_identity _2) in
+    let _3 = int16x16_of_float32x8 (Sys.opaque_identity _3) in
+    let _4 = int16x16_of_float64x4 (Sys.opaque_identity _4) in
+    let _0 = int64x4_of_int16x16 _0 in
+    let _1 = int64x4_of_int16x16 _1 in
+    let _2 = int64x4_of_int16x16 _2 in
+    let _3 = int64x4_of_int16x16 _3 in
+    let _4 = int64x4_of_int16x16 _4 in
+    let a, b, c, d = int64x4_to_quadruple _0 in
+    eq a b c d 1L 2L 3L 4L;
+    let a, b, c, d = int64x4_to_quadruple _1 in
+    eq a b c d 5L 6L 7L 8L;
+    let a, b, c, d = int64x4_to_quadruple _2 in
+    eq a b c d 9L 10L 11L 12L;
+    let a, b, c, d = int64x4_to_quadruple _3 in
+    eq a b c d 13L 14L 15L 16L;
+    let a, b, c, d = int64x4_to_quadruple _4 in
+    eq a b c d 17L 18L 19L 20L
+
+  external int8x32_of_int64x4 : int64x4 -> int8x32
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int8x32_of_int32x8 : int32x8 -> int8x32
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int8x32_of_int16x16 : int16x16 -> int8x32
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int8x32_of_float32x8 : float32x8 -> int8x32
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external int8x32_of_float64x4 : float64x4 -> int8x32
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  let () =
+    let _0 = int64x4_of_int64s 1L 2L 3L 4L in
+    let _1 = int32x8_of_int64s 5L 6L 7L 8L in
+    let _2 = int16x16_of_int64s 9L 10L 11L 12L in
+    let _3 = float32x8_of_int64s 13L 14L 15L 16L in
+    let _4 = float64x4_of_int64s 17L 18L 19L 20L in
+    let _0 = int8x32_of_int64x4 (Sys.opaque_identity _0) in
+    let _1 = int8x32_of_int32x8 (Sys.opaque_identity _1) in
+    let _2 = int8x32_of_int16x16 (Sys.opaque_identity _2) in
+    let _3 = int8x32_of_float32x8 (Sys.opaque_identity _3) in
+    let _4 = int8x32_of_float64x4 (Sys.opaque_identity _4) in
+    let _0 = int64x4_of_int8x32 _0 in
+    let _1 = int64x4_of_int8x32 _1 in
+    let _2 = int64x4_of_int8x32 _2 in
+    let _3 = int64x4_of_int8x32 _3 in
+    let _4 = int64x4_of_int8x32 _4 in
+    let a, b, c, d = int64x4_to_quadruple _0 in
+    eq a b c d 1L 2L 3L 4L;
+    let a, b, c, d = int64x4_to_quadruple _1 in
+    eq a b c d 5L 6L 7L 8L;
+    let a, b, c, d = int64x4_to_quadruple _2 in
+    eq a b c d 9L 10L 11L 12L;
+    let a, b, c, d = int64x4_to_quadruple _3 in
+    eq a b c d 13L 14L 15L 16L;
+    let a, b, c, d = int64x4_to_quadruple _4 in
+    eq a b c d 17L 18L 19L 20L
+
+  external float32x8_of_int64x4 : int64x4 -> float32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float32x8_of_int32x8 : int32x8 -> float32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float32x8_of_int16x16 : int16x16 -> float32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float32x8_of_int8x32 : int8x32 -> float32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float32x8_of_float64x4 : float64x4 -> float32x8
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  let () =
+    let _0 = int64x4_of_int64s 1L 2L 3L 4L in
+    let _1 = int32x8_of_int64s 5L 6L 7L 8L in
+    let _2 = int16x16_of_int64s 9L 10L 11L 12L in
+    let _3 = int8x32_of_int64s 13L 14L 15L 16L in
+    let _4 = float64x4_of_int64s 17L 18L 19L 20L in
+    let _0 = float32x8_of_int64x4 (Sys.opaque_identity _0) in
+    let _1 = float32x8_of_int32x8 (Sys.opaque_identity _1) in
+    let _2 = float32x8_of_int16x16 (Sys.opaque_identity _2) in
+    let _3 = float32x8_of_int8x32 (Sys.opaque_identity _3) in
+    let _4 = float32x8_of_float64x4 (Sys.opaque_identity _4) in
+    let _0 = int64x4_of_float32x8 _0 in
+    let _1 = int64x4_of_float32x8 _1 in
+    let _2 = int64x4_of_float32x8 _2 in
+    let _3 = int64x4_of_float32x8 _3 in
+    let _4 = int64x4_of_float32x8 _4 in
+    let a, b, c, d = int64x4_to_quadruple _0 in
+    eq a b c d 1L 2L 3L 4L;
+    let a, b, c, d = int64x4_to_quadruple _1 in
+    eq a b c d 5L 6L 7L 8L;
+    let a, b, c, d = int64x4_to_quadruple _2 in
+    eq a b c d 9L 10L 11L 12L;
+    let a, b, c, d = int64x4_to_quadruple _3 in
+    eq a b c d 13L 14L 15L 16L;
+    let a, b, c, d = int64x4_to_quadruple _4 in
+    eq a b c d 17L 18L 19L 20L
+
+  external float64x4_of_int64x4 : int64x4 -> float64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float64x4_of_int32x8 : int32x8 -> float64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float64x4_of_int16x16 : int16x16 -> float64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float64x4_of_int8x32 : int8x32 -> float64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external float64x4_of_float32x8 : float32x8 -> float64x4
+    = "caml_vec256_unreachable" "caml_vec256_cast"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  let () =
+    let _0 = int64x4_of_int64s 1L 2L 3L 4L in
+    let _1 = int32x8_of_int64s 5L 6L 7L 8L in
+    let _2 = int16x16_of_int64s 9L 10L 11L 12L in
+    let _3 = int8x32_of_int64s 13L 14L 15L 16L in
+    let _4 = float32x8_of_int64s 17L 18L 19L 20L in
+    let _0 = float64x4_of_int64x4 (Sys.opaque_identity _0) in
+    let _1 = float64x4_of_int32x8 (Sys.opaque_identity _1) in
+    let _2 = float64x4_of_int16x16 (Sys.opaque_identity _2) in
+    let _3 = float64x4_of_int8x32 (Sys.opaque_identity _3) in
+    let _4 = float64x4_of_float32x8 (Sys.opaque_identity _4) in
+    let _0 = int64x4_of_float64x4 _0 in
+    let _1 = int64x4_of_float64x4 _1 in
+    let _2 = int64x4_of_float64x4 _2 in
+    let _3 = int64x4_of_float64x4 _3 in
+    let _4 = int64x4_of_float64x4 _4 in
+    let a, b, c, d = int64x4_to_quadruple _0 in
+    eq a b c d 1L 2L 3L 4L;
+    let a, b, c, d = int64x4_to_quadruple _1 in
+    eq a b c d 5L 6L 7L 8L;
+    let a, b, c, d = int64x4_to_quadruple _2 in
+    eq a b c d 9L 10L 11L 12L;
+    let a, b, c, d = int64x4_to_quadruple _3 in
+    eq a b c d 13L 14L 15L 16L;
+    let a, b, c, d = int64x4_to_quadruple _4 in
+    eq a b c d 17L 18L 19L 20L
+end
+
+module Int8 = struct
+  include Int8
+
+  let to_int8x32 a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 b0 b1 b2
+      b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 =
+    let i0 = pack8 a0 a1 a2 a3 a4 a5 a6 a7 in
+    let i1 = pack8 a8 a9 a10 a11 a12 a13 a14 a15 in
+    let i2 = pack8 b0 b1 b2 b3 b4 b5 b6 b7 in
+    let i3 = pack8 b8 b9 b10 b11 b12 b13 b14 b15 in
+    int8x32_of_int64s i0 i1 i2 i3
+end
+
+module Int16 = struct
+  include Int16
+
+  let to_int16x16 a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 =
+    let i0 = pack4 a0 a1 a2 a3 in
+    let i1 = pack4 a4 a5 a6 a7 in
+    let i2 = pack4 a8 a9 a10 a11 in
+    let i3 = pack4 a12 a13 a14 a15 in
+    int16x16_of_int64s i0 i1 i2 i3
+end
+
+module Int32s = struct
+  include Int32s
+
+  let to_int32x8 a b c d e f g h =
+    let low = to_int32x4 a b c d in
+    let high = to_int32x4 e f g h in
+    int32x8_of_int32x4s low high
+end
+
+module Float32 = struct
+  include Float32
+
+  let to_float32x8 a b c d e f g h =
+    let low = to_float32x4 a b c d in
+    let high = to_float32x4 e f g h in
+    float32x8_of_float32x4s low high
+end
+
+module Float64 = struct
+  include Float64
+
+  let to_float64x4 a b c d =
+    let low = to_float64x2 a b in
+    let high = to_float64x2 c d in
+    float64x4_of_float64x2s low high
+end
