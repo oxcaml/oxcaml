eece70d6e7655fe78627d2e919f9ee341f534624
Joseph Cutler <jwc@seas.upenn.edu>
2025-07-27 11:55:47 +0100

    cleanup

--- backend/printoperation.ml
+++ backend/printoperation.ml
@@ -46,7 +46,7 @@ let operation ?(print_reg = Printreg.reg) (op : Operation.t) arg ppf res =
     fprintf ppf "alloc %i" n
   | Alloc { bytes = n; mode = Cmm.Alloc_mode.Local; dbginfo = _ } ->
     fprintf ppf "alloc_local %i" n
-  | Alloc { bytes = n; mode = Cmm.Alloc_mode.External; dbginfo = _ } ->
+  | Alloc { mode = Cmm.Alloc_mode.External; _ } ->
     Misc.fatal_error "External allocations are generated as Cextcall"
   | Intop op ->
     if Operation.is_unary_integer_operation op
