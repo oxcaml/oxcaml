aabf8567809194f4a281ed55c2e9acd35094549b
Vincent Laviron <vincent.laviron@gmail.com>
2025-07-29 15:59:56 +0200

    Formatting again

--- backend/cmm_peephole_engine.mli
+++ backend/cmm_peephole_engine.mli
@@ -1,7 +1,7 @@
 type _ pattern_kind =
-| Expr : Cmm.expression pattern_kind
-| Int : int pattern_kind
-| Natint : Nativeint.t pattern_kind
+  | Expr : Cmm.expression pattern_kind
+  | Int : int pattern_kind
+  | Natint : Nativeint.t pattern_kind
 
 type 'a pattern_var
 
@@ -9,10 +9,15 @@ val create_var : 'a pattern_kind -> string -> 'a pattern_var
 
 module Default_variables : sig
   val c : Cmm.expression pattern_var
+
   val c1 : Cmm.expression pattern_var
+
   val c2 : Cmm.expression pattern_var
+
   val n : int pattern_var
+
   val n1 : int pattern_var
+
   val n2 : int pattern_var
 end
 
@@ -45,9 +50,11 @@ type 'a clause
 
 val run : Cmm.expression -> Cmm.expression clause list -> Cmm.expression
 
-val run_default : default:(Cmm.expression -> 'a) -> Cmm.expression -> 'a clause list -> 'a
+val run_default :
+  default:(Cmm.expression -> 'a) -> Cmm.expression -> 'a clause list -> 'a
 
 module Syntax : sig
-  val (=>) : cmm_pattern -> (Env.t -> 'a) -> 'a clause
-  val (#.) : Env.t -> 'a pattern_var -> 'a
+  val ( => ) : cmm_pattern -> (Env.t -> 'a) -> 'a clause
+
+  val ( #. ) : Env.t -> 'a pattern_var -> 'a
 end
