109ff5955ca0a08a51faa998c6a086af7283b226
Joseph Cutler <jwc@seas.upenn.edu>
2025-07-29 11:00:17 -0400

    sigh

--- testsuite/tests/external-allocation/stubs.c
+++ testsuite/tests/external-allocation/stubs.c
@@ -11,7 +11,7 @@ CAMLprim value print_block(uint64_t v, uint64_t fields, value name){
     CAMLparam1(name);
     printf("Test %s:\n", String_val(name));
     for(int i = 0; i < fields; i++){
-        printf("  Field %d: %llu\n",i,(uint64_t) Field(v,i));
+        printf("  Field %d: %lu\n",i,(uint64_t) Field(v,i));
     }
     printf("\n");
     fflush(stdout);
