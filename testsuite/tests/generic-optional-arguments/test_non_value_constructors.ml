6c9d62c39eceae57023fee8810ca03ca7888a430
Zhibo Chen <121908802+zchenb@users.noreply.github.com>
2025-08-06 16:45:11 -0400

    convert the non_value constructor test from expect to native

--- testsuite/tests/generic-optional-arguments/test_non_value_constructors.ml
+++ testsuite/tests/generic-optional-arguments/test_non_value_constructors.ml
@@ -1,37 +1,53 @@
 (* TEST
- flags = "-extension-universe alpha";
- expect;
+ include stdlib_upstream_compatible;
+ flags = "-extension-universe alpha -dlambda";
+ ocamlopt.opt;
+ check-ocamlopt.opt-output;
 *)
 
 type ('b : bits32) bits32_option =
   | None32
   | Some32 of 'b
 [@@option_like]
-[%%expect{|
-type ('b : bits32) bits32_option = None32 | Some32 of 'b
-|}]
 
-let f (?x : 'a bits32_option) () = x
-let v = f ()
-let v = f ~x:(#3l) ()
-let v = f ?x:(Some32 #3l) ()
-let v = f ?x:(None32) ()
-let g (?(x = #2l) : int32# bits32_option) () = x
-let v = Some32 (g())
-let v = Some32 (g ~x:#3l ())
-let v = Some32 (g ?x:(Some32 #3l) ())
-let v = Some32 (g ?x:(None32) ())
-[%%expect{|
-val f : ('a : bits32). (?x):'a bits32_option -> unit -> 'a bits32_option =
-  <fun>
-val v : ('a : bits32). 'a bits32_option = None32
-val v : int32# bits32_option = Some32 <abstr>
-val v : int32# bits32_option = Some32 <abstr>
-val v : ('a : bits32). 'a bits32_option = None32
-val g : (?x):int32# bits32_option -> unit -> int32# = <fun>
-val v : int32# bits32_option = Some32 <abstr>
-val v : int32# bits32_option = Some32 <abstr>
-val v : int32# bits32_option = Some32 <abstr>
-val v : int32# bits32_option = Some32 <abstr>
-|}]
-(* CR generic-optional: check matching.ml access patterns *)
+module Int32_u = Stdlib_upstream_compatible.Int32_u
+
+let print_int32u prefix x =
+  Printf.printf "%s: %ld\n" prefix (Int32_u.to_int32 x)
+
+let print_int32u_bits32_option prefix x =
+  match x with
+  | None32 -> Printf.printf "%s: None32\n" prefix
+  | Some32 x -> Printf.printf "%s: Some32 %ld\n" prefix (Int32_u.to_int32 x)
+
+let test() =
+  let f (?x : 'a bits32_option) () = x in
+  let v = f () in
+  let _ = print_int32u_bits32_option "v1=None" v in
+  let v = f ~x:(#3l) () in
+  let _ = print_int32u_bits32_option "v2=3l" v in
+  let v = f ?x:(Some32 #3l) () in
+  let _ = print_int32u_bits32_option "v3=3l" v in
+  let v = f ?x:(None32) () in
+  let _ = print_int32u_bits32_option "v4=None" v in
+  let g (?(x = #2l) : int32# bits32_option) () = x in
+  let v = g() in
+  let _ = print_int32u "v5=2l" v in
+  let v = g ~x:#3l () in
+  let _ = print_int32u "v6=3l" v in
+  let v = g ?x:(Some32 #3l) () in
+  let _ = print_int32u "v7=3l" v in
+  let v = g ?x:(None32) () in
+  let _ = print_int32u "v8=2l" v in
+  ()
+
+(* CR generic-optional: check matching.ml access patterns
+
+  Currently the code below triggers a Core Dump:
+
+  {v
+    let _ = test ()
+  v}
+
+  The reason is that we are using Pfield to access the element of an option,
+  but we should use Pmixedfield *)
