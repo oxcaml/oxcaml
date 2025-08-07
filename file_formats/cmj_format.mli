6e23f281c466ed240e00d5e00e688842b3bec1ea
Leo Lee <seojun.lee@oriel.ox.ac.uk>
2025-08-07 16:22:59 -0400

    get rid of even more fake inria notices

--- file_formats/cmj_format.mli
+++ file_formats/cmj_format.mli
@@ -1,18 +1,3 @@
-(**************************************************************************)
-(*                                                                        *)
-(*                                 OCaml                                  *)
-(*                                                                        *)
-(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
-(*                                                                        *)
-(*   Copyright 1999 Institut National de Recherche en Informatique et     *)
-(*     en Automatique.                                                    *)
-(*                                                                        *)
-(*   All rights reserved.  This file is distributed under the terms of    *)
-(*   the GNU Lesser General Public License version 2.1, with the          *)
-(*   special exception on linking described in the file LICENSE.          *)
-(*                                                                        *)
-(**************************************************************************)
-
 type compilation_unit_descr =
   { cu_name: Compilation_unit.t;
     cu_pos: int;
