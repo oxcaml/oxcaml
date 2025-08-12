de04369f3818652b4e1206d7935660b979067a4e
Zesen Qian <github@riaqn.org>
2025-08-12 16:02:43 +0100

    add tests about linearity

--- testsuite/tests/typing-unique/unique_analysis.ml
+++ testsuite/tests/typing-unique/unique_analysis.ml
@@ -870,3 +870,82 @@ let foo (r : 'a r) =
 [%%expect{|
 val foo : 'a r -> unit = <fun>
 |}]
+
+let foo (r : 'a r @ once) =
+let x = [%atomic.loc r.x] in
+  ignore_once x;
+  let _ = r.y in
+  ()
+[%%expect{|
+Line 4, characters 10-11:
+4 |   let _ = r.y in
+              ^
+Error: This value is read from here,
+       but it is defined as once and has already been used:
+Line 2, characters 21-22:
+2 | let x = [%atomic.loc r.x] in
+                         ^
+
+|}]
+
+let foo (r : 'a r @ once) =
+  let x = [%atomic.loc r.x] in
+  ignore_once x;
+  let _ = r.y in
+  ()
+[%%expect{|
+Line 4, characters 10-11:
+4 |   let _ = r.y in
+              ^
+Error: This value is read from here,
+       but it is defined as once and has already been used:
+Line 2, characters 23-24:
+2 |   let x = [%atomic.loc r.x] in
+                           ^
+
+|}]
+
+
+let foo (r : 'a r @ once) =
+  let _ = r.y in
+  let x = [%atomic.loc r.x] in
+  let _ = ignore_once x in
+  ()
+[%%expect{|
+val foo : 'a r @ once -> unit = <fun>
+|}]
+
+
+let foo (r : 'a r @ once) =
+  let x = [%atomic.loc r.x] in
+  let _ = r.y in
+  let _ = ignore_once x in
+  ()
+[%%expect{|
+Line 3, characters 10-11:
+3 |   let _ = r.y in
+              ^
+Error: This value is read from here,
+       but it is defined as once and has already been used:
+Line 2, characters 23-24:
+2 |   let x = [%atomic.loc r.x] in
+                           ^
+
+|}]
+
+let foo (r : 'a r @ once) =
+  let x = [%atomic.loc r.x] in
+  let _ = r.y in
+  let _ = ignore_once x in
+  ()
+[%%expect{|
+Line 3, characters 10-11:
+3 |   let _ = r.y in
+              ^
+Error: This value is read from here,
+       but it is defined as once and has already been used:
+Line 2, characters 23-24:
+2 |   let x = [%atomic.loc r.x] in
+                           ^
+
+|}]
