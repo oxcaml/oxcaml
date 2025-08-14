d235bacf2c5cdf2c0afe51fea8315293b85d71f2
James Rayman <james@jamesrayman.com>
2025-08-14 17:53:24 -0400

    WIP

--- testsuite/tests/typing-layouts-or-null/basics.ml
+++ testsuite/tests/typing-layouts-or-null/basics.ml
@@ -50,14 +50,13 @@ module type S1 = sig
 end
 
 [%%expect{|
-Line 2, characters 10-29:
-2 |   val x : t_any_mod_separable
-              ^^^^^^^^^^^^^^^^^^^
-Error: This type signature for "x" is not a value type.
-       The layout of type t_any_mod_separable is any
-         because of the definition of t_any_mod_separable at line 2, characters 0-44.
-       But the layout of type t_any_mod_separable must be a sublayout of
-           value
+Line 2, characters 10-24:
+2 |   val x : t_any_non_null
+              ^^^^^^^^^^^^^^
+Error: This type signature for "x" does not have a representable layout.
+       The layout of type t_any_non_null is any
+         because of the definition of t_any_non_null at line 2, characters 0-34.
+       But the layout of type t_any_non_null must be representable
          because it's the type of something stored in a module structure.
 |}]
 
