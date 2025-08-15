55434e19111ef012283185d5c14eb00376ec210c
Leo Lee <41759796+smjleo@users.noreply.github.com>
2025-08-15 09:36:09 -0400

    Change JSIR translation order for Let_cont (#4430)

--- middle_end/flambda2/to_jsir/to_jsir.ml
+++ middle_end/flambda2/to_jsir/to_jsir.ml
@@ -134,6 +134,15 @@ and let_cont ~env ~res (e : Flambda.Let_cont_expr.t) =
     in
     extra_args_boxed, res
   in
+  (* A note on evaluation order: let_cont k = [handler] in [body] should be more
+     interpreted like [body] where k = [handler], and control flow flows from
+     [body] to [handler]. While variables aren't captured in continuations and
+     must be passed explicitly, symbols defined in [body] are scoped in
+     [handler].
+
+     Hence, we translate [body] before [handler], but make sure that the name
+     [k] exists in the environment before translating [body] so that we can use
+     it. *)
   match e with
   | Non_recursive
       { handler; num_free_occurrences = _; is_applied_with_traps = _ } ->
@@ -142,12 +151,19 @@ and let_cont ~env ~res (e : Flambda.Let_cont_expr.t) =
         Continuation_handler.pattern_match handler
           ~f:(fun params ~handler:cont_body ->
             let params, env = To_jsir_shared.bound_parameters ~env params in
+            let res, addr = To_jsir_result.reserve_address res in
             let env, res =
               match Continuation_handler.is_exn_handler handler with
               | false ->
-                let res, addr = To_jsir_result.new_block res ~params in
-                let _env, res = expr ~env ~res cont_body in
                 let env = To_jsir_env.add_continuation env k addr in
+                (* CR selee: we keep the environment because we need the symbols
+                   defined there, but really there should be a way to flush
+                   variables out of the environment to respect flambda scoping
+                   invariants *)
+                let env, res = expr ~env ~res body in
+                let res =
+                  To_jsir_result.new_block_with_addr_exn res ~params ~addr
+                in
                 env, res
               | true ->
                 let exn_param, extra_args =
@@ -161,7 +177,14 @@ and let_cont ~env ~res (e : Flambda.Let_cont_expr.t) =
                 let extra_args_boxed, res =
                   make_mutables ~res (List.length extra_args)
                 in
-                let res, addr = To_jsir_result.new_block res ~params:[] in
+                let env =
+                  To_jsir_env.add_exn_handler env k ~addr ~exn_param
+                    ~extra_args:extra_args_boxed
+                in
+                let env, res = expr ~env ~res body in
+                let res =
+                  To_jsir_result.new_block_with_addr_exn res ~params:[] ~addr
+                in
                 let res =
                   List.fold_left2
                     (fun res boxed actual ->
@@ -169,14 +192,9 @@ and let_cont ~env ~res (e : Flambda.Let_cont_expr.t) =
                         (Let (actual, Field (boxed, 0, Non_float))))
                     res extra_args_boxed extra_args
                 in
-                let _env, res = expr ~env ~res cont_body in
-                let env =
-                  To_jsir_env.add_exn_handler env k ~addr ~exn_param
-                    ~extra_args:extra_args_boxed
-                in
                 env, res
             in
-            expr ~env ~res body))
+            expr ~env ~res cont_body))
   | Recursive handlers ->
     Recursive_let_cont_handlers.pattern_match handlers
       ~f:(fun ~invariant_params ~body conts ->
@@ -198,6 +216,7 @@ and let_cont ~env ~res (e : Flambda.Let_cont_expr.t) =
               env, res)
             (env, res) domain
         in
+        let env, res = expr ~env ~res body in
         let res =
           Continuation.Lmap.fold
             (fun k handler res ->
@@ -216,7 +235,7 @@ and let_cont ~env ~res (e : Flambda.Let_cont_expr.t) =
             (Flambda.Continuation_handlers.to_map conts)
             res
         in
-        expr ~env ~res body)
+        env, res)
 
 and apply_expr ~env ~res e =
   (* Pass in any extra arguments for the exception continuation in mutable
