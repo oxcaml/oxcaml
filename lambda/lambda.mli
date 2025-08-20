967a3596d43bf43b6903d56e9438681cc6c46e4a
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-08-20 14:35:59 -0400

    `transl_exp` takes `Lambda.layout`

--- lambda/lambda.mli
+++ lambda/lambda.mli
@@ -996,6 +996,7 @@ val lambda_unit: lambda
 val of_bool : bool -> lambda
 
 val layout_unit : layout
+val layout_bool : layout
 val layout_unboxed_unit : layout
 val layout_int : layout
 val layout_array : array_kind -> layout
@@ -1004,6 +1005,7 @@ val layout_list : layout
 val layout_exception : layout
 val layout_function : layout
 val layout_object : layout
+val layout_poly_variant : layout
 val layout_class : layout
 val layout_module : layout
 val layout_functor : layout
@@ -1032,6 +1034,7 @@ val layout_any_value : layout
 val layout_letrec : layout
 (* The probe hack: Free vars in probes must have layout value. *)
 val layout_probe_arg : layout
+val layout_block_idx : layout
 
 val layout_unboxed_product : layout list -> layout
 
