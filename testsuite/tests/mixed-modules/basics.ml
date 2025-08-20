5476b21f1cbc1db173015d3600c9c3f566c9dabb
James Rayman <james@jamesrayman.com>
2025-08-20 10:19:14 -0400

    WIP

--- testsuite/tests/mixed-modules/basics.ml
+++ testsuite/tests/mixed-modules/basics.ml
@@ -12,6 +12,8 @@
 open Stdlib_upstream_compatible
 external [@layout_poly] id : ('a : any). 'a -> 'a = "%opaque"
 
+let _ = print_endline "Test 1: basic mixed module"
+
 module My_module = struct
   let foo = "a"
   let bar = #5L
@@ -23,3 +25,20 @@ let _ = print_int (Int64_u.to_int (id My_module.bar))
 let _ = print_endline ""
 let _ = print_int (id My_module.qux)
 let _ = print_endline ""
+
+
+let _ = print_endline "Test 2: shadowing within a module"
+
+module Shadow_test = struct
+  let foo = "a"
+  let x = #10.0
+
+  let _ = print_float (Float_u.to_float (id x))
+  let _ = print_string " "
+
+  let x = #20.0
+  let baz = "y"
+
+  let _ = print_float (Float_u.to_float (id x))
+  let _ = print_endline ""
+end
