c66be5fff6614c4cb1b30a7d80d22cf4ac5b7a85
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2025-08-21 09:35:09 +0200

    make fmt

--- middle_end/flambda2/reaper/dep_solver.ml
+++ middle_end/flambda2/reaper/dep_solver.ml
@@ -1354,13 +1354,10 @@ let datalog_rules =
        constructor_rel call_witness code_id_of_witness codeid;
        cannot_change_calling_convention codeid ]
      ==> cannot_unbox0 allocation_id);
-    (* Cannot unbox parameters of [Indirect_unknown_arity] calls, even if they do not escape. *)
-    (let$ [ usage; allocation_id;
-            relation;
-            _v] =
-       [ "usage"; "allocation_id";
-         "relation";
-         "_v" ]
+    (* Cannot unbox parameters of [Indirect_unknown_arity] calls, even if they
+       do not escape. *)
+    (let$ [usage; allocation_id; relation; _v] =
+       ["usage"; "allocation_id"; "relation"; "_v"]
      in
      [ sources_rel usage allocation_id;
        coaccessor_rel usage relation _v;
