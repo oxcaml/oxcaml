abfa8b46f599561875f97bc05dbfcc2533c3323c
James Rayman <james@jamesrayman.com>
2025-08-25 15:40:33 -0400

    WIP

--- typing/env.mli
+++ typing/env.mli
@@ -58,16 +58,6 @@ type address = Persistent_env.address =
   | Alocal of Ident.t
   | Adot of address * Jkind.Sort.t Jkind.Layout.t array * int
 
-(* CR jrayman: Is there a better place to put these functions?
-   Both are used in Env and Includemod, and depend on Subst and Jkind (not just
-   Jkind_types)
-
-   Move to types *)
-val layout_of_lazy_signature_item :
-  Subst.Lazy.signature_item -> Jkind_types.Sort.t Jkind_types.Layout.t option
-val layout_of_signature_item :
-  signature_item -> Jkind_types.Sort.t Jkind_types.Layout.t option
-
 type t
 
 val empty: t
