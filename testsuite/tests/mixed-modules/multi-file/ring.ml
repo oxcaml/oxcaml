6d3f0f189872bb127bcc1135020f8ef571d196d9
James Rayman <james@jamesrayman.com>
2025-08-25 16:39:26 -0400

    Multifile test

--- testsuite/tests/mixed-modules/multi-file/ring.ml
+++ testsuite/tests/mixed-modules/multi-file/ring.ml
@@ -9,7 +9,8 @@ let zero = #0L
 let one = #1L
 
 let of_int a = int64_of_int a |> unbox
-let to_int a = int_of_int64 a |> box
+let to_int a = box a |> int_of_int64
 
 let add a b = of_int (to_int a + to_int b)
+let sub a b = of_int (to_int a - to_int b)
 let mul a b = of_int (to_int a * to_int b)
