3667206600e33542e1a1b0699f5cae349cdb2a57
James Rayman <james@jamesrayman.com>
2025-08-26 11:01:12 -0400

    Add tests

--- middle_end/flambda2/types/env/typing_env.ml
+++ middle_end/flambda2/types/env/typing_env.ml
@@ -1307,8 +1307,9 @@ end = struct
       | Naked_int16 _ | Naked_int32 _ | Naked_int64 _ | Naked_vec128 _
       | Naked_vec256 _ | Naked_vec512 _ | Naked_nativeint _ | Rec_info _
       | Region _ ->
-        assert (not expect_value);
-        Value_unknown
+        if expect_value
+        then Misc.fatal_errorf "Got type %a, but expected value" TG.print ty
+        else Value_unknown
     in
     let symbol_ty, _binding_time_and_mode =
       Name.Map.find (Name.symbol symbol)
