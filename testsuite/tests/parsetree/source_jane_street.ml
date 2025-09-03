328d99a9acba41c38bc4ea48622837c6831fc1a8
James Rayman <james@jamesrayman.com>
2025-09-03 14:40:19 -0400

    Maybe we need to change the untagged immediate literal

--- testsuite/tests/parsetree/source_jane_street.ml
+++ testsuite/tests/parsetree/source_jane_street.ml
@@ -1,6 +1,7 @@
 (* TEST
    flags = "-extension-universe alpha";
    include stdlib_upstream_compatible;
+   include stdlib_stable;
    expect;
 *)
 
@@ -1032,30 +1033,45 @@ val matches : int * int = (1, 2)
 (* Unboxed literals *)
 
 module Float_u = Stdlib_upstream_compatible.Float_u
+module Int8_u = Stdlib_stable.Int8_u
+module Int16_u = Stdlib_stable.Int16_u
 module Int32_u = Stdlib_upstream_compatible.Int32_u
 module Int64_u = Stdlib_upstream_compatible.Int64_u
 module Nativeint_u = Stdlib_upstream_compatible.Nativeint_u
+module Int_u = Stdlib_stable.Int_u
 
 [%%expect{|
 module Float_u = Stdlib_upstream_compatible.Float_u
+module Int8_u = Stdlib_stable.Int8_u
+module Int16_u = Stdlib_stable.Int16_u
 module Int32_u = Stdlib_upstream_compatible.Int32_u
 module Int64_u = Stdlib_upstream_compatible.Int64_u
 module Nativeint_u = Stdlib_upstream_compatible.Nativeint_u
+module Int_u = Stdlib_stable.Int_u
 |}]
 
 let test_float s f =
   Format.printf "%s: %f\n" s (Float_u.to_float f); Format.print_flush ()
+let test_int8 s f =
+  Format.printf "%s: %d\n" s (Int8_u.to_int f); Format.print_flush ()
+let test_int16 s f =
+  Format.printf "%s: %d\n" s (Int16_u.to_int f); Format.print_flush ()
 let test_int32 s f =
   Format.printf "%s: %ld\n" s (Int32_u.to_int32 f); Format.print_flush ()
 let test_int64 s f =
   Format.printf "%s: %Ld\n" s (Int64_u.to_int64 f); Format.print_flush ()
+let test_int s f =
+  Format.printf "%s: %d\n" s (Int_u.to_int f); Format.print_flush ()
 let test_nativeint s f =
   Format.printf "%s: %s\n" s (Nativeint_u.to_string f); Format.print_flush ()
 
 [%%expect{|
 val test_float : string -> Float_u.t -> unit = <fun>
+val test_int8 : string -> int8# -> unit = <fun>
+val test_int16 : string -> int16# -> unit = <fun>
 val test_int32 : string -> Int32_u.t -> unit = <fun>
 val test_int64 : string -> Int64_u.t -> unit = <fun>
+val test_int : string -> int# -> unit = <fun>
 val test_nativeint : string -> Nativeint_u.t -> unit = <fun>
 |}]
 
@@ -1075,11 +1091,19 @@ let x = test_float "one_twenty_seven_point_two_five_in_floating_hex" (#0x7f.4)
 let x = test_float "five_point_three_seven_five_in_floating_hexponent" (#0xa.cp-1)
 
 let x = test_nativeint "zero" (#0n)
+let x = test_int "zero" (#0)
+let x = test_int8 "positive_one" (+#1s)
+let x = test_int8 "positive_one" (+ #1s)
+let x = test_int16 "positive_one" (+#1S)
+let x = test_int16 "positive_one" (+ #1S)
 let x = test_int32 "positive_one" (+#1l)
 let x = test_int32 "positive_one" (+ #1l)
 let x = test_int64 "negative_one" (-#1L)
 let x = test_int64 "negative_one" (- #1L)
 let x = test_nativeint "two_fifty_five_in_hex" (#0xFFn)
+let x = test_int "ten_in_binary" (#0b1010)
+let x = test_int8 "one_hundred_in_octal" (#0o144s)
+let x = test_int16 "two_hundred_in_hex" (#0xC8S)
 let x = test_int32 "twenty_five_in_octal" (#0o31l)
 let x = test_int64 "forty_two_in_binary" (#0b101010L)
 
@@ -1110,6 +1134,16 @@ five_point_three_seven_five_in_floating_hexponent: 5.375000
 val x : unit = ()
 zero: 0
 val x : unit = ()
+zero: 0
+val x : unit = ()
+positive_one: 1
+val x : unit = ()
+positive_one: 1
+val x : unit = ()
+positive_one: 1
+val x : unit = ()
+positive_one: 1
+val x : unit = ()
 positive_one: 1
 val x : unit = ()
 positive_one: 1
@@ -1119,6 +1153,12 @@ val x : unit = ()
 negative_one: -1
 val x : unit = ()
 two_fifty_five_in_hex: 255
+val x : unit = ()
+ten_in_binary: 10
+val x : unit = ()
+one_hundred_in_octal: 100
+val x : unit = ()
+two_hundred_in_hex: 200
 val x : unit = ()
 twenty_five_in_octal: 25
 val x : unit = ()
--- testsuite/tests/typing-layouts/literals.ml
+++ testsuite/tests/typing-layouts/literals.ml
@@ -1,5 +1,6 @@
 (* TEST
  include stdlib_upstream_compatible;
+ include stdlib_stable;
  flags = "-extension layouts_beta";
  expect;
 *)
@@ -11,6 +12,7 @@ module Float_u = Stdlib_upstream_compatible.Float_u
 module Int32_u = Stdlib_upstream_compatible.Int32_u
 module Int64_u = Stdlib_upstream_compatible.Int64_u
 module Nativeint_u = Stdlib_upstream_compatible.Nativeint_u
+module Int_u = Stdlib_stable.Int_u
 
 let test_float s f =
   Format.printf "%s: %f\n" s (Float_u.to_float f); Format.print_flush ()
@@ -20,16 +22,20 @@ let test_int64 s f =
   Format.printf "%s: %Ld\n" s (Int64_u.to_int64 f); Format.print_flush ()
 let test_nativeint s f =
   Format.printf "%s: %s\n" s (Nativeint_u.to_string f); Format.print_flush ()
+let test_int s f =
+  Format.printf "%s: %d\n" s (Int_u.to_int f); Format.print_flush ()
 
 [%%expect{|
 module Float_u = Stdlib_upstream_compatible.Float_u
 module Int32_u = Stdlib_upstream_compatible.Int32_u
 module Int64_u = Stdlib_upstream_compatible.Int64_u
 module Nativeint_u = Stdlib_upstream_compatible.Nativeint_u
+module Int_u = Stdlib_stable.Int_u
 val test_float : string -> Float_u.t -> unit = <fun>
 val test_int32 : string -> Int32_u.t -> unit = <fun>
 val test_int64 : string -> Int64_u.t -> unit = <fun>
 val test_nativeint : string -> Nativeint_u.t -> unit = <fun>
+val test_int : string -> int# -> unit = <fun>
 |}]
 
 (*****************************************)
@@ -169,6 +175,27 @@ Line 1, characters 36-51:
 Error: Integer literal exceeds the range of representable integers of type "int32#"
 |}]
 
+let () = test_int "invalid_int" (#0x8000000000000000)
+[%%expect{|
+Line 1, characters 32-53:
+1 | let () = test_int "invalid_int" (#0x8000000000000000)
+                                    ^^^^^^^^^^^^^^^^^^^^^
+Error: Integer literal exceeds the range of representable integers of type "int#"
+|}]
+
+let () = test_int "max_int + 1" (#4611686018427387904)
+[%%expect{|
+max_int + 1: -4611686018427387904
+|}]
+
+let () = test_int "max_int + 2" (#4611686018427387905)
+[%%expect{|
+Line 1, characters 32-54:
+1 | let () = test_int "max_int + 2" (#4611686018427387905)
+                                    ^^^^^^^^^^^^^^^^^^^^^^
+Error: Integer literal exceeds the range of representable integers of type "int#"
+|}]
+
 (*****************************************)
 (* Patterns *)
 
@@ -297,9 +324,7 @@ val f2 : float# -> float# -> unit = <fun>
 
 let f _ _ = ();;
 let () = f
-(* This lexes as a directive. #2 is not a valid unboxed int literal
-   anyway, as it lacks a suffix.
-*)
+(* This lexes as a directive. *)
 #2 "literals.ml"
 ()
 ()
