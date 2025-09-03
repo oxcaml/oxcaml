28ce57c764f1c996362062c09257f54197e34e82
Xavier Clerc <xclerc@janestreet.com>
2025-09-03 13:29:08 +0100

    ocamlformat

--- backend/regalloc/regalloc_rewrite.ml
+++ backend/regalloc/regalloc_rewrite.ml
@@ -366,19 +366,20 @@ let prelude :
   on_fatal ~f:on_fatal_callback;
   let cfg = Cfg_with_layout.cfg cfg_with_layout in
   (* Extract function-specific regalloc params from codegen_options *)
-  function_specific_params :=
-    List.concat_map cfg.fun_codegen_options ~f:(function
-      | Cfg.Use_regalloc_param params -> params
-      | Cfg.Reduce_code_size | Cfg.No_CSE | Cfg.Use_linscan_regalloc 
-      | Cfg.Use_regalloc _ | Cfg.Cold | Cfg.Assume_zero_alloc _ | Cfg.Check_zero_alloc _ -> []);
+  function_specific_params
+    := List.concat_map cfg.fun_codegen_options ~f:(function
+         | Cfg.Use_regalloc_param params -> params
+         | Cfg.Reduce_code_size | Cfg.No_CSE | Cfg.Use_linscan_regalloc
+         | Cfg.Use_regalloc _ | Cfg.Cold | Cfg.Assume_zero_alloc _
+         | Cfg.Check_zero_alloc _ ->
+           []);
   if debug
   then (
     Utils.log "run (%S)" cfg.fun_name;
     match !function_specific_params with
     | [] -> ()
-    | params -> 
-      Utils.log "function_specific_params: %s" 
-        (String.concat ", " params));
+    | params ->
+      Utils.log "function_specific_params: %s" (String.concat ", " params));
   Reg.reinit_relocatable_regs ();
   if debug && Lazy.force invariants
   then (
--- middle_end/flambda2/parser/fexpr_to_flambda.ml
+++ middle_end/flambda2/parser/fexpr_to_flambda.ml
@@ -1006,7 +1006,7 @@ let rec expr env (e : Fexpr.expr) : Flambda.Expr.t =
             ~cost_metrics (* CR poechsel: grab inlining arguments from fexpr. *)
             ~inlining_arguments:(Inlining_arguments.create ~round:0)
             ~poll_attribute:Default
-            ~regalloc_attribute:Regalloc_attribute.Default 
+            ~regalloc_attribute:Regalloc_attribute.Default
             ~regalloc_param_attribute:[] ~cold:false ~dbg:Debuginfo.none
             ~is_tupled ~is_my_closure_used
             ~inlining_decision:Never_inline_attribute
