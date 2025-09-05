a62b7750cc8abf809b2441d8bfa71ce38ef98362
Leo Lee <seojun.lee@oriel.ox.ac.uk>
2025-09-05 17:24:42 -0400

    WIP
    support static consts

--- middle_end/flambda2/to_jsir/to_jsir_static_const.ml
+++ middle_end/flambda2/to_jsir/to_jsir_static_const.ml
@@ -58,9 +58,9 @@ let int32_to_jsir_const int32 : Jsir.constant = Int32 int32
 let int64_to_jsir_const int64 : Jsir.constant = Int64 int64
 
 let nativeint_to_jsir_const nativeint : Jsir.constant =
-  Int32 (Targetint_32_64.to_int32 nativeint)
+  NativeInt (Targetint_32_64.to_int32 nativeint)
 
-let block_or_array ~env ~res ~symbol ~tag ~mut ~array_or_not fields =
+let simple_block_or_array ~env ~res ~symbol ~tag ~mut ~array_or_not fields =
   let fields = List.map Simple.With_debuginfo.simple fields in
   let all_consts = List.for_all Simple.is_const fields in
   if all_consts && not (Mutability.is_mutable mut)
@@ -81,31 +81,31 @@ let block_or_array ~env ~res ~symbol ~tag ~mut ~array_or_not fields =
     let expr, env, res = To_jsir_shared.block ~env ~res ~tag ~mut ~fields in
     bind_expr_to_symbol ~env ~res symbol expr
 
-let immutable_float_block_or_array ~env ~res ~symbol values ~array_or_not =
+let numeric_block_or_array ~env ~res ~symbol values ~tag ~array_or_not ~to_bits
+    ~bits_to_constant ~bits_to_array =
   let all_consts = List.for_all Or_variable.is_const values in
   if all_consts
   then
     let f (x : 'a Or_variable.t) =
       match x with
-      | Const c -> Numeric_types.Float_by_bit_pattern.to_bits c
+      | Const c -> to_bits c
       | Var _ ->
         Misc.fatal_error
           "Found a variable in Or_variable.t, even though we check that all \
            values are constants"
     in
     let values = List.map f values |> Array.of_list in
-    bind_expr_to_symbol ~env ~res symbol (Constant (Float_array values))
+    bind_expr_to_symbol ~env ~res symbol (Constant (bits_to_array values))
   else
     let values, res =
       List.fold_right
         (fun x (values, res) ->
-          match (x : Numeric_types.Float_by_bit_pattern.t Or_variable.t) with
+          match (x : 'a Or_variable.t) with
           | Const c ->
-            let bits = Numeric_types.Float_by_bit_pattern.to_bits c in
             let var = Jsir.Var.fresh () in
             ( var :: values,
               To_jsir_result.add_instr_exn res
-                (Let (var, Constant (Float bits))) )
+                (Let (var, Constant (bits_to_constant (to_bits c)))) )
           | Var (v, dbg) ->
             To_jsir_result.with_debuginfo_exn res dbg ~f:(fun res ->
                 let var = To_jsir_env.get_var_exn env v in
@@ -113,9 +113,10 @@ let immutable_float_block_or_array ~env ~res ~symbol values ~array_or_not =
         values ([], res)
     in
     let values = Array.of_list values in
-    let tag = Tag.double_array_tag |> Tag.to_int in
     let expr : Jsir.expr = Block (tag, values, array_or_not, Immutable) in
     bind_expr_to_symbol ~env ~res symbol expr
+
+let length_is_even values = List.length values mod 2 = 0
 
 let block_like ~env ~res symbol (const : Static_const.t) =
   match const with
@@ -126,7 +127,8 @@ let block_like ~env ~res symbol (const : Static_const.t) =
       Static_const.print const
   | Block (tag, mut, _shape, fields) ->
     let tag = Tag.Scannable.to_tag tag in
-    block_or_array ~env ~res ~symbol ~tag ~mut ~array_or_not:NotArray fields
+    simple_block_or_array ~env ~res ~symbol ~tag ~mut ~array_or_not:NotArray
+      fields
   | Boxed_float32 value ->
     const_or_var ~env ~res ~symbol ~to_jsir_const:float32_to_jsir_const value
   | Boxed_float value ->
@@ -141,43 +143,74 @@ let block_like ~env ~res symbol (const : Static_const.t) =
     (* Need SIMD *)
     static_const_not_supported ()
   | Immutable_float_block values ->
-    immutable_float_block_or_array ~env ~res ~symbol values
-      ~array_or_not:NotArray
+    numeric_block_or_array ~env ~res ~symbol values
+      ~tag:(Tag.to_int Tag.double_array_tag)
+      ~array_or_not:NotArray ~to_bits:Numeric_types.Float_by_bit_pattern.to_bits
+      ~bits_to_constant:(fun bits -> Float bits)
+      ~bits_to_array:(fun bits -> Float_array bits)
   | Immutable_float_array values ->
-    immutable_float_block_or_array ~env ~res ~symbol values ~array_or_not:Array
+    numeric_block_or_array ~env ~res ~symbol values
+      ~tag:(Tag.to_int Tag.double_array_tag)
+      ~array_or_not:Array ~to_bits:Numeric_types.Float_by_bit_pattern.to_bits
+      ~bits_to_constant:(fun bits -> Float bits)
+      ~bits_to_array:(fun bits -> Float_array bits)
   | Immutable_float32_array values ->
-    ignore values;
-    static_const_not_supported ()
-  | Immutable_int32_array values ->
-    ignore values;
-    static_const_not_supported ()
-  | Immutable_int64_array values ->
-    ignore values;
-    static_const_not_supported ()
-  | Immutable_nativeint_array values ->
-    ignore values;
-    static_const_not_supported ()
-  | Immutable_vec128_array values ->
-    ignore values;
-    static_const_not_supported ()
-  | Immutable_vec256_array values ->
-    ignore values;
-    static_const_not_supported ()
-  | Immutable_vec512_array values ->
-    ignore values;
-    static_const_not_supported ()
+    let tag =
+      if length_is_even values
+      then Cmm_helpers.Unboxed_array_tags.unboxed_float32_array_even_tag
+      else Cmm_helpers.Unboxed_array_tags.unboxed_float32_array_odd_tag
+    in
+    numeric_block_or_array ~env ~res ~symbol values ~tag ~array_or_not:Array
+      ~to_bits:(fun float ->
+        Numeric_types.Float32_by_bit_pattern.to_bits float |> Int64.of_int32)
+      ~bits_to_constant:(fun bits -> Float32 bits)
+      ~bits_to_array:(fun bits -> Float_array bits)
   | Immutable_value_array values ->
-    block_or_array ~env ~res ~symbol ~tag:Tag.zero ~mut:Immutable
+    simple_block_or_array ~env ~res ~symbol ~tag:Tag.zero ~mut:Immutable
       ~array_or_not:Array values
-  | Empty_array kind -> (
-    match kind with
-    | Values_or_immediates_or_naked_floats | Naked_float32s ->
-      bind_expr_to_symbol ~env ~res symbol
-        (Prim (Extern "caml_make_vect", [Pc (Int Targetint.zero); Pc Null]))
-    | Unboxed_products | Naked_int32s | Naked_int64s | Naked_nativeints
-    | Naked_vec128s | Naked_vec256s | Naked_vec512s ->
-      (* No SIMD *)
-      static_const_not_supported ())
+  | Immutable_int32_array values ->
+    let tag =
+      if length_is_even values
+      then Cmm_helpers.Unboxed_array_tags.unboxed_int32_array_even_tag
+      else Cmm_helpers.Unboxed_array_tags.unboxed_int32_array_odd_tag
+    in
+    numeric_block_or_array ~env ~res ~symbol values ~tag ~array_or_not:Array
+      ~to_bits:Fun.id ~bits_to_constant:int32_to_jsir_const
+      ~bits_to_array:(fun bits ->
+        Tuple (tag, Array.map int32_to_jsir_const bits, Array))
+  | Immutable_int64_array values ->
+    let tag = Cmm_helpers.Unboxed_array_tags.unboxed_int64_array_tag in
+    numeric_block_or_array ~env ~res ~symbol values ~tag ~array_or_not:Array
+      ~to_bits:Fun.id ~bits_to_constant:int64_to_jsir_const
+      ~bits_to_array:(fun bits ->
+        Tuple (tag, Array.map int64_to_jsir_const bits, Array))
+  | Immutable_nativeint_array values ->
+    let tag = Cmm_helpers.Unboxed_array_tags.unboxed_nativeint_array_tag in
+    numeric_block_or_array ~env ~res ~symbol values ~tag ~array_or_not:Array
+      ~to_bits:Targetint_32_64.to_int32 ~bits_to_constant:int32_to_jsir_const
+      ~bits_to_array:(fun bits ->
+        Tuple (tag, Array.map int32_to_jsir_const bits, Array))
+  | Immutable_vec128_array _ | Immutable_vec256_array _
+  | Immutable_vec512_array _ ->
+    (* Need SIMD *)
+    static_const_not_supported ()
+  | Empty_array kind ->
+    let tag =
+      match kind with
+      | Values_or_immediates_or_naked_floats -> 0
+      | Naked_float32s ->
+        Cmm_helpers.Unboxed_array_tags.unboxed_float32_array_even_tag
+      | Naked_int32s ->
+        Cmm_helpers.Unboxed_array_tags.unboxed_int32_array_even_tag
+      | Naked_nativeints ->
+        Cmm_helpers.Unboxed_array_tags.unboxed_nativeint_array_tag
+      | Naked_int64s -> Cmm_helpers.Unboxed_array_tags.unboxed_int64_array_tag
+      | Unboxed_products | Naked_vec128s | Naked_vec256s | Naked_vec512s ->
+        (* No SIMD *)
+        static_const_not_supported ()
+    in
+    bind_expr_to_symbol ~env ~res symbol
+      (Prim (Extern "caml_make_vect", [Pc (Int (Targetint.of_int tag)); Pc Null]))
   | Mutable_string { initial_value } ->
     ignore initial_value;
     static_const_not_supported ()
