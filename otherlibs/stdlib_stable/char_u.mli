876b5f1354a6b492e23e8db230b2e081df4da525
James Rayman <james@jamesrayman.com>
2025-09-05 14:41:06 -0400

    Begin `Char_u`

---
+++ otherlibs/stdlib_stable/char_u.mli
@@ -0,0 +1,23 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                  James Rayman, Jane Street, New York                   *)
+(*                                                                        *)
+(*   Copyright 2025 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Converting between other types *)
+
+external of_int8_u : int8# -> char# = "%identity"
+
+external to_int8_u : char# -> int8# = "%identity"
+
+val of_char : char -> char#
+
+val to_char : char# -> char
