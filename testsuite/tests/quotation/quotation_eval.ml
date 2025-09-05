29f0ad4d6b4dcc822fa3dc9ade08a79a0ac92173
Jack Rickard <jrickard@janestreet.com>
2025-09-05 11:01:41 +0100

    Improve the quotation evaluation tests

--- testsuite/tests/quotation/quotation_eval.ml
+++ testsuite/tests/quotation/quotation_eval.ml
@@ -1,23 +1,34 @@
 (* TEST *)
 
-let test0 =
-  Printf.printf "\nTest 0\n";
+let test_simple_eval =
+  Printf.printf "\nTest simple eval\n";
   let eval : <[int]> expr -> int = [%eval: int] in
   let output = eval <[ 42 ]> in
   Printf.printf "Output: %d\n" output;
 ;;
 
-let test1 =
-  Printf.printf "\nTest 1\n";
+let test_complex_return_type =
+  Printf.printf "\nTest complex return type\n";
   let compiled = [%eval: int -> int list] <[ fun x -> [ x ; x + 1 ] ]> in
   let output = compiled 42 in
   Printf.printf
-    "Output: [%s]\n"
-    (String.concat "; " (List.map string_of_int output))
+  "Output: [%s]\n"
+  (String.concat "; " (List.map string_of_int output))
 ;;
 
-let test2 =
-  Printf.printf "\nTest 2\n";
+let test_side_effects =
+  Printf.printf "\nTest side effects\n";
+  Printf.printf "Compiling...\n";
+  let compiled = [%eval: unit -> unit]
+    <[ print_endline "Outside";
+       fun () -> print_endline "Inside" ]>
+  in
+  Printf.printf "Running...\n";
+  let () = compiled () in
+  Printf.printf "Done\n"
+
+let test_reference_to_global =
+  Printf.printf "\nTest reference to global\n";
   let eval = [%eval: Buffer.t] in
   let output : Buffer.t = eval
     <[ let b = Buffer.create 42 in Buffer.add_string b "Hello world!" ; b ]>
@@ -25,18 +36,20 @@ let test2 =
   Printf.printf "Output: %s\n" (Buffer.contents output);
 ;;
 
-let test3 =
-  Printf.printf "\nTest 3\n";
-  (* This quote passes type-checking but fail during compilation. *)
+let test_late_compilation_error =
+  Printf.printf "\nTest late compilation error\n";
+  (* This quote passes type-checking but fail during compilation.
+     Eventually we should run quotes through transl so that we spot these errors
+     (and don't emit warning 53 for the attributes). *)
   let quote = <[
     let ignore (_ @ local) = () in
     let[@tail_mod_cons] rec foo x = exclave_
       if x = 0 then [] else x :: foo (x - 1)
     in
     ignore (foo 5);
-    "This quote should fail to compile. If it's started working then you need\n\
-     to come up with a new way for this test to pass type-checking but fail\n\
-     to compile."
+    "This quote should fail to compile. If it's started working but you\n\
+     aren't running quotes through transl then you may need to find a new way\n\
+     to trigger this."
   ]> in
   try
     let output = [%eval: string] quote in
