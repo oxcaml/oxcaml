05fae7b76dc9fd087f19696ca005f60081e08b2b
Jack Rickard <jrickard@janestreet.com>
2025-09-05 11:01:41 +0100

    Improve the quotation evaluation tests

--- testsuite/tests/quotation/quotation_eval.ml
+++ testsuite/tests/quotation/quotation_eval.ml
@@ -1,7 +1,7 @@
 (* TEST *)
 
-let test0 =
-  Printf.printf "\nTest 0\n";
+let test_simple_eval =
+  Printf.printf "\nTest simple eval\n";
   let eval : <[int]> expr -> unit -> int = [%eval: int] in
   Printf.printf "Compiling...\n";
   let compiled = eval <[ 42 ]> in
@@ -10,8 +10,8 @@ let test0 =
   Printf.printf "Output: %d\n" output;
 ;;
 
-let test1 =
-  Printf.printf "\nTest 1\n";
+let test_complex_return_type =
+  Printf.printf "\nTest complex return type\n";
   let eval = [%eval: int -> int list] in
   Printf.printf "Compiling...\n";
   let compiled = eval <[ fun x -> [ x ; x + 1 ] ]> in
@@ -22,8 +22,8 @@ let test1 =
     (String.concat "; " (List.map string_of_int output))
 ;;
 
-let test2 =
-  Printf.printf "\nTest 2\n";
+let test_reference_to_global =
+  Printf.printf "\nTest reference to global\n";
   let eval = [%eval: Buffer.t] in
   Printf.printf "Compiling...\n";
   let compiled : unit -> Buffer.t = eval
@@ -34,8 +34,10 @@ let test2 =
   Printf.printf "Output: %s\n" (Buffer.contents output);
 ;;
 
-let test3 =
-  Printf.printf "\nTest 3\n";
+let test_late_compilation_error =
+  (* Eventually we should run quotes through transl so that we spot these errors
+     (and don't emit warning 53 for the attributes). *)
+  Printf.printf "\nTest late compilation error\n";
   let eval = [%eval: string] in
   Printf.printf "Compiling...\n";
   (* This quote passes type-checking but fail during compilation, this lets us test exactly
@@ -46,9 +48,9 @@ let test3 =
       if x = 0 then [] else x :: foo (x - 1)
     in
     ignore (foo 5);
-    "This quote should fail to compile. If it's started working then you need\n\
-     to come up with a new way for this test to pass type-checking but fail\n\
-     to compile."
+    "This quote should fail to compile. If it's started working but you\n\
+     aren't running quotes through transl then you may need to find a new way\n\
+     to trigger this."
   ]> in
   try
     let compiled = eval quote in
