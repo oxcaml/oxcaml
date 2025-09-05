d3017ea2074b7914e62eba4b585ca4dc76f4cd21
James Rayman <james@jamesrayman.com>
2025-09-05 14:37:28 -0400

    Add some tests

--- testsuite/tests/typing-layouts/literals.ml
+++ testsuite/tests/typing-layouts/literals.ml
@@ -13,6 +13,7 @@ module Int32_u = Stdlib_upstream_compatible.Int32_u
 module Int64_u = Stdlib_upstream_compatible.Int64_u
 module Nativeint_u = Stdlib_upstream_compatible.Nativeint_u
 module Int_u = Stdlib_stable.Int_u
+module Char_u = Stdlib_stable.Char_u
 
 let test_float s f =
   Format.printf "%s: %f\n" s (Float_u.to_float f); Format.print_flush ()
@@ -24,6 +25,8 @@ let test_nativeint s f =
   Format.printf "%s: %s\n" s (Nativeint_u.to_string f); Format.print_flush ()
 let test_int s f =
   Format.printf "%s: %d\n" s (Int_u.to_int f); Format.print_flush ()
+let test_char s f =
+  Format.printf "%s: %C\n" s (Char_u.to_char f); Format.print_flush ()
 
 [%%expect{|
 module Float_u = Stdlib_upstream_compatible.Float_u
@@ -194,6 +197,14 @@ Line 1, characters 32-55:
 1 | let () = test_int "max_int + 2" (#4611686018427387905m)
                                     ^^^^^^^^^^^^^^^^^^^^^^^
 Error: Integer literal exceeds the range of representable integers of type "int#"
+|}]
+
+let () = test_char "untagged char" #'c'
+[%%expect{|
+|}]
+
+let () = test_char "untagged escape char" #'\o000'
+[%%expect{|
 |}]
 
 (*****************************************)
