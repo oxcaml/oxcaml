6ca1d075612a7afbc99eacc4643506acf5abe955
julesjacobs <julesjacobs@gmail.com>
2025-09-16 14:11:49 -0400

    Formatting

--- typing/ikinds/ldd.ml
+++ typing/ikinds/ldd.ml
@@ -2,20 +2,31 @@
 
 module type LATTICE = sig
   type t
+
   val bot : t
+
   val top : t
+
   val join : t -> t -> t
+
   val meet : t -> t -> t
+
   val co_sub : t -> t -> t
+
   val to_string : t -> string
+
   val equal : t -> t -> bool
+
   val hash : t -> int
+
   val find_non_bot_axis : t -> int option
 end
 
 module type ORDERED = sig
   type t
+
   val compare : t -> t -> int
+
   val to_string : t -> string
 end
 
