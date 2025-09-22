a2bf64b30bf643918063dbb952dba5e4fe6d88fc
James Rayman <james@jamesrayman.com>
2025-09-22 12:39:17 -0400

    upd

--- testsuite/tests/typing-misc/wishlist.ml
+++ testsuite/tests/typing-misc/wishlist.ml
@@ -13,8 +13,8 @@ let partial_application = add 5
 let error = double partial_application
 [%%expect{|
 val partial_application : int -> int = <fun>
-Line 1, characters 19-38:
-1 | let error = double partial_application
+Line 2, characters 19-38:
+2 | let error = double partial_application
                        ^^^^^^^^^^^^^^^^^^^
 Error: This expression has type "int -> int"
        but an expression was expected of type "int"
@@ -27,4 +27,18 @@ Line 1, characters 51-56:
                                                        ^^^^^
 Error: This expression has type "int" but an expression was expected of type
          "int -> int"
+|}]
+
+
+let foo_1 a b ~c = (a + b) * c
+let foo_2 a b = foo_1 (a + 1) (b + 1)
+let foo_3 a b condition = if condition then foo_2 a b else 0
+[%%expect{|
+val foo_1 : int -> int -> c:int -> int = <fun>
+val foo_2 : int -> int -> c:int -> int = <fun>
+Line 3, characters 59-60:
+3 | let foo_3 a b condition = if condition then foo_2 a b else 0
+                                                               ^
+Error: This expression has type "int" but an expression was expected of type
+         "c:int -> int"
 |}]
