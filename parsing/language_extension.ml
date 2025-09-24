b0a2ffcfe48f54381bebeac53526f4601a0065b1
Andrej Ivaskovic <aivaskovic@janestreet.com>
2025-09-24 10:53:26 +0100

    ocamlformat

--- parsing/language_extension.ml
+++ parsing/language_extension.ml
@@ -126,7 +126,8 @@ module Exist_pair = struct
     | Pair (Small_numbers, m) ->
       to_string Small_numbers ^ "_" ^ maturity_to_string m
     | Pair (SIMD, m) -> to_string SIMD ^ "_" ^ maturity_to_string m
-    | Pair (Layout_poly, m) -> to_string Layout_poly ^ "_" ^ maturity_to_string m
+    | Pair (Layout_poly, m) ->
+      to_string Layout_poly ^ "_" ^ maturity_to_string m
     | Pair
         ( (( Comprehensions | Include_functor | Polymorphic_parameters
            | Immutable_arrays | Module_strengthening | Labeled_tuples
