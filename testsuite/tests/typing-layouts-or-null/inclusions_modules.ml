f3a0fe55760cac19cfe3b5c125d811f0fa0e4804
julesjacobs <julesjacobs@gmail.com>
2025-09-29 13:18:32 -0400

    More tests

--- testsuite/tests/typing-layouts-or-null/inclusions_modules.ml
+++ testsuite/tests/typing-layouts-or-null/inclusions_modules.ml
@@ -2,6 +2,54 @@
  expect;
 *)
 
+module A1 : sig type t : value end = struct
+  type t : value_or_null
+end
+
+[%%expect{|
+Lines 1-3, characters 37-3:
+1 | .....................................struct
+2 |   type t : value_or_null
+3 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig type t : value_or_null end
+       is not included in
+         sig type t end
+       Type declarations do not match:
+         type t : value_or_null
+       is not included in
+         type t
+       The kind of the first is value_or_null
+         because of the definition of t at line 2, characters 2-24.
+       But the kind of the first must be a subkind of value
+         because of the definition of t at line 1, characters 16-30.
+|}]
+
+module A2 : sig type t : any mod separable end = struct
+  type t : any
+end
+
+[%%expect{|
+Lines 1-3, characters 49-3:
+1 | .................................................struct
+2 |   type t : any
+3 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig type t : any end
+       is not included in
+         sig type t : any mod separable end
+       Type declarations do not match:
+         type t : any
+       is not included in
+         type t : any mod separable
+       The kind of the first is any
+         because of the definition of t at line 2, characters 2-14.
+       But the kind of the first must be a subkind of any mod separable
+         because of the definition of t at line 1, characters 16-42.
+|}]
+
 (* value_or_null is not a sublayout of value (module inclusion) *)
 module M1 (X : sig type t : value_or_null end) : sig type t : value end = X
 
@@ -42,4 +90,3 @@ Error: Signature mismatch:
        But the kind of the first must be a subkind of any mod separable
          because of the definition of t at line 1, characters 43-69.
 |}]
-
