1da2d0d8791474d32265b0ad2411aa397a5f92d3
James Rayman <james@jamesrayman.com>
2025-09-30 16:59:54 -0400

    init

---
+++ tools/allprims.ml
@@ -0,0 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                  James Rayman, Jane Street, New York                   *)
+(*                                                                        *)
+(*   Copyright 2025 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+open! Lambda
+
+let () = print_endline "Hello world!"
