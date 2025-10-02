03fb4172272960f40ae4b532e82e362380cd0d0d
Luke Maurer <lmaurer@janestreet.com>
2025-10-02 15:30:29 +0100

    Add profiling

--- lambda/slambdaeval.ml
+++ lambda/slambdaeval.ml
@@ -1,3 +1,5 @@
 module SL = Slambda
 
-let eval ({ Lambda.code = SL.Quote lam } as p) = { p with code = lam }
+let do_eval ({ Lambda.code = SL.Quote lam } as p) = { p with code = lam }
+
+let eval p = Profile.(record static_eval) do_eval p
