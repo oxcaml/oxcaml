6db19f40b0b74ecac533e78a62e359d83d21af33
Mark Shinwell <mshinwell@pm.me>
2025-10-09 18:54:52 +0100

    Two different intersection functions

--- backend/debug/reg_availability_set.ml
+++ backend/debug/reg_availability_set.ml
@@ -35,6 +35,14 @@ let union t1 t2 =
   | Unreachable, _ | _, Unreachable -> Unreachable
 
 let inter t1 t2 =
+  (* This is for Compute_ranges, not the join in Cfg_available_regs. *)
+  match t1, t2 with
+  | Unreachable, _ -> Unreachable
+  | _, Unreachable -> Unreachable
+  | Ok avail1, Ok avail2 -> Ok (RD_quotient_set.inter avail1 avail2)
+
+let inter_removing_conflicting_debug_info t1 t2 =
+  (* This is for the join in Cfg_available_regs, not for Compute_ranges. *)
   match t1, t2 with
   | Unreachable, _ -> t2
   | _, Unreachable -> t1
