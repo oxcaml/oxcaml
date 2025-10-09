ecb64689b568a35c132c18e74567c0f879bdc419
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-10-09 12:07:58 +0100

    Add a test

---
+++ oxcaml/tests/backend/oxcaml_dwarf/test_tailrec_dwarf.ml
@@ -0,0 +1,9 @@
+let[@inline never] [@local never] [@loop never] rec loop x =
+  print_int x;
+  print_newline ();
+  if x = 0
+  then 0
+  else loop (x - 1)
+;;
+
+let () = loop (Sys.opaque_identity 3) |> ignore
