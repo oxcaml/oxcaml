754262df888dc3a6a61d6126458e4b99ea981803
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2025-10-17 14:46:43 +0200

    Add labels on some constructors of relations

--- middle_end/flambda2/reaper/global_flow_graph.ml
+++ middle_end/flambda2/reaper/global_flow_graph.ml
@@ -13,34 +13,26 @@
 (*                                                                        *)
 (**************************************************************************)
 
-module Alias_rel = Datalog.Schema.Relation2 (Code_id_or_name) (Code_id_or_name)
-module Use_rel = Datalog.Schema.Relation2 (Code_id_or_name) (Code_id_or_name)
-module Accessor_rel =
+module NN = Datalog.Schema.Relation2 (Code_id_or_name) (Code_id_or_name)
+module NFN =
   Datalog.Schema.Relation3 (Code_id_or_name) (Field) (Code_id_or_name)
-module Constructor_rel =
-  Datalog.Schema.Relation3 (Code_id_or_name) (Field) (Code_id_or_name)
-module Propagate_rel =
+module NCN =
+  Datalog.Schema.Relation3 (Code_id_or_name) (Cofield) (Code_id_or_name)
+module NNN =
   Datalog.Schema.Relation3 (Code_id_or_name) (Code_id_or_name) (Code_id_or_name)
-module CoAccessor_rel =
-  Datalog.Schema.Relation3 (Code_id_or_name) (Cofield) (Code_id_or_name)
-module CoConstructor_rel =
-  Datalog.Schema.Relation3 (Code_id_or_name) (Cofield) (Code_id_or_name)
-module Any_usage_pred = Datalog.Schema.Relation1 (Code_id_or_name)
-module Any_source_pred = Datalog.Schema.Relation1 (Code_id_or_name)
-module Code_id_my_closure_rel =
-  Datalog.Schema.Relation2 (Code_id_or_name) (Code_id_or_name)
+module N = Datalog.Schema.Relation1 (Code_id_or_name)
 
 type graph =
-  { mutable alias_rel : Alias_rel.t;
-    mutable use_rel : Use_rel.t;
-    mutable accessor_rel : Accessor_rel.t;
-    mutable constructor_rel : Constructor_rel.t;
-    mutable coaccessor_rel : CoAccessor_rel.t;
-    mutable coconstructor_rel : CoConstructor_rel.t;
-    mutable propagate_rel : Propagate_rel.t;
-    mutable any_usage_pred : Any_usage_pred.t;
-    mutable any_source_pred : Any_source_pred.t;
-    mutable code_id_my_closure_rel : Code_id_my_closure_rel.t
+  { mutable alias_rel : NN.t;
+    mutable use_rel : NN.t;
+    mutable accessor_rel : NFN.t;
+    mutable constructor_rel : NFN.t;
+    mutable coaccessor_rel : NCN.t;
+    mutable coconstructor_rel : NCN.t;
+    mutable propagate_rel : NNN.t;
+    mutable any_usage_pred : N.t;
+    mutable any_source_pred : N.t;
+    mutable code_id_my_closure_rel : NN.t
   }
 
 let print_iter_edges ~print_edge graph =
@@ -71,26 +63,25 @@ let print_iter_edges ~print_edge graph =
     (fun _if_defined m -> iter_nn "purple" m)
     graph.propagate_rel
 
-let alias_rel = Alias_rel.create ~name:"alias"
+let alias_rel = NN.create ~name:"alias"
 
-let use_rel = Use_rel.create ~name:"use"
+let use_rel = NN.create ~name:"use"
 
-let accessor_rel = Accessor_rel.create ~name:"accessor"
+let accessor_rel = NFN.create ~name:"accessor"
 
-let constructor_rel = Constructor_rel.create ~name:"constructor"
+let constructor_rel = NFN.create ~name:"constructor"
 
-let coaccessor_rel = CoAccessor_rel.create ~name:"coaccessor"
+let coaccessor_rel = NCN.create ~name:"coaccessor"
 
-let coconstructor_rel = CoConstructor_rel.create ~name:"coconstructor"
+let coconstructor_rel = NCN.create ~name:"coconstructor"
 
-let propagate_rel = Propagate_rel.create ~name:"propagate"
+let propagate_rel = NNN.create ~name:"propagate"
 
-let any_usage_pred = Any_usage_pred.create ~name:"any_usage"
+let any_usage_pred = N.create ~name:"any_usage"
 
-let any_source_pred = Any_source_pred.create ~name:"any_source"
+let any_source_pred = N.create ~name:"any_source"
 
-let code_id_my_closure_rel =
-  Code_id_my_closure_rel.create ~name:"code_id_my_closure"
+let code_id_my_closure_rel = NN.create ~name:"code_id_my_closure"
 
 let to_datalog graph =
   Datalog.set_table alias_rel graph.alias_rel
@@ -105,13 +96,9 @@ let to_datalog graph =
   @@ Datalog.set_table code_id_my_closure_rel graph.code_id_my_closure_rel
   @@ Datalog.empty
 
-type 'a rel0 = [> `Atom of Datalog.atom] as 'a
+type 'a atom = [> `Atom of Datalog.atom] as 'a
 
-type ('a, 'b) rel1 = 'a Datalog.Term.t -> 'b rel0
-
-type ('a, 'b, 'c) rel2 = 'a Datalog.Term.t -> ('b, 'c) rel1
-
-type ('a, 'b, 'c, 'd) rel3 = 'a Datalog.Term.t -> ('b, 'c, 'd) rel2
+type 'a term = 'a Datalog.Term.t
 
 (* Naming:
  * to_ = from; (alias)
@@ -120,72 +107,68 @@ type ('a, 'b, 'c, 'd) rel3 = 'a Datalog.Term.t -> ('b, 'c, 'd) rel2
  * base = Make_block { from_ } (constructor)
  * *)
 
-let alias_rel to_ from = Datalog.atom alias_rel [to_; from]
+let alias_rel ~to_ ~from = Datalog.atom alias_rel [to_; from]
 
-let use_rel to_ from = Datalog.atom use_rel [to_; from]
+let use_rel ~to_ ~from = Datalog.atom use_rel [to_; from]
 
-let accessor_rel to_ relation base =
+let accessor_rel ~to_ ~relation ~base =
   Datalog.atom accessor_rel [to_; relation; base]
 
-let constructor_rel base relation from =
+let constructor_rel ~base ~relation ~from =
   Datalog.atom constructor_rel [base; relation; from]
 
-let coaccessor_rel to_ relation base =
+let coaccessor_rel ~to_ ~relation ~base =
   Datalog.atom coaccessor_rel [to_; relation; base]
 
-let coconstructor_rel base relation from =
+let coconstructor_rel ~base ~relation ~from =
   Datalog.atom coconstructor_rel [base; relation; from]
 
-let propagate_rel if_used to_ from =
+let propagate_rel ~if_used ~to_ ~from =
   Datalog.atom propagate_rel [if_used; to_; from]
 
 let any_usage_pred var = Datalog.atom any_usage_pred [var]
 
 let any_source_pred var = Datalog.atom any_source_pred [var]
 
-let code_id_my_closure_rel code_id var =
-  Datalog.atom code_id_my_closure_rel [code_id; var]
+let code_id_my_closure_rel ~code_id ~my_closure =
+  Datalog.atom code_id_my_closure_rel [code_id; my_closure]
 
 let create () =
-  { alias_rel = Alias_rel.empty;
-    use_rel = Use_rel.empty;
-    accessor_rel = Accessor_rel.empty;
-    constructor_rel = Constructor_rel.empty;
-    coaccessor_rel = CoAccessor_rel.empty;
-    coconstructor_rel = CoConstructor_rel.empty;
-    propagate_rel = Propagate_rel.empty;
-    any_usage_pred = Any_usage_pred.empty;
-    any_source_pred = Any_source_pred.empty;
-    code_id_my_closure_rel = Code_id_my_closure_rel.empty
+  { alias_rel = NN.empty;
+    use_rel = NN.empty;
+    accessor_rel = NFN.empty;
+    constructor_rel = NFN.empty;
+    coaccessor_rel = NCN.empty;
+    coconstructor_rel = NCN.empty;
+    propagate_rel = NNN.empty;
+    any_usage_pred = N.empty;
+    any_source_pred = N.empty;
+    code_id_my_closure_rel = NN.empty
   }
 
 let add_alias t ~to_ ~from =
-  t.alias_rel <- Alias_rel.add_or_replace [to_; from] () t.alias_rel
+  t.alias_rel <- NN.add_or_replace [to_; from] () t.alias_rel
 
 let add_use_dep t ~to_ ~from =
-  t.use_rel <- Use_rel.add_or_replace [to_; from] () t.use_rel
+  t.use_rel <- NN.add_or_replace [to_; from] () t.use_rel
 
 let add_constructor_dep t ~base relation ~from =
   t.constructor_rel
-    <- Constructor_rel.add_or_replace [base; relation; from] ()
-         t.constructor_rel
+    <- NFN.add_or_replace [base; relation; from] () t.constructor_rel
 
 let add_accessor_dep t ~to_ relation ~base =
-  t.accessor_rel
-    <- Accessor_rel.add_or_replace [to_; relation; base] () t.accessor_rel
+  t.accessor_rel <- NFN.add_or_replace [to_; relation; base] () t.accessor_rel
 
 let add_coaccessor_dep t ~to_ relation ~base =
   t.coaccessor_rel
-    <- CoAccessor_rel.add_or_replace [to_; relation; base] () t.coaccessor_rel
+    <- NCN.add_or_replace [to_; relation; base] () t.coaccessor_rel
 
 let add_coconstructor_dep t ~base relation ~from =
   t.coconstructor_rel
-    <- CoConstructor_rel.add_or_replace [base; relation; from] ()
-         t.coconstructor_rel
+    <- NCN.add_or_replace [base; relation; from] () t.coconstructor_rel
 
 let add_propagate_dep t ~if_used ~to_ ~from =
-  t.propagate_rel
-    <- Propagate_rel.add_or_replace [if_used; to_; from] () t.propagate_rel
+  t.propagate_rel <- NNN.add_or_replace [if_used; to_; from] () t.propagate_rel
 
 let add_opaque_let_dependency t ~to_ ~from =
   let bound_to = Bound_pattern.free_names to_ in
@@ -200,13 +183,13 @@ let add_opaque_let_dependency t ~to_ ~from =
   Name_occurrences.fold_names bound_to ~f ~init:()
 
 let add_use t (var : Code_id_or_name.t) =
-  t.any_usage_pred <- Any_usage_pred.add_or_replace [var] () t.any_usage_pred
+  t.any_usage_pred <- N.add_or_replace [var] () t.any_usage_pred
 
 let add_any_source t (var : Code_id_or_name.t) =
-  t.any_source_pred <- Any_source_pred.add_or_replace [var] () t.any_source_pred
+  t.any_source_pred <- N.add_or_replace [var] () t.any_source_pred
 
 let add_code_id_my_closure t code_id my_closure =
   t.code_id_my_closure_rel
-    <- Code_id_my_closure_rel.add_or_replace
+    <- NN.add_or_replace
          [Code_id_or_name.code_id code_id; Code_id_or_name.var my_closure]
          () t.code_id_my_closure_rel
--- middle_end/flambda2/reaper/global_flow_graph.mli
+++ middle_end/flambda2/reaper/global_flow_graph.mli
@@ -17,51 +17,62 @@ type graph
 
 val to_datalog : graph -> Datalog.database
 
-type 'a rel0 = [> `Atom of Datalog.atom] as 'a
+type 'a atom = [> `Atom of Datalog.atom] as 'a
 
-type ('a, 'b) rel1 = 'a Datalog.Term.t -> 'b rel0
+type 'a term = 'a Datalog.Term.t
 
-type ('a, 'b, 'c) rel2 = 'a Datalog.Term.t -> ('b, 'c) rel1
-
-type ('a, 'b, 'c, 'd) rel3 = 'a Datalog.Term.t -> ('b, 'c, 'd) rel2
-
-val alias_rel : (Code_id_or_name.t, Code_id_or_name.t, _) rel2
-
-val use_rel : (Code_id_or_name.t, Code_id_or_name.t, _) rel2
-
-val accessor_rel : (Code_id_or_name.t, Field.t, Code_id_or_name.t, _) rel3
-
-val constructor_rel : (Code_id_or_name.t, Field.t, Code_id_or_name.t, _) rel3
-
-val coaccessor_rel : (Code_id_or_name.t, Cofield.t, Code_id_or_name.t, _) rel3
-
-val coconstructor_rel :
-  (Code_id_or_name.t, Cofield.t, Code_id_or_name.t, _) rel3
-
-val propagate_rel :
-  (Code_id_or_name.t, Code_id_or_name.t, Code_id_or_name.t, _) rel3
-
-val any_usage_pred : (Code_id_or_name.t, _) rel1
-
-val any_source_pred : (Code_id_or_name.t, _) rel1
-
-(* An entry (code_id, v) in this relation means that [v] is the "my_closure"
-   variable of the code associated to [code_id]. *)
-val code_id_my_closure_rel : (Code_id_or_name.t, Code_id_or_name.t, _) rel2
-
-val create : unit -> graph
-
-val add_opaque_let_dependency :
-  graph -> to_:Bound_pattern.t -> from:Name_occurrences.t -> unit
+val alias_rel :
+  to_:Code_id_or_name.t term -> from:Code_id_or_name.t term -> _ atom
 
 val add_alias : graph -> to_:Code_id_or_name.t -> from:Code_id_or_name.t -> unit
+
+val use_rel :
+  to_:Code_id_or_name.t term -> from:Code_id_or_name.t term -> _ atom
 
 val add_use_dep :
   graph -> to_:Code_id_or_name.t -> from:Code_id_or_name.t -> unit
 
-val add_use : graph -> Code_id_or_name.t -> unit
+val accessor_rel :
+  to_:Code_id_or_name.t term ->
+  relation:Field.t term ->
+  base:Code_id_or_name.t term ->
+  _ atom
 
-val add_any_source : graph -> Code_id_or_name.t -> unit
+val add_accessor_dep :
+  graph -> to_:Code_id_or_name.t -> Field.t -> base:Code_id_or_name.t -> unit
+
+val constructor_rel :
+  base:Code_id_or_name.t term ->
+  relation:Field.t term ->
+  from:Code_id_or_name.t term ->
+  _ atom
+
+val add_constructor_dep :
+  graph -> base:Code_id_or_name.t -> Field.t -> from:Code_id_or_name.t -> unit
+
+val coaccessor_rel :
+  to_:Code_id_or_name.t term ->
+  relation:Cofield.t term ->
+  base:Code_id_or_name.t term ->
+  _ atom
+
+val add_coaccessor_dep :
+  graph -> to_:Code_id_or_name.t -> Cofield.t -> base:Code_id_or_name.t -> unit
+
+val coconstructor_rel :
+  base:Code_id_or_name.t term ->
+  relation:Cofield.t term ->
+  from:Code_id_or_name.t term ->
+  _ atom
+
+val add_coconstructor_dep :
+  graph -> base:Code_id_or_name.t -> Cofield.t -> from:Code_id_or_name.t -> unit
+
+val propagate_rel :
+  if_used:Code_id_or_name.t term ->
+  to_:Code_id_or_name.t term ->
+  from:Code_id_or_name.t term ->
+  _ atom
 
 val add_propagate_dep :
   graph ->
@@ -70,19 +81,25 @@ val add_propagate_dep :
   from:Code_id_or_name.t ->
   unit
 
-val add_constructor_dep :
-  graph -> base:Code_id_or_name.t -> Field.t -> from:Code_id_or_name.t -> unit
+val any_usage_pred : Code_id_or_name.t term -> _ atom
 
-val add_accessor_dep :
-  graph -> to_:Code_id_or_name.t -> Field.t -> base:Code_id_or_name.t -> unit
+val add_use : graph -> Code_id_or_name.t -> unit
 
-val add_coaccessor_dep :
-  graph -> to_:Code_id_or_name.t -> Cofield.t -> base:Code_id_or_name.t -> unit
+val any_source_pred : Code_id_or_name.t term -> _ atom
 
-val add_coconstructor_dep :
-  graph -> base:Code_id_or_name.t -> Cofield.t -> from:Code_id_or_name.t -> unit
+val add_any_source : graph -> Code_id_or_name.t -> unit
+
+(* An entry (code_id, v) in this relation means that [v] is the "my_closure"
+   variable of the code associated to [code_id]. *)
+val code_id_my_closure_rel :
+  code_id:Code_id_or_name.t term -> my_closure:Code_id_or_name.t term -> _ atom
 
 val add_code_id_my_closure : graph -> Code_id.t -> Variable.t -> unit
+
+val create : unit -> graph
+
+val add_opaque_let_dependency :
+  graph -> to_:Bound_pattern.t -> from:Name_occurrences.t -> unit
 
 val print_iter_edges :
   print_edge:(Code_id_or_name.t * Code_id_or_name.t * string -> unit) ->
