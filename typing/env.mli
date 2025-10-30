cf8d97bb4ddf787ad934db5cbe8499f745513df3
Andrei Odintsov <forestryks1@gmail.com>
2025-10-30 16:50:28 +0000

    Update to new format

--- typing/env.mli
+++ typing/env.mli
@@ -454,6 +454,8 @@ val add_local_constraint: Path.t -> type_declaration -> t -> t
    contents of the module is accessed. *)
 val add_persistent_structure : Ident.t -> t -> t
 
+val persistent_structures_of_basenames : string list -> Misc.Stdlib.String.Set.t
+
  (* Returns the set of persistent structures found in the given
    directory. *)
 val persistent_structures_of_dir : Load_path.Dir.t -> Misc.Stdlib.String.Set.t
