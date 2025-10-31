398f87dabfa0fa31079fad7767e1fe2fadabeafb
Mark Shinwell <mshinwell@pm.me>
2025-10-31 15:17:43 +0000

    Add example

---
+++ p.ml
@@ -0,0 +1,11 @@
+let f x y p' =
+  let p = x, y in
+  let a = fst p in
+  let b = snd p in
+  let _c = fst p' in
+  let d = 42.0 in
+  let s = "foobar" in
+  let foo q = q ^ s in
+  let m = a * 2 in
+  let m' = b + 3 in
+  m, m', d, s, foo
