ca90ff3fc64f44126112cd351885721f0507c8a5
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-11-06 08:33:41 +0000

    Add a test

---
+++ oxcaml/tests/intrinsics/test_arch.ml
@@ -0,0 +1,11 @@
+external amd64 : unit -> bool @@ portable = "%arch_amd64"
+external arm64 : unit -> bool @@ portable = "%arch_arm64"
+
+
+let amd64 = amd64 ()
+let arm64 = arm64 ()
+
+let () =
+  if amd64 then print_endline "amd64";
+  if arm64 then print_endline "arm64";
+  ()
