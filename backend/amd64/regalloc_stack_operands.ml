63c0348f2c3cfdef932f8a1314553109e9299559
TheNumbat <max@thenumb.at>
2025-11-07 18:20:50 -0500

    edit

--- backend/amd64/regalloc_stack_operands.ml
+++ backend/amd64/regalloc_stack_operands.ml
@@ -171,7 +171,10 @@ let basic (map : spilled_map) (instr : Cfg.basic Cfg.instruction) =
         instr
     in
     match Array.length simd.args, simd.res with
-    | 1, (Res_none | First_arg) -> May_still_have_spilled_registers
+    | _, Res_none ->
+      (* Res_none implies one argument is always a memory location. *)
+      May_still_have_spilled_registers
+    | 1, First_arg -> May_still_have_spilled_registers
     | 1, Res { loc = res_loc; _ } ->
       let arg_mem = Simd.loc_allows_mem simd.args.(0).loc in
       let res_mem = Simd.loc_allows_mem res_loc in
@@ -181,7 +184,7 @@ let basic (map : spilled_map) (instr : Cfg.basic Cfg.instruction) =
       else if res_mem
       then may_use_stack_operand_for_result map instr ~num_args:1
       else May_still_have_spilled_registers
-    | num_args, (Res_none | First_arg) ->
+    | num_args, First_arg ->
       if Simd.loc_allows_mem simd.args.(1).loc
       then
         may_use_stack_operand_for_second_argument map instr ~num_args
