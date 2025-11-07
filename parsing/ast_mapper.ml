a3b7084729bfd243a48abf74be3c5b0a72b57916
Zachary Eisbach <zeisbach@janestreet.com>
2025-11-07 12:03:41 -0500

    fix some failing tests

--- parsing/ast_mapper.ml
+++ parsing/ast_mapper.ml
@@ -968,7 +968,8 @@ let default_mapper =
       let pjkind_desc =
         match pjkind_desc with
         | Pjk_default -> Pjk_default
-        | Pjk_abbreviation (s, nts) -> Pjk_abbreviation (s, nts)
+        | Pjk_abbreviation (s, nts) ->
+          Pjk_abbreviation (map_loc this s, List.map (map_loc this) nts)
         | Pjk_mod (t, mode_list) ->
           Pjk_mod (this.jkind_annotation this t, this.modes this mode_list)
         | Pjk_with (t, ty, modalities) ->
