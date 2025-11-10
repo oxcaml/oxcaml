2c66525699c18542c420c9d762701294ce8ec15d
Zachary Eisbach <zeisbach@janestreet.com>
2025-11-10 13:41:27 -0500

    add a test for bad scannable axis annotation

--- testsuite/tests/typing-layouts-non-trivial-values/user_errors.ml
+++ testsuite/tests/typing-layouts-non-trivial-values/user_errors.ml
@@ -3,10 +3,18 @@
  expect;
 *)
 
-type t : non_pointer value = int
+type t : value non_pointerrrrr
+[%%expect{|
+Line 1, characters 15-30:
+1 | type t : value non_pointerrrrr
+                   ^^^^^^^^^^^^^^^
+Error: Unknown scannable axis non_pointerrrrr
+|}]
+
+type t : non_pointer value
 [%%expect{|
 Line 1, characters 9-26:
-1 | type t : non_pointer value = int
+1 | type t : non_pointer value
              ^^^^^^^^^^^^^^^^^
 Error: Unknown layout non_pointer value
 |}]
@@ -22,88 +30,88 @@ Error: A type declaration's layout can be given at most once.
 
 (* CR zeisbach: this should give a warning for a redundant annotation.
    this logic will come in the _following_ PR! *)
-type t : immediate non_pointer = int
+type t : immediate non_pointer
 [%%expect{|
-type t = int
+type t : immediate
 |}]
 
 (* CR zeisbach: the following errors should only print ONCE.
    they are disabled by default because of the triple printing
    and enabled locally in this test file. once this is fixed, adjust this *)
 
-type t : value non_pointer maybe_pointer = int
+type t : value non_pointer maybe_pointer
 [%%expect{|
 Line 1, characters 27-40:
-1 | type t : value non_pointer maybe_pointer = int
+1 | type t : value non_pointer maybe_pointer
                                ^^^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 27-40:
-1 | type t : value non_pointer maybe_pointer = int
+1 | type t : value non_pointer maybe_pointer
                                ^^^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 27-40:
-1 | type t : value non_pointer maybe_pointer = int
+1 | type t : value non_pointer maybe_pointer
                                ^^^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
-type t = int
+type t
 |}]
 
-type t : value non_pointer non_pointer = int
+type t : value non_pointer non_pointer
 [%%expect{|
 Line 1, characters 27-38:
-1 | type t : value non_pointer non_pointer = int
+1 | type t : value non_pointer non_pointer
                                ^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 27-38:
-1 | type t : value non_pointer non_pointer = int
+1 | type t : value non_pointer non_pointer
                                ^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 27-38:
-1 | type t : value non_pointer non_pointer = int
+1 | type t : value non_pointer non_pointer
                                ^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
-type t = int
+type t
 |}]
 
-type t : value maybe_pointer non_pointer maybe_pointer = int
+type t : value maybe_pointer non_pointer maybe_pointer
 [%%expect{|
 Line 1, characters 29-40:
-1 | type t : value maybe_pointer non_pointer maybe_pointer = int
+1 | type t : value maybe_pointer non_pointer maybe_pointer
                                  ^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 41-54:
-1 | type t : value maybe_pointer non_pointer maybe_pointer = int
+1 | type t : value maybe_pointer non_pointer maybe_pointer
                                              ^^^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 29-40:
-1 | type t : value maybe_pointer non_pointer maybe_pointer = int
+1 | type t : value maybe_pointer non_pointer maybe_pointer
                                  ^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 41-54:
-1 | type t : value maybe_pointer non_pointer maybe_pointer = int
+1 | type t : value maybe_pointer non_pointer maybe_pointer
                                              ^^^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 29-40:
-1 | type t : value maybe_pointer non_pointer maybe_pointer = int
+1 | type t : value maybe_pointer non_pointer maybe_pointer
                                  ^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
 Line 1, characters 41-54:
-1 | type t : value maybe_pointer non_pointer maybe_pointer = int
+1 | type t : value maybe_pointer non_pointer maybe_pointer
                                              ^^^^^^^^^^^^^
 Warning 185 [duplicated-scannable-axis]: The pointerness axis has already been specified.
 
-type t = int
+type t
 |}]
 
 type t : void non_pointer
