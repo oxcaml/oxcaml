aeb8848c5aa57a98e0541e6aeac92e608dfc91ca
Zachary Eisbach <zeisbach@janestreet.com>
2025-11-12 18:03:18 -0500

    add test that is supposed to work but doesn't

--- testsuite/tests/typing-layouts-scannable/basics.ml
+++ testsuite/tests/typing-layouts-scannable/basics.ml
@@ -105,9 +105,6 @@ type succeeds : value non_pointer & value non_pointer = #{ a : t_nonptr_val; b :
 type succeeds = #{ a : t_nonptr_val; b : t_nonptr_val; }
 |}]
 
-(* CR layouts-scannable: the current approach does not play nicely with
-   mutually recursive declarations, as demonstrated by the following test: *)
-(* CR zeisbach: is this actually a problem? the examples here look ok ... *)
 type a : (value non_pointer & value) & value non_pointer
        = #{ p : #(t_nonptr_val * t_maybeptr_val); b : b }
 (* CR zeisbach: is it expected for this annotation to get ignored?
@@ -131,6 +128,25 @@ module M :
     type a : (value non_pointer & value) & value non_pointer
     and b : value non_pointer
   end
+|}]
+
+(* CR layouts-scannable: the current approach does not play nicely with
+   mutually recursive declarations, as demonstrated by the following test: *)
+type a : (value non_pointer & value) & (value non_pointer & value non_pointer)
+       = #{ p : #(t_nonptr_val * t_maybeptr_val); b : b }
+and b = #{ i : t_nonptr_val; j : t_nonptr_val }
+[%%expect{|
+Lines 1-2, characters 0-57:
+1 | type a : (value non_pointer & value) & (value non_pointer & value non_pointer)
+2 |        = #{ p : #(t_nonptr_val * t_maybeptr_val); b : b }
+Error: The layout of type "a" is
+           (value non_pointer & value maybe_pointer)
+           & (value maybe_pointer & value maybe_pointer)
+         because it is an unboxed record.
+       But the layout of type "a" must be a sublayout of
+           (value non_pointer & value maybe_pointer)
+           & (value non_pointer & value non_pointer)
+         because of the annotation on the declaration of the type a.
 |}]
 
 
