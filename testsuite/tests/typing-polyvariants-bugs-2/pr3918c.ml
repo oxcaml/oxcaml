5a068948847a15f7b97f394938109e73da06bc50
Liam Stevenson <lstevenson@janestreet.com>
2025-11-12 14:51:57 -0500

    Add comment explaining that error used to be raised

--- testsuite/tests/typing-polyvariants-bugs-2/pr3918c.ml
+++ testsuite/tests/typing-polyvariants-bugs-2/pr3918c.ml
@@ -36,3 +36,11 @@ let f (x : 'a vlist) = (x : 'b vlist)
  module = "pr3918c.ml";
  ocamlc.byte;
 *)
+
+(* This program used to unnecessarily raise the error:
+File "pr3918c.ml", line 24, characters 11-12:
+24 | let f x = (x : 'a vlist :> 'b vlist)
+                ^
+Error: This expression has type "'b Pr3918b.vlist"
+       but an expression was expected of type "'b Pr3918b.vlist"
+*)
