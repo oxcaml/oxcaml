6c07a80b85db165310daaf71be5733aae026e9f3
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-11-18 15:46:55 +0000

    Test rdtsc for arm

--- oxcaml/tests/intrinsics/test_arch.ml
+++ oxcaml/tests/intrinsics/test_arch.ml
@@ -11,3 +11,29 @@ let () =
   match Sys.arch with
   | Amd64 -> assert amd64
   | Arm64 -> assert arm64
+
+(* Architecture specific intrinsics *)
+
+(* Performance monitoring intrinsics *)
+module Amd64 = struct
+  external rdtsc : unit -> (int64[@unboxed]) = "caml_rdtsc" "caml_rdtsc_unboxed"
+  [@@noalloc] [@@builtin]
+
+end
+
+module Arm64 = struct
+  external read_cntvct_el0
+    :  unit
+    -> (int64[@unboxed])
+    = "caml_arm64_read_cntvct_el0" "caml_arm64_read_cntvct_el0_unboxed"
+  [@@noalloc][@@builtin]
+end
+
+let[@inline always] cycle_counter () =
+  match Sys.arch with
+  | Amd64 -> Amd64.rdtsc ()
+  | Arm64 -> Arm64.read_cntvct_el0 ()
+;;
+
+let () =
+  assert (not (Int64.equal (cycle_counter ()) (cycle_counter ())));
