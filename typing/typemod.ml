e49859fc82ceba9d338bd4bbb59eb0a62ad4f25a
Andrei Odintsov <forestryks1@gmail.com>
2025-11-18 13:50:04 +0000

    Fix CRs

--- typing/typemod.ml
+++ typing/typemod.ml
@@ -295,8 +295,9 @@ let initial_env ~loc ~initially_opened_module
         (open_module env m, units)
   in
   let env = List.fold_left add_units env units in
-  (* Add [basenames] skipping [initially_opened_module] check. [initially_opened_module]
-     is usually [Stdlib], which cannot come from [basenames]. *)
+  (* Add [basenames] skipping [initially_opened_module] check.
+     [initially_opened_module] is usually [Stdlib], which cannot come from
+     [basenames]. *)
   let units_from_filenames = Env.persistent_structures_of_basenames (basenames) in
   let env = add_units env units_from_filenames in
   List.fold_left open_module env open_implicit_modules
