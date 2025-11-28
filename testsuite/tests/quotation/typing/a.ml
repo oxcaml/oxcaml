15a12bb85a4bc8ee5d8f012b6cd8313fba64b6dc
Jakub Bachurski <kbachurski@gmail.com>
2025-11-28 13:22:43 +0000

    Add tests for constructor/field disambiguation in quotes

---
+++ testsuite/tests/quotation/typing/a.ml
@@ -0,0 +1,5 @@
+type vrt  = Foo | Bar
+type vrt' = Foo | Baz
+
+type rcd  = { foo: int; bar: string }
+type rcd' = { foo: int; bar: string }
---
+++ testsuite/tests/quotation/typing/inspection_cstrs_and_fields.ml
@@ -0,0 +1,21 @@
+(* TEST
+ modules = "a.ml util.ml";
+ flags = "-extension runtime_metaprogramming";
+ native;
+*)
+
+#syntax quotations on
+
+open Util
+
+let () =
+  let open A in
+  let (v : <[vrt]> expr) = <[Foo]> in
+  test <[ ignore ((fun x -> x) $v : vrt) ]>
+;;
+
+let () =
+  let open A in
+  let (r : <[rcd]> expr) = <[{ foo = 123; bar = "abc"}]> in
+  test <[ ignore (((fun x -> x) $r) : rcd) ]>
+;;
