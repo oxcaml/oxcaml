48d6d4134630af5a603fa603bd90c2cdbdd41e9a
Nailen Matschke <nmatschke@janestreet.com>
2025-12-17 22:03:55 -0500

    add #() literals

--- typing/value_rec_check.ml
+++ typing/value_rec_check.ml
@@ -197,6 +197,7 @@ let classify_expression : Typedtree.expression -> sd =
     | Texp_atomic_loc _
     | Texp_extension_constructor _
     | Texp_constant _
+    | Texp_unboxed_unit
     | Texp_src_pos ->
         Static
 
@@ -688,6 +689,8 @@ let rec expression : Typedtree.expression -> term_judg =
         expression tf.for_body << Guard;
       ]
     | Texp_constant _ ->
+      empty
+    | Texp_unboxed_unit ->
       empty
     | Texp_new (pth, _, _, _) ->
       (*
@@ -1525,6 +1528,7 @@ and is_destructuring_pattern : type k . k general_pattern -> bool =
     | Tpat_var (_, _, _, _, _) -> false
     | Tpat_alias (pat, _, _, _, _, _, _) -> is_destructuring_pattern pat
     | Tpat_constant _ -> true
+    | Tpat_unboxed_unit -> true
     | Tpat_tuple _ -> true
     | Tpat_unboxed_tuple _ -> true
     | Tpat_construct _ -> true
