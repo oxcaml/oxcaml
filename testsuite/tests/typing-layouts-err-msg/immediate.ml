d2dbd7e416b13c4c3e5571fee2ad42bdb442a20b
Ryan Tjoa <rtjoa@janestreet.com>
2025-12-24 15:12:03 -0500

    Improve error messages for layouts containing `value non_pointer` (#5128)
    * remove special case for immediate when printing layouts
    
    add logic for printing layout abbreviation hints
    
    change to inline_code; may revert
    
    promote tests and fix edge case
    
    * fix failing test
    
    * tweak printed hint message
    
    * hint -> note
    
    * format
    
    * Eliminate duplication with helper

--- testsuite/tests/typing-layouts-err-msg/immediate.ml
+++ testsuite/tests/typing-layouts-err-msg/immediate.ml
@@ -28,10 +28,11 @@ Line 3, characters 21-22:
                          ^
 Error: This expression has type "v" = "unit"
        but an expression was expected of type "'a t" = "('a : void)"
-       The layout of unit is immediate
+       The layout of unit is value non_pointer
          because it is the primitive type unit.
        But the layout of unit must be a sublayout of void
          because of the definition of t at line 1, characters 0-22.
+       Note: The layout of immediate is value non_pointer.
 |}]
 
 (* Primitive *)
@@ -44,10 +45,11 @@ Line 2, characters 23-24:
                            ^
 Error: This expression has type "int" but an expression was expected of type
          "'a t" = "('a : void)"
-       The layout of int is immediate
+       The layout of int is value non_pointer
          because it is the primitive type int.
        But the layout of int must be a sublayout of void
          because of the definition of t at line 1, characters 0-22.
+       Note: The layout of immediate is value non_pointer.
 |}];;
 
 (* Immediate_polymorphic_variant *)
@@ -60,9 +62,10 @@ Line 2, characters 29-30:
                                  ^
 Error: This expression has type "[ `A | `B ]"
        but an expression was expected of type "'a t" = "('a : void)"
-       The layout of [ `A | `B ] is immediate
+       The layout of [ `A | `B ] is value non_pointer
          because it's an enumeration variant type (all constructors are constant).
        But the layout of [ `A | `B ] must be a sublayout of void
          because of the definition of t at line 1, characters 0-22.
+       Note: The layout of immediate is value non_pointer.
 |}]
 
