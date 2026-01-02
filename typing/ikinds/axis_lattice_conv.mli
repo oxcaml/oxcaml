03c411832109149ebd4401d616c788e778913a40
julesjacobs <julesjacobs@gmail.com>
2026-01-02 12:58:31 -0500

    Add headers to ikinds files

--- typing/ikinds/axis_lattice_conv.mli
+++ typing/ikinds/axis_lattice_conv.mli
@@ -1,3 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                  Jules Jacobs, Jane Street                             *)
+(*                                                                        *)
+(*   Copyright 2025 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 val of_mod_bounds : Types.Jkind_mod_bounds.t -> Axis_lattice.t
 
 val to_mod_bounds : Axis_lattice.t -> Types.Jkind_mod_bounds.t
