beae14695310e6f5008ed6352e09f3b953b059b8
julesjacobs <julesjacobs@gmail.com>
2026-01-05 15:48:17 -0500

    testsuite: update liam ikinds debug output

--- testsuite/tests/typing-jkind-bounds/liam.ml
+++ testsuite/tests/typing-jkind-bounds/liam.ml
@@ -29,3 +29,47 @@ Error: The kind of type "t" is immutable_data
        But the kind of type "t" must be a subkind of immutable_data
          because of the annotation on the declaration of the type t.
 |}]
+
+module M : sig
+  type t : immutable_data
+end = struct
+  type t = string
+end
+[%%expect {|
+[ikinds-todo] transl_declaration initial unboxed
+[ikinds-todo] transl_declaration initial
+[ikinds-todo] update_decl_jkind initial
+[ikinds-todo] transl_declaration initial unboxed
+[ikinds-todo] transl_declaration initial
+[ikind] t/286[3]: Ty
+[ikind] string/19!: Poly(base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ string/19!.0; coeffs=[])
+[ikind] t/286[3]: stored=any, base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ string/19!.0, coeffs=[]
+[ikind-subjkind] call origin=typedecl:normalize t/286[3] (Line 4, characters 2-17) allow_any=false
+sub=any super=any  sub_poly=⊤
+super_poly=⊤
+[ikind] t/286[3]: Poly(base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ string/19!.0; coeffs=[])
+[ikind] string/19!: Poly(base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ string/19!.0; coeffs=[])
+[ikind] t/286[3]: stored=any, base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ string/19!.0, coeffs=[]
+[ikinds-todo] transl_declaration initial unboxed
+[ikinds-todo] transl_declaration initial
+[ikinds-todo] update_decl_jkind initial
+[ikinds-todo] transl_declaration initial unboxed
+[ikinds-todo] transl_declaration initial
+[ikind] t/287[4]: Ty
+[ikind] t/287[4]: stored=immutable_data, base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ t/287[4].0, coeffs=[]
+[ikind-subjkind] call origin=typedecl:normalize t/287[4] (Line 2, characters 2-25) allow_any=false
+sub=immutable_data super=immutable_data  sub_poly=[2,0,1,0,0,0,0,0,0,1,2,0,0]
+super_poly=[2,0,1,0,0,0,0,0,0,1,2,0,0]
+[ikind] string/19!: Poly(base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ string/19!.0; coeffs=[])
+[ikind-subjkind] call origin=ctype:decl Line 4, characters 2-17 allow_any=false
+sub=immediate mod dynamic with string super=immutable_data
+sub_poly=[2,0,1,0,0,0,0,0,0,1,2,0,0]
+super_poly=[2,0,1,0,0,0,0,0,0,1,2,0,0]
+[ikinds-todo] strengthen abstract path=M/288[2].t
+[ikind] M/288[2].t: Poly(base=[2,0,1,0,0,0,0,0,0,1,2,0,0] ⊓ M/288[2].t.0; coeffs=[])
+[ikind-subjkind] call origin=ctype:decl Line 2, characters 2-25 allow_any=false
+sub=immediate mod dynamic with M.t super=immutable_data
+sub_poly=[2,0,1,0,0,0,0,0,0,1,2,0,0]
+super_poly=[2,0,1,0,0,0,0,0,0,1,2,0,0]
+module M : sig type t : immutable_data end
+|}]
