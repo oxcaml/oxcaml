7812c7480d12155c3d5a8686061767ba6ae6c060
Ryan Tjoa <51928404+rtjoa@users.noreply.github.com>
2026-01-06 17:21:18 -0500

    Fix imprecise separability for `or_null` in `type_jkind` (#5178)
    * Add regression test
    
    * Track or_null in get_unboxed_type_representation, fixing type_jkind
    
    * Promote
    
    * Respond to most review
    
    * Split out apply_layout_wrapping_l
    
    * Add comment about dead code
    
    * Track previous unwrapped type in case applying or_null fails
    
    * expand on comment describing or_null backtracking
    
    * Remove redundant argument of `unbox_once`

--- testsuite/tests/typing-layouts-scannable/non_pointer.ml
+++ testsuite/tests/typing-layouts-scannable/non_pointer.ml
@@ -461,31 +461,13 @@ end
 module M : sig type t : immediate_or_null end
 |}]
 
-(* CR layouts-scannable: This test should pass! *)
 module M : sig
   type t : immediate_or_null & float64
 end = struct
   type t = #(int or_null * float#)
 end
 [%%expect{|
-Lines 3-5, characters 6-3:
-3 | ......struct
-4 |   type t = #(int or_null * float#)
-5 | end
-Error: Signature mismatch:
-       Modules do not match:
-         sig type t = #(int or_null * float#) end
-       is not included in
-         sig type t : value_or_null non_pointer & float64 end
-       Type declarations do not match:
-         type t = #(int or_null * float#)
-       is not included in
-         type t : value_or_null non_pointer & float64
-       The layout of the first is value maybe_separable maybe_null & float64
-         because it is an unboxed tuple.
-       But the layout of the first must be a sublayout of
-           value non_pointer maybe_null & float64
-         because of the definition of t at line 2, characters 2-38.
+module M : sig type t : value_or_null non_pointer & float64 end
 |}]
 
 (* modules and module inclusion *)
