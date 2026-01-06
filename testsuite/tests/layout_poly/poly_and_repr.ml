2d535ac5c2b3a4eec343b1af26c3c20c852f6696
Andrej Ivaskovic <aivaskovic@janestreet.com>
2026-01-06 12:23:23 +0000

    Address review comments: tests, variable order in Trepr matters.

--- testsuite/tests/layout_poly/poly_and_repr.ml
+++ testsuite/tests/layout_poly/poly_and_repr.ml
@@ -6,18 +6,20 @@
 exception Force_type
 ;;
 
+(** Instantiation. Currently fails, will pass in the future. *)
 let const_one : (repr_ 'a). 'a -> int =
   let f x = 1 in f
 ;;
 [%%expect {|
 exception Force_type
-Line 5, characters 17-18:
-5 |   let f x = 1 in f
+Line 6, characters 17-18:
+6 |   let f x = 1 in f
                      ^
 Error: This expression has type "'b -> int"
        but an expression was expected of type "(repr_ 'a). 'a -> int"
 |}];;
 
+(** [(repr_ 'a). t] and [t] do not unify. *)
 let const_one_with_fun : (repr_ 'a). 'a -> int =
   fun (x : any) -> 1
 ;;
@@ -29,6 +31,19 @@ Error: This expression should not be a function, the expected type is
        "(repr_ 'a). 'a -> int"
 |}];;
 
+(** Polymorphic identity, should succeed with [let]. Currently fails. *)
+let ident : (repr_ 'a). 'a -> 'a =
+  let f x = x in f
+;;
+[%%expect {|
+Line 2, characters 17-18:
+2 |   let f x = x in f
+                     ^
+Error: This expression has type "'b -> 'b"
+       but an expression was expected of type "(repr_ 'a). 'a -> 'a"
+|}];;
+
+(** Polymorphic identity, fails with [fun x -> x]. *)
 let ident : (repr_ 'a). 'a -> 'a =
   fun x -> x
 ;;
@@ -39,6 +54,8 @@ Line 2, characters 2-12:
 Error: This expression should not be a function, the expected type is
        "(repr_ 'a). 'a -> 'a"
 |}];;
+
+(** These test the interaction between Tpoly and Trepr. *)
 
 let const_fun_repr_arg : ((repr_ 'a). 'a -> unit) -> unit =
   fun _ -> ();;
@@ -72,6 +89,7 @@ let poly_and_repr_4 : ('b : value). ((repr_ 'a). 'a -> 'b -> unit) -> unit =
 val poly_and_repr_4 : ((repr_ 'a). 'a -> 'b -> unit) -> unit = <fun>
 |}]
 
+(** [Tpoly] is not an instance of [Trepr]. *)
 module Test_1 : sig
   val x : ((repr_ 'a). 'a -> unit) -> unit
 end = struct
@@ -96,6 +114,7 @@ Error: Signature mismatch:
        Type "'a -> unit" is not compatible with type "(repr_ 'a0). 'a0 -> unit"
 |}]
 
+(** [Trepr] is not an instance of [Tpoly]. *)
 module Test_2 : sig
   val x : ('a. 'a -> unit) -> unit
 end = struct
@@ -119,6 +138,106 @@ Error: Signature mismatch:
        is not compatible with the type "('a. 'a -> unit) -> unit"
        Type "(repr_ 'a). 'a -> unit" is not compatible with type "'a. 'a -> unit"
 |}]
+
+(** Is alpha-conversion performed? *)
+module Test_3 (X : sig val f : (repr_ 'a). 'a -> unit end)
+  : sig val f : (repr_ 'b). 'b -> unit end
+  = X
+[%%expect {|
+module Test_3 :
+  functor (X : sig val f : (repr_ 'a). 'a -> unit end) ->
+    sig val f : (repr_ 'b). 'b -> unit end
+|}]
+
+(** Test alpha conversion for multiple variables. *)
+module Test_3' (X : sig val f : (repr_ 'a) (repr_ 'b). 'a -> 'b end)
+  : sig val f : (repr_ 'c) (repr_ 'd). 'c -> 'd end
+  = X
+[%%expect {|
+module Test_3' :
+  functor (X : sig val f : (repr_ 'a) (repr_ 'b). 'a -> 'b end) ->
+    sig val f : (repr_ 'c) (repr_ 'd). 'c -> 'd end
+|}]
+
+(** [repr_]-bound type variable subtyping is handled reasonably. *)
+module Test_4 (X : sig val f : (repr_ 'a). 'a -> unit end)
+  : sig val f : (repr_ 'b). ('b -> 'b) -> unit end
+  = X
+[%%expect {|
+Line 3, characters 4-5:
+3 |   = X
+        ^
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : (repr_ 'a). 'a -> unit end
+       is not included in
+         sig val f : (repr_ 'b). ('b -> 'b) -> unit end
+       Values do not match:
+         val f : (repr_ 'a). 'a -> unit
+       is not included in
+         val f : (repr_ 'b). ('b -> 'b) -> unit
+       The type "(repr_ 'a). 'a -> unit" is not compatible with the type
+         "(repr_ 'b). ('b -> 'b) -> unit"
+       Type "'a" is not compatible with type "'b -> 'b"
+|}]
+
+(** Order of [repr_]-bound type variables matters. *)
+
+module Test_5 (X : sig val f : (repr_ 'a) (repr_ 'b). 'a -> 'b end)
+  : sig val f : (repr_ 'a) (repr_ 'b). 'b -> 'a end
+  = X
+[%%expect {|
+Line 5, characters 4-5:
+5 |   = X
+        ^
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : (repr_ 'a) (repr_ 'b). 'a -> 'b end
+       is not included in
+         sig val f : (repr_ 'a) (repr_ 'b). 'b -> 'a end
+       Values do not match:
+         val f : (repr_ 'a) (repr_ 'b). 'a -> 'b
+       is not included in
+         val f : (repr_ 'a) (repr_ 'b). 'b -> 'a
+       The type "(repr_ 'a) (repr_ 'b). 'a -> 'b"
+       is not compatible with the type "(repr_ 'a) (repr_ 'b). 'b -> 'a"
+       Type "'a" is not compatible with type "'b"
+|}]
+
+module Test_5' (X : sig val f : (repr_ 'a) (repr_ 'b). 'a -> 'b end)
+  : sig val f : (repr_ 'b) (repr_ 'a). 'b -> 'a end
+  = X
+[%%expect {|
+module Test_5' :
+  functor (X : sig val f : (repr_ 'a) (repr_ 'b). 'a -> 'b end) ->
+    sig val f : (repr_ 'b) (repr_ 'a). 'b -> 'a end
+|}]
+
+
+(** This should pass and generate a module coercion.
+    Currently not accepted.
+    CR layout-polymorphism aivaskovic: internal ticket XXXX. *)
+module Test_6 (X : sig val f : (repr_ 'a). 'a -> unit end)
+  : sig val f : 'a -> unit end
+  = X
+[%%expect {|
+Line 3, characters 4-5:
+3 |   = X
+        ^
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : (repr_ 'a). 'a -> unit end
+       is not included in
+         sig val f : 'a -> unit end
+       Values do not match:
+         val f : (repr_ 'a). 'a -> unit
+       is not included in
+         val f : 'a -> unit
+       The type "(repr_ 'a). 'a -> unit" is not compatible with the type
+         "'b -> unit"
+|}]
+
+(** Check that type-checking works end-to-end. *)
 
 let forced : (repr_ 'a). unit =
   raise Force_type
