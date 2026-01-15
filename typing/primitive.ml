229b5ad4ebd135a4496eda633be767ac53dc2422
TheNumbat <max@thenumb.at>
2026-01-15 13:58:53 -0500

    rev prim

--- typing/primitive.ml
+++ typing/primitive.ml
@@ -966,9 +966,8 @@ let prim_has_valid_reprs ~loc prim =
        errors dependent on the [prim_name]. *)
     ()
   | Wrong_repr ->
-    ignore loc
-    (*= raise (Error (loc,
-            Invalid_native_repr_for_primitive (prim.prim_name))) *)
+    raise (Error (loc,
+            Invalid_native_repr_for_primitive (prim.prim_name)))
 
 let prim_can_contain_layout_any prim =
   match prim.prim_name with
