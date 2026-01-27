f242e8a92a742e66de6f0ea1d67b34a60fe41d7f
Jakub Bachurski <kbachurski@gmail.com>
2026-01-27 16:48:30 +0000

    refactor misc tests and remove module pack

--- testsuite/tests/quotation/typing/inspection/miscs.ml
+++ testsuite/tests/quotation/typing/inspection/miscs.ml
@@ -1,5 +1,5 @@
 (* TEST
- modules = "util.ml";
+ modules = "miscs_types.ml util.ml";
  flags = "-extension runtime_metaprogramming";
  arch_amd64;
  native;
@@ -8,11 +8,12 @@
 #syntax quotations on
 
 open Util
+open Miscs_types
 
 (* Propagating constraints on value bindings to pattern *)
 let () = test <[
-    let x = ("abc" : string @ local) in
-    let _ : string @ local = x in ()
+    let x = ("abc" : string) in
+    let _ : string = x in ()
   ]>
 
 (* Inferring format6 on a string *)
@@ -21,18 +22,8 @@ let () =
   test <[let p = $(f <[ "e_format6 says: %s %s\n\n" ]>) in Format.printf p "abc" "xyz" ]>
 
 (* Eliminating optional arguments *)
-
 (* FIXME: For ~eval:true, we need to prevent the eta expansion introduced by
    optional argument elimination from introducing a local-returning exclave *)
 let () =
   let g = <[ fun ?(x = 0) y -> Format.printf "%d\n" (x + y) ]> in
   test ~eval:false <[ let f = $g in List.iter f [1; 2; 3] ]>
-
-(* Module packing *)
-
-(* FIXME: Support struct..end in quotes *)
-(*
-let () =
-  let m = <[ let module M = struct let f x y = x + y end in (module M) ]> in
-  test <[ let (module M) = $m in M.f 2 2 ]>
-*)
