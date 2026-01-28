3450efdb3cae3df57c6822ac576a89eb9a1edfd3
Aspen Smith <aspsmith@janestreet.com>
2026-01-28 13:08:29 -0500

    Add primitives for making preemptible fibers
    Add new `with_stack_preemptible` and `with_stack_bind_preemptible` primitives,
    which are like their non-preemptible counterparts except that they accept an
    additional `htick` argument, which will (in subsequent changes) be called
    whenever a tick occurs while this fiber or any of its children are running, and
    can (optionally) decide to preempt itself.

--- lambda/lambda.ml
+++ lambda/lambda.ml
@@ -178,6 +178,8 @@ type primitive =
   (* Context switches *)
   | Pwith_stack
   | Pwith_stack_bind
+  | Pwith_stack_preemptible
+  | Pwith_stack_bind_preemptible
   | Pperform
   | Presume
   | Preperform
@@ -2327,7 +2329,8 @@ let primitive_may_allocate : primitive -> locality_mode option = function
   | Pjoin_vec256 | Psplit_vec256 ->
     (* Aborts in bytecode, unboxed in native code *)
     None
-  | Pwith_stack | Pwith_stack_bind | Presume | Pperform | Preperform
+  | Pwith_stack | Pwith_stack_bind | Pwith_stack_preemptible
+  | Pwith_stack_bind_preemptible | Presume | Pperform | Preperform
     (* CR mshinwell: check *)
   | Ppoll ->
     Some alloc_heap
@@ -2530,7 +2533,8 @@ let primitive_can_raise prim =
   | Patomic_compare_set_field _ | Patomic_fetch_add_field  | Patomic_add_field
   | Patomic_sub_field  | Patomic_land_field | Patomic_lor_field
   | Patomic_lxor_field  | Patomic_load_field _ | Patomic_set_field _ -> false
-  | Pwith_stack | Pwith_stack_bind | Pperform | Presume
+  | Pwith_stack | Pwith_stack_bind | Pwith_stack_preemptible
+  | Pwith_stack_bind_preemptible | Pperform | Presume
   | Preperform -> true (* XXX! *)
   | Pdls_get | Ptls_get | Pdomain_index | Ppoll | Pcpu_relax
   | Preinterpret_tagged_int63_as_unboxed_int64
@@ -2936,7 +2940,8 @@ let primitive_result_layout (p : primitive) =
     layout_any_value
   | (Parray_to_iarray | Parray_of_iarray) -> layout_any_value
   | Pget_header _ -> layout_boxed_int Boxed_nativeint
-  | Pwith_stack | Pwith_stack_bind | Presume | Pperform | Preperform ->
+  | Pwith_stack | Pwith_stack_bind | Pwith_stack_preemptible
+  | Pwith_stack_bind_preemptible | Presume | Pperform | Preperform ->
     layout_any_value
   | Patomic_load_field { immediate_or_pointer = Immediate } ->
     layout_int_or_null
