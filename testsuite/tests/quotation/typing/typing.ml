6006ae4b3fbf7f54521520f6507f1da40ad7d93b
Jakub Bachurski <kbachurski@gmail.com>
2026-01-29 13:47:42 +0000

    Add tests
    Address initial review pass
    
    Add tests for the basic GADT unification cases
    
    All the tests

--- testsuite/tests/quotation/typing/typing.ml
+++ testsuite/tests/quotation/typing/typing.ml
@@ -317,7 +317,17 @@ val foo7 : 'b -> <[$('a) -> $('a)]> expr = <fun>
 
 let foo7' = (fun (type a) (type b) x -> <[fun (y : $a) -> y]>) 42;;
 [%%expect {|
-val foo7' : <[$('_a) -> $('_a)]> expr = <[fun (y : _) -> y]>
+Line 1:
+Error: Values do not match:
+         val foo7' : <[$('_a) -> $('_a)]> expr
+       is not included in
+         val foo7' : <[$('_a) -> $('_a)]> expr
+       The type "<[$('_a) -> $('_a)]> expr" is not compatible with the type
+         "<[$('_a) -> $('_a)]> expr"
+       Type "$('_a) -> $('_a)" is not compatible with type
+         "$('_a) -> $('_a)" = "$('_a) -> $('_a)"
+       Type "'_a" is not compatible with type "'_a" = "'_a"
+       The type variable "'a" occurs inside "'a"
 |}];;
 
 let foo8 (type a) (type b) x = <[fun ((p, q) : $a * $b) -> ($x, (p, q))]> <["foo"]>;;
@@ -478,150 +488,86 @@ Error: Type variable "'a" is used outside any quotations,
        Hint: Consider using "<['a]>".
 |}];;
 
+(* Eta expansion of quotes/splices *)
+
 let eta (type a) (x : a expr) : a expr = <[ $x ]>
 [%%expect {|
 val eta : 'a expr -> 'a expr = <fun>
 |}]
 
-(* Inclusion checks *)
+let eta1 (type a) = <[ fun (x : $a expr) : $a expr -> <[ $x ]> ]>
+[%%expect {|
+val eta1 : <[$('a) expr -> $('a) expr]> expr =
+  <[fun (x : _ expr) -> (<[$x]> : _ expr)]>
+|}]
 
-(*  [M1] and [M2] pass solely due to stage normalisation
-    (moving all type variables to occur at stage offset zero) *)
+let eta1' = <[ fun (type a) (x : a) : a -> $(<[ x ]>) ]>
+[%%expect {|
+val eta1' : <[$('a) -> $('a)]> expr = <[fun (type a) (x : a) -> (x : a)]>
+|}]
 
-module M1 : sig
-  val foo : 'a -> <[$('a) -> int]> expr
+(* Applicative *)
+
+let app (type a b) (f : <[$a -> $b]> expr) (x : a expr) =
+  <[ $f $x ]>
+[%%expect {|
+val app : <[$('a) -> $('b)]> expr -> 'a expr -> 'b expr = <fun>
+|}]
+
+let both (type a b) (p : a expr * b expr) : <[$a * $b]> expr =
+  let (x, y) = p in <[ $x, $y ]>
+[%%expect {|
+val both : 'a expr * 'b expr -> <[$('a) * $('b)]> expr = <fun>
+|}]
+
+(* Type equality *)
+
+module M : sig
+  type 'a t = <[$('a) -> $('a)]> expr
 end = struct
-  let foo (x: 'a) = <[fun (y : $'a) -> 1]>;;
+  type 'a t = <[$('a) -> $('a)]> expr
 end
-
 [%%expect{|
-module M1 : sig val foo : 'a -> <[$('a) -> int]> expr end
+module M : sig type 'a t = <[$('a) -> $('a)]> expr end
 |}]
 
-module M1' : sig
-  val foo : <['a]> expr -> <['a -> int]> expr
+module M : sig
+  type t = <[int]> expr
 end = struct
-  let foo (x: 'a expr) = <[fun (y : $'a) -> 1]>;;
+  type t = <[int]> expr
 end
-
 [%%expect{|
-module M1' : sig val foo : 'a expr -> <[$('a) -> int]> expr end
+module M : sig type t = <[int]> expr end
 |}]
 
-module M1'' : sig
-  val foo : 'a expr -> <[$('a) -> int]> expr
+module M : sig
+  type t = <[int]> expr
 end = struct
-  let foo (x: <['a]> expr) = <[fun (y : 'a) -> 1]>;;
+  type t = <[string]> expr
 end
-
 [%%expect{|
-module M1'' : sig val foo : 'a expr -> <[$('a) -> int]> expr end
-|}]
-
-(*  Simple functions with a type variable under a quote-splice *)
-module M2 = struct
-  let f (x : <[ 'a ]> expr) = <[ ($x, $x) ]>
-end
-
-[%%expect{|
-module M2 : sig val f : 'a expr -> <[$('a) * $('a)]> expr end
-|}]
-
-(*  Checking [M2'] does not rely on any quote-splice inverses:
-    'a will be unified with <[int]> when checking the function parameter side,
-    skipping any nontrivial reasoning.  *)
-module M2' : sig
-  val f : <[ int ]> expr -> <[ int * int ]> expr
-end = M2
-
-[%%expect{|
-module M2' : sig val f : <[int]> expr -> <[int * int]> expr end
-|}]
-
-(*  [M3] is trickier, leading to the case $'a < t *)
-
-module M3 = struct
-  let f = let (x : <[ 'a -> unit ]> expr) = <[ fun _ -> () ]> in <[ ($x, $x) ]>
-end
-
-[%%expect{|
-module M3 : sig val f : <[($('a) -> unit) * ($('a) -> unit)]> expr end
-|}]
-
-(*   $('a) = int  <=>  'a = <[int]> *)
-module M3' : sig
-  val f : <[ (int -> unit) * (int -> unit) ]> expr
-end = M3
-
-[%%expect{|
-module M3' : sig val f : <[(int -> unit) * (int -> unit)]> expr end
-|}]
-
-(*   [M3''] is a simple failure to check the error message when we end up with
-     contradicting quoted unificands *)
-(*   string = $('a) = int  <=>  <[string]> = 'a = <[int]>  <=>  string = int (error!) *)
-module M3'' : sig
-  val f : <[ (string -> unit) * (int -> unit) ]> expr
-end = M3
-
-[%%expect{|
-Line 3, characters 6-8:
-3 | end = M3
-          ^^
+Lines 3-5, characters 6-3:
+3 | ......struct
+4 |   type t = <[string]> expr
+5 | end
 Error: Signature mismatch:
        Modules do not match:
-         sig val f : <[($('a) -> unit) * ($('a) -> unit)]> expr end
+         sig type t = <[string]> expr end
        is not included in
-         sig val f : <[(string -> unit) * (int -> unit)]> expr end
-       Values do not match:
-         val f : <[($('a) -> unit) * ($('a) -> unit)]> expr
+         sig type t = <[int]> expr end
+       Type declarations do not match:
+         type t = <[string]> expr
        is not included in
-         val f : <[(string -> unit) * (int -> unit)]> expr
-       The type "<[(string -> unit) * (string -> unit)]> expr"
-       is not compatible with the type
-         "<[(string -> unit) * (int -> unit)]> expr"
-       Type "string" = "string" is not compatible with type "int"
+         type t = <[int]> expr
+       The type "<[string]> expr" is not equal to the type "<[int]> expr"
+       Type "string" is not equal to type "int"
 |}]
 
-(*  [M4] is analogous to [M3], but featuring an uninhabited type *)
-
-module M4 : sig
-  val x : <[ 'a * 'a ]> expr
+module M : sig
+  type t = <[<[int]> expr -> <[int]> expr]> expr
 end = struct
-  let x = <[ let y = Obj.magic () in (y, y) ]>
+  type t = <[<[int]> expr -> <[int]> expr]> expr
 end
-
 [%%expect{|
-module M4 : sig val x : <[$('a) * $('a)]> expr end
-|}]
-
-module M4' : sig
-  val x : <[ int * int ]> expr
-end = M4
-
-[%%expect{|
-module M4' : sig val x : <[int * int]> expr end
-|}]
-
-(* Analogously to [M3''], [M4''] checks we detect any failing unifications *)
-module M4'' : sig
-  val x : <[ int * string ]> expr
-end = M4
-
-[%%expect{|
-Line 3, characters 6-8:
-3 | end = M4
-          ^^
-Error: Signature mismatch:
-       Modules do not match:
-         sig val x : <[$('a) * $('a)]> expr end
-       is not included in
-         sig val x : <[int * string]> expr end
-       Values do not match:
-         val x : <[$('a) * $('a)]> expr
-       is not included in
-         val x : <[int * string]> expr
-       The type "<[int * int]> expr" is not compatible with the type
-         "<[int * string]> expr"
-       Type "int" = "int" is not compatible with type "string"
+module M : sig type t = <[<[int]> expr -> <[int]> expr]> expr end
 |}]
