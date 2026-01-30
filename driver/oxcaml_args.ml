9516c8ff6927a98101939f27eebb9f7463a869a3
Mark Shinwell <mshinwell@pm.me>
2026-01-30 18:17:51 +0000

    64-bit .eh_frame* support for using the dissector (#5303)
    * 64-bit .eh_frame* support for using the dissector
    
    Typos
    
    llvm-mc invocation fix
    
    Tweak dissector test partition sizes
    
    Avoid the _bases functions
    
    Add check on dissector partition sizes
    
    * Do not dissect .eh_frame
    
    * Formatting
    
    * Addressing review comments
    
    * Partition size check
    
    * Linker_script module changes
    
    (cherry picked from commit aba82ab83c1e599c97053a32e27990378cb8ec0f)

--- driver/oxcaml_args.ml
+++ driver/oxcaml_args.ml
@@ -416,6 +416,22 @@ let mk_ddissector_inputs f =
     Arg.String f,
     "<file>  Write dissector input analysis to <file>" )
 
+let mk_verify_binary_emitter f =
+  ( "-verify-binary-emitter",
+    Arg.Unit f,
+    " Verify binary emitter output matches system assembler output. Exits with \
+     error on mismatch. (ARM64 only)" )
+
+let mk_dissector_assume_lld_without_64_bit_eh_frames f =
+  ( "-dissector-assume-lld-without-64-bit-eh-frames",
+    Arg.Unit f,
+    " Assume LLD linker without 64-bit EH frame support (default)" )
+
+let mk_no_dissector_assume_lld_without_64_bit_eh_frames f =
+  ( "-no-dissector-assume-lld-without-64-bit-eh-frames",
+    Arg.Unit f,
+    " Do not assume LLD linker limitation" )
+
 let mk_gc_timings f =
   ("-dgc-timings", Arg.Unit f, "Output information about time spent in the GC")
 
@@ -1167,6 +1183,8 @@ module type Oxcaml_options = sig
   val ddissector_verbose : unit -> unit
   val ddissector_partitions : unit -> unit
   val ddissector_inputs : string -> unit
+  val dissector_assume_lld_without_64_bit_eh_frames : unit -> unit
+  val no_dissector_assume_lld_without_64_bit_eh_frames : unit -> unit
   val gc_timings : unit -> unit
   val no_mach_ir : unit -> unit
   val dllvmir : unit -> unit
@@ -1325,6 +1343,10 @@ module Make_oxcaml_options (F : Oxcaml_options) = struct
       mk_ddissector_verbose F.ddissector_verbose;
       mk_ddissector_partitions F.ddissector_partitions;
       mk_ddissector_inputs F.ddissector_inputs;
+      mk_dissector_assume_lld_without_64_bit_eh_frames
+        F.dissector_assume_lld_without_64_bit_eh_frames;
+      mk_no_dissector_assume_lld_without_64_bit_eh_frames
+        F.no_dissector_assume_lld_without_64_bit_eh_frames;
       mk_gc_timings F.gc_timings;
       mk_no_mach_ir F.no_mach_ir;
       mk_dllvmir F.dllvmir;
@@ -1433,6 +1455,13 @@ module Make_oxcaml_options (F : Oxcaml_options) = struct
       mk_cached_generic_functions_path F.cached_generic_functions_path;
     ]
 end
+
+let set_dissector_partition_size f =
+  if f <= 0.0 || f >= 2.0 then
+    raise
+      (Arg.Bad
+         "-dissector-partition-size must be greater than 0 and less than 2 GiB");
+  Clflags.dissector_partition_size := Some f
 
 module Oxcaml_options_impl = struct
   let set r () = r := Oxcaml_flags.Set true
@@ -1568,12 +1597,19 @@ module Oxcaml_options_impl = struct
 
   let internal_assembler = set' Oxcaml_flags.internal_assembler
   let dissector = set' Clflags.dissector
-  let dissector_partition_size f = Clflags.dissector_partition_size := Some f
+  let dissector_partition_size = set_dissector_partition_size
   let ddissector = set' Clflags.ddissector
   let ddissector_sizes = set' Clflags.ddissector_sizes
   let ddissector_verbose = set' Clflags.ddissector_verbose
   let ddissector_partitions = set' Clflags.ddissector_partitions
   let ddissector_inputs f = Clflags.ddissector_inputs := Some f
+
+  let dissector_assume_lld_without_64_bit_eh_frames () =
+    Oxcaml_flags.dissector_assume_lld_without_64_bit_eh_frames := true
+
+  let no_dissector_assume_lld_without_64_bit_eh_frames =
+    clear' Oxcaml_flags.dissector_assume_lld_without_64_bit_eh_frames
+
   let gc_timings = set' Oxcaml_flags.gc_timings
   let no_mach_ir () = ()
   let dllvmir () = set' Oxcaml_flags.dump_llvmir ()
@@ -2233,7 +2269,7 @@ module Extra_params = struct
     | "dissector-partition-size" -> (
         match float_of_string_opt v with
         | Some f ->
-            Clflags.dissector_partition_size := Some f;
+            set_dissector_partition_size f;
             true
         | None ->
             raise
@@ -2244,6 +2280,11 @@ module Extra_params = struct
     | "ddissector-partitions" -> set' Clflags.ddissector_partitions
     | "ddissector-inputs" ->
         Clflags.ddissector_inputs := Some v;
+        true
+    | "dissector-assume-lld-without-64-bit-eh-frames" ->
+        set' Oxcaml_flags.dissector_assume_lld_without_64_bit_eh_frames
+    | "no-dissector-assume-lld-without-64-bit-eh-frames" ->
+        Oxcaml_flags.dissector_assume_lld_without_64_bit_eh_frames := false;
         true
     | _ -> false
 end
