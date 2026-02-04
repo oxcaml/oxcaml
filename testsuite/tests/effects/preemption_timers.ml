56e11abdbfb6465a46795b3f1d2a50c48ba369c8
Aspen Smith <aspsmith@janestreet.com>
2026-02-04 16:51:41 -0500

    More tests

--- testsuite/tests/effects/preemption_timers.ml
+++ testsuite/tests/effects/preemption_timers.ml
@@ -48,7 +48,10 @@ type ('a,'b) preemptible_handler =
     effc: 'c.'c t -> (('c,'b) continuation -> 'b) option;
     tickc: unit -> tick_outcome }
 
+external enable_tick_thread : bool -> unit = "caml_enable_tick_thread"
+
 let match_with_preemptible comp arg handler =
+  enable_tick_thread true;
   let effc eff k last_fiber =
     match handler.effc eff with
     | Some f ->
@@ -58,9 +61,25 @@ let match_with_preemptible comp arg handler =
   in
   with_stack_preemptible handler.retc handler.exnc effc handler.tickc comp arg
 
-(* CR aspsmith: Remove in favor of automatically starting when we run a
-   preemptible fiber *)
-external enable_tick_thread : bool -> unit = "caml_enable_tick_thread"
+type 'a effect_handler =
+  { effc: 'b. 'b t -> (('b, 'a) continuation -> 'a) option }
+
+let try_with_preemptible
+  :  on_tick:(unit -> tick_outcome)
+  -> ('b -> 'a)
+  -> 'b
+  -> 'a effect_handler
+  -> 'a
+  =
+  fun ~on_tick comp arg handler ->
+  match_with_preemptible comp arg
+    { retc = Fun.id
+    ; exnc = raise
+    ; effc = handler.effc
+    ; tickc = on_tick
+    };
+;;
+
 
 (***********)
 
@@ -72,18 +91,16 @@ let perform_normal_effect () =
   end
   in
   let happened = ref false in
-  match_with_preemptible
+  try_with_preemptible
+    ~on_tick:(fun () -> failwith "Should not tick")
     (fun () -> perform Go)
     ()
-    { retc = Fun.id
-    ; exnc = raise
-    ; effc = (fun (type a) (eff : a Effect.t) ->
+    { effc = (fun (type a) (eff : a Effect.t) ->
         match eff with
         | Go -> Some (fun (k : (a, _) continuation) ->
           happened := true;
           continue k ())
         | _ -> None)
-    ; tickc = (fun () -> failwith "Should not tick")
     };
   assert !happened;
   print_endline "OK"
@@ -91,9 +108,8 @@ let perform_normal_effect () =
 
 let preempt_on_tick () =
   print_endline "# Preempt on tick";
-  enable_tick_thread true;
   let preempted = ref false in
-  match_with_preemptible
+  try_with_preemptible
     (fun () ->
        let start_at = Sys.time () in
        while not (!preempted) do
@@ -115,8 +131,71 @@ let preempt_on_tick () =
   print_endline "OK"
 ;;
 
+let preempt_after_two_ticks () =
+  print_endline "# Preempt after two ticks";
+  let preempted = ref false in
+  let ticks_received = ref 0 in
+  match_with_preemptible
+    (fun () ->
+       let start_at = Sys.time () in
+       while not (!preempted) do
+         if Sys.time () -. start_at > 5.
+         then failwith "Didn't get preempted after 5s!"
+       done)
+    ()
+    { retc = Fun.id
+    ; exnc = raise
+    ; effc = (fun (type a) (eff : a Effect.t) ->
+        match eff with
+        | Preemption -> Some (fun (k : (a, _) continuation) ->
+          preempted := true;
+          continue k ())
+        | _ -> None)
+    ; tickc = (fun () ->
+        incr ticks_received;
+        if !ticks_received = 2
+        then Preempt
+        else Continue)
+    };
+  assert !preempted;
+  print_endline "OK"
+;;
+
+
+let preempt_after_two_ticks () =
+  print_endline "# Preempt after two ticks";
+  let preempted = ref false in
+  let ticks_received = ref 0 in
+  match_with_preemptible
+    (fun () ->
+       let start_at = Sys.time () in
+       while not (!preempted) do
+         if Sys.time () -. start_at > 5.
+         then failwith "Didn't get preempted after 5s!"
+       done)
+    ()
+    { retc = Fun.id
+    ; exnc = raise
+    ; effc = (fun (type a) (eff : a Effect.t) ->
+        match eff with
+        | Preemption -> Some (fun (k : (a, _) continuation) ->
+          preempted := true;
+          continue k ())
+        | _ -> None)
+    ; tickc = (fun () ->
+        incr ticks_received;
+        if !ticks_received = 2
+        then Preempt
+        else Continue)
+    };
+  assert !preempted;
+  print_endline "OK"
+;;
+
 
 let () =
+  enable_tick_thread true;
   perform_normal_effect ();
-  preempt_on_tick ()
+  preempt_on_tick ();
+  preempt_after_two_ticks ()
 ;;
