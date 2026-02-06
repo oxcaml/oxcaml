fe4bb56886f7569fa1635f595c4bf3714e539e5c
Keryan Didier <keryan.didier@ocamlpro.com>
2026-02-06 15:33:43 +0100

    move mlexamples in testsuite

---
+++ testsuite/tests/flambda2/examples/offset.ml
@@ -0,0 +1,20 @@
+(* TEST
+ compile_only = "true";
+ flambda2;
+ setup-ocamlopt.byte-build-env;
+ ocamlopt.byte with dump-simplify;
+ check-fexpr-dump;
+*)
+
+let[@inline always] inline a b =
+  let r = Random.int 10 in
+  let s = Random.int 10 in
+  let rec f x = if a then r + x else 0
+  and g x y = if b then s + (x * y) else 0
+  and h x y = (if a then f (x + y) else 0) + if b then g x y else 0 in
+  h
+
+(* let foo = inline true true *)
+let bar = inline true false
+
+let foobar = inline false false
