c79b6d1beb82d90040d1a4b44e7513501d7c4372
Basile Clément <129742207+bclement-ocp@users.noreply.github.com>
2026-02-14 10:31:59 +0100

    chamelon: add subcommands (#5421)
    This patch adds subcommand support to the `chamelon` minimizer.
    Currently, a single subcommand `run` is provided, with the same
    interface as the existing `chamelon` binary. One should now use:
    
     $ chamelon run -c 'ocamlopt' file.ml
    
    instead of
    
     $ # Old syntax, do not use!
     $ chamelon -c 'ocamlopt' file.ml
    
    The old syntax is still supported with a warning nudging users to use
    `chamelon run` instead.
    
    This is the last step in preparation of the `chamelon dune` subcommand
    to automatically minimize a complete `dune` library or executable
    rather than an OCaml file (or set of OCaml files).

--- chamelon/chamelon_args.ml
+++ chamelon/chamelon_args.ml
@@ -25,12 +25,81 @@
  * DEALINGS IN THE SOFTWARE.                                                  *
  ******************************************************************************)
 
+type subcommand = Run
+
+let spec_list = ref []
+
+let anon_fun = ref ignore
+
+let subcommand = ref None
+
+let usage_msg =
+  Format.asprintf
+    "usage: %s <file1> [<file2>] ... -c \"<command>\" [-m <minimizers>] [-x \
+     <minimizers>] [-e <error>] [[-t <typing command>] | [--cmt <cmt file>]] \
+     [-i | [-o <output>]]"
+    (Filename.basename Sys.executable_name)
+
+(* `chamelon run` *)
+
+let input_files : string list ref = ref []
+
+let arg_minimizers = ref ""
+
+let exclude_minimizers = ref ""
+
+let command = ref ""
+
+let typing_command = ref ""
+
+let cmt_files : string list ref = ref []
+
+let output_file = ref ""
+
+let test = ref (-1)
+
+let run_anon_fun filename = input_files := filename :: !input_files
+
+let list_minimizers = ref false
+
+let inplace = ref false
+
+let run_spec_list =
+  [ "-c", Arg.Set_string command, "Set command";
+    "-m", Arg.Set_string arg_minimizers, "Set minimizers";
+    "-x", Arg.Set_string exclude_minimizers, "Exclude minimizers";
+    "-e", Arg.Set_string Utils.error_str, "Set error to preserve";
+    ( "-t",
+      Arg.Set_string typing_command,
+      "Set command to use to generate cmt file" );
+    "-o", Arg.Set_string output_file, "Set output file/folder";
+    "--test", Arg.Set_int test, "Run provided iteration of minimizer";
+    "-l", Arg.Set list_minimizers, "List available minimizers";
+    ( "--cmt",
+      Arg.String (fun s -> cmt_files := s :: !cmt_files),
+      "Set cmt files to use (incompatible with -t)" );
+    ( "--inplace",
+      Arg.Set inplace,
+      "Minimize file in place (incompatible with -o); in that case, command \
+       should include the input file" ) ]
+
+(* `chamelon` *)
+
+let chamelon_anon_fun s =
+  match s with
+  | "run" ->
+    subcommand := Some Run;
+    anon_fun := run_anon_fun
+  | _ ->
+    Format.eprintf "@[%a@ %s;@ %a@]@." Format.pp_print_text
+      "warning: unknown subcommand" (Filename.quote s) Format.pp_print_text
+      "parsing as file name in legacy mode.";
+    !anon_fun "run";
+    !anon_fun s
+
 let () =
-  Arg.parse_dynamic Chamelon_args.spec_list Chamelon_args.anon_fun
-    Chamelon_args.usage_msg;
-  match !Chamelon_args.subcommand with
-  | None ->
-    Format.eprintf "required COMMAND name is missing, must be 'run'.@.";
-    Arg.usage !Chamelon_args.spec_list Chamelon_args.usage_msg;
-    exit 2
-  | Some Run -> Chamelon_run.main ()
+  spec_list := run_spec_list;
+  anon_fun := chamelon_anon_fun;
+  subcommand := Some Run
+
+let anon_fun s = !anon_fun s
