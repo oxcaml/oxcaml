055f224efe58e9ad4faf9f2594a514758ac086b1
Samuel Hym <samuel@tarides.com>
2026-02-19 19:28:09 +0100

    Hide stamps in `filter.sh` for llvmize structured-name-mangling tests

--- oxcaml/tests/backend/llvmize/filter.sh
+++ oxcaml/tests/backend/llvmize/filter.sh
@@ -4,8 +4,17 @@
 sed "s/[ \t]*\n/\n"/ | \
 sed "s/[ \t]*$//" | \
 
-# hide flaky parts of identifiers
-sed -r 's/caml(.*)_[0-9]+_[0-9]+(_code)?/caml\1_HIDE_STAMP/' | \
+# hide flaky parts of identifiers, according to the name-mangling scheme
+case "$OXCAML_NAME_MANGLING" in
+  structured)
+    # process both .data and .text sections
+    sed -E -e 's/(caml[0-9A-Za-z_]*)_[0-9]+_[0-9]+/\1_HIDE_STAMP/g' \
+           -e 's/(_Caml[0-9A-Za-z_]*)_[0-9]+_[0-9]+_code/\1_HIDE_STAMP/g'
+    ;;
+  flat|*)
+    sed -r 's/caml(.*)_[0-9]+_[0-9]+(_code)?/caml\1_HIDE_STAMP/'
+    ;;
+esac | \
 # sed 's/__/./' | \
 
 # hide target triple
