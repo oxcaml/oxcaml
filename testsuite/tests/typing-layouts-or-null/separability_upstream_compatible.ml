6bec2c3e9df246f2a9cebef3631cc66b30c97d3a
Ryan Tjoa <rtjoa@janestreet.com>
2026-02-25 15:21:20 -0500

    Update sep warning test

--- testsuite/tests/typing-layouts-or-null/separability_upstream_compatible.ml
+++ testsuite/tests/typing-layouts-or-null/separability_upstream_compatible.ml
@@ -36,6 +36,22 @@ and would not be accepted by upstream OCaml.
 type packed = P : 'a non_float -> packed [@@unboxed]
 |}]
 
+type 'a non_float_external : value mod non_float external_
+[%%expect{|
+type 'a non_float_external : value mod external_ non_float
+|}]
+
+(* CR separability: this type should warn, but the incompatibility-with-upstream
+   warning doesn't account for the fact that externality crossing
+   information is not available upstream *)
+type packed_non_float_external =
+    P : 'a non_float_external -> packed_non_float_external [@@unboxed]
+[%%expect{|
+type packed_non_float_external =
+    P : 'a non_float_external -> packed_non_float_external [@@unboxed]
+|}]
+
+
 (* [: immediate] gets erased to [@@immediate] and is upstream-compatible. *)
 
 type 'a immediate : immediate
@@ -61,15 +77,23 @@ and would not be accepted by upstream OCaml.
 type exists = E : ('a : value mod non_float). 'a -> exists [@@unboxed]
 |}]
 
+
 (* Non-value layouts. *)
 
-type 'a void : void
+type 'a void_not_external : void with string
 
-type packed_void = P : 'a void -> packed_void [@@unboxed]
-
+type packed_void_not_external =
+    P : 'a void_not_external -> packed_void_not_external [@@unboxed]
 [%%expect{|
-type 'a void : void
-type packed_void = P : 'a void -> packed_void [@@unboxed]
+type 'a void_not_external : void mod non_float
+Lines 3-4, characters 0-68:
+3 | type packed_void_not_external =
+4 |     P : 'a void_not_external -> packed_void_not_external [@@unboxed]
+Warning 187 [incompatible-with-upstream]: This type relies on OxCaml's extended separability checking
+and would not be accepted by upstream OCaml.
+
+type packed_void_not_external =
+    P : 'a void_not_external -> packed_void_not_external [@@unboxed]
 |}]
 
 type exists_word = W : ('a : word) . 'a -> exists_word [@@unboxed]
