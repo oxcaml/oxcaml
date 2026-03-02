cc0951587f6dbc68aaa9b3ce6c7773db7df69f5e
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2026-03-02 12:56:14 +0100

    Gate "COULD NOT IDENTIFY" message behind a reaper debug flag

--- middle_end/flambda2/reaper/dep_solver.ml
+++ middle_end/flambda2/reaper/dep_solver.ml
@@ -2946,7 +2946,8 @@ module Rewriter = struct
             in
             match set_of_closures with
             | None ->
-              Format.eprintf "COULD NOT IDENTIFY@.";
+              if Lazy.force debug_types
+              then Format.eprintf "COULD NOT IDENTIFY@.";
               forget_type ()
             | Some set_of_closures ->
               if
