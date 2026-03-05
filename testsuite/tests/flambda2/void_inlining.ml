8547a7a80fafc1755b1a041beef7ca1867737467
Mark Shinwell <mshinwell@pm.me>
2026-03-05 18:55:48 +0000

    Fix inlining in classic mode with void parameters

---
+++ testsuite/tests/flambda2/void_inlining.ml
@@ -0,0 +1,27 @@
+(* TEST
+ compile_only = "true";
+ flags = "-extension layouts_alpha -w +a-70";
+ flambda2;
+ {
+   ocamlopt_flags = "-Oclassic";
+   setup-ocamlopt.byte-build-env;
+   ocamlopt.opt;
+ }{
+   ocamlopt_flags = "-O3";
+   setup-ocamlopt.byte-build-env;
+   ocamlopt.opt;
+ }
+*)
+
+type void : void
+
+external unsafe_set_void
+  : ('a : value).
+  'a or_null @ local -> ('a, void) idx_mut -> void -> unit
+  = "%unsafe_set_idx"
+
+let[@inline] unsafe_set_void (type a) (idx : (a, void) idx_mut) v =
+  unsafe_set_void Null idx v
+
+(* Emission of warning 55 would be a test failure. *)
+let f idx v = (unsafe_set_void [@inlined]) idx v
