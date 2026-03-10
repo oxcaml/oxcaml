3219c723eeed87f8b32767647cfe6ead1af903d5
julesjacobs <julesjacobs@gmail.com>
2026-03-10 16:29:02 -0400

    Rewrite axis lattice roundtrip test

--- oxcaml/tests/typing/axis_lattice_roundtrip_test.ml
+++ oxcaml/tests/typing/axis_lattice_roundtrip_test.ml
@@ -1,71 +1,112 @@
 open Axis_lattice
 
-let base_boxeds = [to_boxed bot; to_boxed top]
+type sample =
+  { areality : Mode.Regionality.Const.t;
+    linearity : Mode.Linearity.Const.t;
+    uniqueness : Mode.Uniqueness.Const.t;
+    portability : Mode.Portability.Const.t;
+    contention : Mode.Contention.Const.t;
+    forkable : Mode.Forkable.Const.t;
+    yielding : Mode.Yielding.Const.t;
+    statefulness : Mode.Statefulness.Const.t;
+    visibility : Mode.Visibility.Const.t;
+    staticity : Mode.Staticity.const;
+    externality : Jkind_axis.Externality.t;
+    nullability : Jkind_axis.Nullability.t;
+    separability : Jkind_axis.Separability.t
+  }
 
-let check_roundtrip label boxed =
-  let roundtripped = to_boxed (of_boxed boxed) in
-  if roundtripped <> boxed
+let sample_of_lattice x =
+  { areality = areality x;
+    linearity = linearity x;
+    uniqueness = uniqueness x;
+    portability = portability x;
+    contention = contention x;
+    forkable = forkable x;
+    yielding = yielding x;
+    statefulness = statefulness x;
+    visibility = visibility x;
+    staticity = staticity x;
+    externality = externality x;
+    nullability = nullability x;
+    separability = separability x
+  }
+
+let lattice_of_sample sample =
+  create ~areality:sample.areality ~linearity:sample.linearity
+    ~uniqueness:sample.uniqueness ~portability:sample.portability
+    ~contention:sample.contention ~forkable:sample.forkable
+    ~yielding:sample.yielding ~statefulness:sample.statefulness
+    ~visibility:sample.visibility ~staticity:sample.staticity
+    ~externality:sample.externality ~nullability:sample.nullability
+    ~separability:sample.separability
+
+let base_samples = [sample_of_lattice bot; sample_of_lattice top]
+
+let check_roundtrip label sample =
+  let roundtripped = sample_of_lattice (lattice_of_sample sample) in
+  if roundtripped <> sample
   then failwith (Format.asprintf "axis roundtrip failed: %s" label)
 
 let check_values label update values =
   List.iter
     (fun value ->
       List.iter
-        (fun base_boxed -> check_roundtrip label (update base_boxed value))
-        base_boxeds)
+        (fun base_sample -> check_roundtrip label (update base_sample value))
+        base_samples)
     values
 
 let () =
   check_values "areality"
-    (fun base_boxed areality -> { base_boxed with areality })
+    (fun sample areality -> { sample with areality })
     [ Mode.Regionality.Const.Global;
       Mode.Regionality.Const.Regional;
       Mode.Regionality.Const.Local ];
   check_values "linearity"
-    (fun base_boxed linearity -> { base_boxed with linearity })
+    (fun sample linearity -> { sample with linearity })
     [Mode.Linearity.Const.Many; Mode.Linearity.Const.Once];
   check_values "uniqueness"
-    (fun base_boxed uniqueness -> { base_boxed with uniqueness })
+    (fun sample uniqueness -> { sample with uniqueness })
     [Mode.Uniqueness.Const.Unique; Mode.Uniqueness.Const.Aliased];
   check_values "portability"
-    (fun base_boxed portability -> { base_boxed with portability })
+    (fun sample portability -> { sample with portability })
     [ Mode.Portability.Const.Portable;
       Mode.Portability.Const.Shareable;
       Mode.Portability.Const.Nonportable ];
   check_values "contention"
-    (fun base_boxed contention -> { base_boxed with contention })
+    (fun sample contention -> { sample with contention })
     [ Mode.Contention.Const.Uncontended;
       Mode.Contention.Const.Shared;
       Mode.Contention.Const.Contended ];
   check_values "forkable"
-    (fun base_boxed forkable -> { base_boxed with forkable })
+    (fun sample forkable -> { sample with forkable })
     [Mode.Forkable.Const.Forkable; Mode.Forkable.Const.Unforkable];
   check_values "yielding"
-    (fun base_boxed yielding -> { base_boxed with yielding })
+    (fun sample yielding -> { sample with yielding })
     [Mode.Yielding.Const.Unyielding; Mode.Yielding.Const.Yielding];
   check_values "statefulness"
-    (fun base_boxed statefulness -> { base_boxed with statefulness })
+    (fun sample statefulness -> { sample with statefulness })
     [ Mode.Statefulness.Const.Stateless;
       Mode.Statefulness.Const.Observing;
       Mode.Statefulness.Const.Stateful ];
   check_values "visibility"
-    (fun base_boxed visibility -> { base_boxed with visibility })
+    (fun sample visibility -> { sample with visibility })
     [ Mode.Visibility.Const.Immutable;
       Mode.Visibility.Const.Read;
       Mode.Visibility.Const.Read_write ];
   check_values "staticity"
-    (fun base_boxed staticity -> { base_boxed with staticity })
+    (fun sample staticity -> { sample with staticity })
     [Mode.Staticity.Static; Mode.Staticity.Dynamic];
   check_values "externality"
-    (fun base_boxed externality -> { base_boxed with externality })
+    (fun sample externality -> { sample with externality })
     [ Jkind_axis.Externality.External;
       Jkind_axis.Externality.External64;
       Jkind_axis.Externality.Internal ];
   check_values "nullability"
-    (fun base_boxed nullability -> { base_boxed with nullability })
+    (fun sample nullability -> { sample with nullability })
     [Jkind_axis.Nullability.Non_null; Jkind_axis.Nullability.Maybe_null];
   check_values "separability"
-    (fun base_boxed separability -> { base_boxed with separability })
+    (fun sample separability -> { sample with separability })
     [ Jkind_axis.Separability.Non_float;
       Jkind_axis.Separability.Separable;
       Jkind_axis.Separability.Maybe_separable ]
