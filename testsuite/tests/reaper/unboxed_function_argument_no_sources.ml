54c8d56cfcc83fd70f709237710cbbc71e3800a3
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2026-03-13 10:54:43 +0100

    reaper: avoid failure when function argument is unboxed but has no sources
    (cherry picked from commit 70ff87ef3a990a79746ff0a9414f19f53a1dca83)

---
+++ testsuite/tests/reaper/unboxed_function_argument_no_sources.ml
@@ -0,0 +1,25 @@
+(* TEST
+   flambda2;
+   flags += "-flambda2-reaper";
+   { native; }
+ *)
+
+let[@inline never][@local never] nope () = assert false
+
+(* [id] is here to prevent simplify from noticing this function
+   never returns. From the point of view of the reaper, this
+   function returns a value, but projecting the first field of
+   this value will always result in a variable without sources. *)
+let[@inline never][@local never] nope2 () =
+  let[@inline never][@local never] id g = g in
+  (id nope (), 0)
+
+let go x =
+  (* Here, [f] will have its argument unboxed: there is only a
+     single matching allocation, [(x, x)] below, as [u] has no
+     sources. We are in an inconsistent context when rewriting
+     the call to [f u ()], and must take care not to crash, as
+     [u] is not an unboxed variable. *)
+  let[@inline never][@local never] f (a, b) () = a + b in
+  let (u, _) = nope2 () in
+  f u () + f (x, x) ()
