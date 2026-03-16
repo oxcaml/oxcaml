7b68b21568ce5bf69b771b46493a6b34adbe18db
Xavier Clerc <xclerc@janestreet.com>
2026-03-16 18:56:39 +0000

    Remove unused IRC spilling heuristics.

--- backend/regalloc/regalloc_irc.ml
+++ backend/regalloc/regalloc_irc.ml
@@ -257,19 +257,6 @@ let select_spilling_register_using_heuristics : State.t -> SpillCosts.t -> Reg.t
     =
  fun state costs ->
   match Lazy.force Spilling_heuristics.value with
-  | Set_choose -> (
-    (* This is the "heuristics" from the IRC paper: pick any candidate, just try
-       to avoid any of the temporaries introduces for spilling. *)
-    let spill_work_list = State.spill_work_list state in
-    match
-      Reg.Set.choose_opt
-        (State.diff_all_introduced_temporaries state spill_work_list)
-    with
-    | Some reg -> reg
-    | None -> (
-      match Reg.Set.choose_opt spill_work_list with
-      | Some reg -> reg
-      | None -> fatal "spill_work_list is empty"))
   | Flat_uses | Hierarchical_uses -> (
     (* note: this assumes that `Reg.spill_cost` has been updated as needed (only
        when `rewrite` is called); the value computed here can however not be
@@ -484,9 +471,6 @@ let rec main : round:int -> State.t -> Cfg_with_infos.t -> unit =
         | None ->
           let costs =
             match Lazy.force Spilling_heuristics.value with
-            | Set_choose ->
-              (* note: `spill_cost` will not be used by the heuristics *)
-              SpillCosts.empty ()
             | Flat_uses -> SpillCosts.compute cfg_with_infos ~flat:true ()
             | Hierarchical_uses ->
               SpillCosts.compute cfg_with_infos ~flat:false ()
