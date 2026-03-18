c1184b0ebd8857c574a36b56566b86dd5e56927e
Aspen Smith <aspsmith@janestreet.com>
2026-03-18 16:58:09 -0400

    Use the API to set the tick interval

--- runtime/domain.c
+++ runtime/domain.c
@@ -2295,7 +2295,7 @@ static void domain_terminate (void)
   call_timing_hook(&caml_domain_terminated_hook);
 
   /* Reset the tick interval back to 0, since we no longer want ticks */
-  atomic_store_relaxed(&domain_self->tick_interval_usec, 0);
+  caml_domain_set_tick_interval_usec(0);
 
   while (!finished) {
     caml_finish_sweeping();
