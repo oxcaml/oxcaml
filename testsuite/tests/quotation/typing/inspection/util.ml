99b11f0c70bb242b2cdfb46bad7e5727f13e2b7d
Jakub Bachurski <kbachurski@gmail.com>
2026-03-24 18:33:02 +0000

    ticket 6725 workaround until #5657 is merged

--- testsuite/tests/quotation/typing/inspection/util.ml
+++ testsuite/tests/quotation/typing/inspection/util.ml
@@ -1,5 +1,5 @@
 #syntax quotations on
 
-let test ?(eval = true) e =
+let test ?(eval = true) e : unit =
   print_endline (Quote.string_of_expr e); print_newline ();
   if eval then [%eval: unit] e else ()
