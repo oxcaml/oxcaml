0c7654bd3ba5f38a27ca98515cc0c47a79da0c7e
Tobias Tebbi <ttebbi@janestreet.com>
2026-03-27 15:32:59 +0000

    Codegen examples for materialized bits and others

--- testsuite/tests/codegen/check_elimination.ml
+++ testsuite/tests/codegen/check_elimination.ml
@@ -76,3 +76,85 @@ arr_sum:
   movl  $1, %eax
   ret
 |}]
+
+(* CR ttebbi: The generated control flow branches three (!) times on
+   should_continue. In block 123, we can even statically know that the bit is 1.
+   Additionally, we materialise the should_continue bit. *)
+let search ~target (start : int list) =
+  let node = ref start in
+  while
+    match !node with
+    | [] -> false
+    | x :: xs ->
+      let should_continue = target < x in
+      if should_continue then node := xs;
+      should_continue
+  do () done;
+  !node
+;;
+[%%expect_asm X86_64{|
+search:
+  movq  %rax, %rdi
+  testb $1, %bl
+  je    .L116
+.L114:
+  xorl  %esi, %esi
+  movq  %rbx, %rax
+  jmp   .L131
+.L116:
+  movq  (%rbx), %rax
+  cmpq  %rax, %rdi
+  setl  %al
+  movzbq %al, %rsi
+  testq %rsi, %rsi
+  je    .L123
+  movq  8(%rbx), %rax
+  testq %rsi, %rsi
+  jne   .L129
+  jmp   .L131
+.L123:
+  movq  %rbx, %rax
+  testq %rsi, %rsi
+  je    .L131
+.L129:
+  movq  %rax, %rbx
+  testb $1, %bl
+  je    .L116
+  jmp   .L114
+.L131:
+  ret
+|}]
+
+(* CR ttebbi: The second branch is always true. *)
+let redundant_compare (x: int) = if x > 0 && x > 5 then 100 else 200
+[%%expect_asm X86_64{|
+redundant_compare:
+  cmpq  $1, %rax
+  jle   .L110
+  cmpq  $11, %rax
+  jle   .L107
+  movl  $201, %eax
+  ret
+.L107:
+  movl  $401, %eax
+  ret
+.L110:
+  movl  $401, %eax
+  ret
+|}]
+
+(* CR ttebbi: We don't learn that x is 3 in the first case. *)
+let learn_from_branch (x : int) : int =
+  match x with
+  | 3 -> x * 2
+  | _ -> 100
+[%%expect_asm X86_64{|
+learn_from_branch:
+  cmpq  $7, %rax
+  je    .L105
+  movl  $201, %eax
+  ret
+.L105:
+  leaq  -1(%rax,%rax), %rax
+  ret
+|}]
