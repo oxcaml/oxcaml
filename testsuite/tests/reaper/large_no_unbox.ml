7123f8adbcae2ca046810aa8764ea4d7a317eeed
Nathanaëlle Courant <Ekdohibs@users.noreply.github.com>
2026-03-27 19:00:18 +0100

    reaper: do not print value slot stamps when the stamps debug flag is not enabled (#5700)
    * reaper: do not print value slot stamps when the stamps debug flag is not enabled
    
    * empty commit

--- testsuite/tests/reaper/large_no_unbox.ml
+++ testsuite/tests/reaper/large_no_unbox.ml
@@ -1,7 +1,7 @@
 (* TEST
  compile_only = "true";
  flambda2;
- ocamlopt_flags = "-flambda2-reaper -reaper-max-unbox-size 3";
+ ocamlopt_flags = "-flambda2-reaper -reaper-max-unbox-size 3 -reaper-debug-flags=nostamps";
  setup-ocamlopt.byte-build-env;
  ocamlopt.byte with dump-reaper;
  check-fexpr-dump;
