7efb66572ef3859154ed8c58cea543ee88804876
Xavier Clerc <xclerc@janestreet.com>
2026-03-31 18:25:52 +0100

    Fix comment.

--- backend/regalloc/regalloc_irc.mli
+++ backend/regalloc/regalloc_irc.mli
@@ -1,6 +1,6 @@
 [@@@ocaml.warning "+a-30-40-41-42"]
 
-(* XXX/doc *)
+(* Returns `None` if the functions is deemed too complex for IRC allocation. *)
 val run : Cfg_with_infos.t -> Cfg_with_infos.t option
 
 module For_testing : sig
