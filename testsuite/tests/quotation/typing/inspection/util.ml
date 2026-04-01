aaf675a17d0635d9796587c35c3f5f7fc4c04471
Jack Rickard <jrickard@janestreet.com>
2026-04-01 10:43:54 +0100

    Simplify testsuite/tests/quotation/typing/inspection/util.ml

--- testsuite/tests/quotation/typing/inspection/util.ml
+++ testsuite/tests/quotation/typing/inspection/util.ml
@@ -1,10 +1,6 @@
 #syntax quotations on
 
-include (struct
-  let test ?(eval = true) (e : <[unit]> expr) =
-    let e = Obj.magic_many e in
-    print_endline (Quote.string_of_expr e); print_newline ();
-    if eval then Eval.eval e else ()
-end : sig
-  val test : ?eval:bool -> <[unit]> expr @ once -> unit
-end)
+let test ?(eval = true) (e : <[unit]> expr @ once) =
+  let e = Obj.magic_many e in
+  print_endline (Quote.string_of_expr e); print_newline ();
+  if eval then Eval.eval e else ()
