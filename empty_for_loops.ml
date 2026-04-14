92907d95f400293fad84a3096bc9a000eeee2893
Mark Shinwell <mshinwell@pm.me>
2026-04-14 14:34:19 +0100

    New example

--- empty_for_loops.ml
+++ empty_for_loops.ml
@@ -23,3 +23,22 @@ let l4 a b =
   for x = a to b do
     f x
   done
+
+external ( < ) : ('a : value_or_null) .
+  ('a[@local_opt]) -> ('a[@local_opt]) -> bool @@ portable = "%lessthan"
+external ( + ) : (int[@local_opt]) -> (int[@local_opt]) -> int @@ portable
+  = "%addint"
+
+let[@inline] nothing _ = ()
+
+let f lo hi =
+  let rec loop i =
+    nothing i;
+    if i < hi then loop (i + 1)
+  in
+  loop lo
+
+let g lo hi =
+  for i = lo to hi do
+    nothing i
+  done
