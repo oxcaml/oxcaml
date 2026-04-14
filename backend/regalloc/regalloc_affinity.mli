8e0cee8ab6fca4657db56705fb19cf21c92188c5
Xavier Clerc <xclerc@janestreet.com>
2026-04-14 15:55:26 +0100

    Affinity: use information from phi moves.

--- backend/regalloc/regalloc_affinity.mli
+++ backend/regalloc/regalloc_affinity.mli
@@ -3,11 +3,7 @@
 (** This module implements a basic variant of affinity: we compute the numbers
     of moves between temporaries and physical registers, and use this
     information in register allocators to try to assign a temporary to a
-    physical register with high affinity.
-
-    This could be extended to the phi moves introduced during splitting, but
-    this basic version already seems useful, in particular for the linscan and
-    greedy allocators. *)
+    physical register with high affinity. *)
 
 type affinity =
   { priority : int;
@@ -18,7 +14,7 @@ type t
 
 (** Computes the affinities for the passed CFG, i.e. for each temporary the
     number of times it moves from/to a given physical register. *)
-val compute : Cfg_with_infos.t -> t
+val compute : Cfg_with_infos.t -> (Reg.t * Reg.t) list -> t
 
 (** Returns the affinities for the passed temporary in descending order (i.e.
     from the highest to the lowest affinity), returning an empty list if the
