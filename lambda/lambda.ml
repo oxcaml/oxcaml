68417e1bbadda5c138a04ab3aac9cc126c4c0739
Jack Rickard <jrickard@janestreet.com>
2026-04-17 12:01:34 +0100

    Support sort vars in primitives

--- lambda/lambda.ml
+++ lambda/lambda.ml
@@ -2851,8 +2851,8 @@ let rec layout_of_const_sort (c : Jkind.Sort.Const.t) : layout =
     layout_unboxed_product (List.map layout_of_const_sort sorts)
   | Univar _ ->
     Misc.fatal_error "layout_of_const_sort: unexpected univar"
-  | Genvar _ ->
-    Misc.fatal_error "layout_of_const_sort: unexpected genvar"
+  | Genvar var ->
+    Psplicevar (Ident.create_sort_var (Jkind_types.Sort.Var.get_id var :> int))
 
 let layout_of_extern_repr : extern_repr -> _ = function
   | Unboxed_vector v -> layout_boxed_vector v
