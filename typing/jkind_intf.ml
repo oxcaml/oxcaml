84f462be2a9282b09a7c31c06b8884f4a3e4ef67
Andrej Ivaskovic <aivaskovic@janestreet.com>
2026-04-17 17:23:07 +0100

    Add comment around id and physical equality.

--- typing/jkind_intf.ml
+++ typing/jkind_intf.ml
@@ -175,7 +175,8 @@ module type Sort = sig
         saved to a cmi. *)
     val is_cmi_var : var -> bool
 
-    (** Extract the unique id for a [var]. *)
+    (** Extract the unique id for a [var]. Outside of a cmi, equal [id]s imply
+        physical equality of [var]s. *)
     val get_id : var -> id
 
     (** Get the number of an [id], useful for printing. These numbers get
