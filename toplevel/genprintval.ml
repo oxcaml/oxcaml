bff6424b745099f57a99495549b725472b79833b
Diana Kalinichenko <dkalinichenko@janestreet.com>
2026-04-27 16:40:44 -0400

    fix printing

--- toplevel/genprintval.ml
+++ toplevel/genprintval.ml
@@ -305,6 +305,17 @@ module Make(O : OBJ)(EVP : EVALPATH with type valu = O.t) = struct
            ~loc:Location.none Env.Construct)
         Data_types.lbl_res_type_path
 
+    and tree_of_unboxed_product_label =
+      let lbl_res_type_path lbl =
+        match Types.get_desc lbl.Data_types.lbl_res with
+        | Tconstr (p, _, _) -> p
+        | _ -> assert false
+      in
+      tree_of_qualified
+        (Env.lookup_all_labels ~use:false ~record_form:Unboxed_product
+           ~loc:Location.none Env.Construct)
+        lbl_res_type_path
+
     (* An abstract type *)
 
     let abstract_type =
@@ -746,7 +757,7 @@ module Make(O : OBJ)(EVP : EVALPATH with type valu = O.t) = struct
               (* PR#5722: print full module path only
                  for first record field *)
               let lid =
-                if first then tree_of_label env path name
+                if first then tree_of_unboxed_product_label env path name
                 else tree_of_name name
               and v =
                 match print_sort ld_sort with
