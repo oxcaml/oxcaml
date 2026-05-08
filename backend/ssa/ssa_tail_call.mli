f89161f8e3a9b16a527601aa25ee32eec8e86628
Tobias Tebbi <ttebbi@janestreet.com>
2026-05-08 22:41:35 +0100

    fix cfg_compare

--- backend/ssa/ssa_tail_call.mli
+++ backend/ssa/ssa_tail_call.mli
@@ -1,6 +1,9 @@
 (** Tail-call detection.
 
-    Replaces [Call]+[Return] sequences that satisfy the calling-convention and
-    trap-stack constraints with [Tailcall_self] or [Tailcall_func]. *)
+    Replaces [Call]+[Return] sequences with [Tailcall_self] or [Tailcall_func].
+*)
 
-val run : (module Ssa.Finished_graph) -> (module Ssa.Finished_graph)
+val run :
+  keep_unused_ops:bool ->
+  (module Ssa.Finished_graph) ->
+  (module Ssa.Finished_graph)
