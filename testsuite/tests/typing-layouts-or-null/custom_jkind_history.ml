05d0c399cd36c37a67b695b130f5e7688a435e34
julesjacobs <julesjacobs@gmail.com>
2026-05-11 13:43:26 -0400

    tests: cover custom or_null result jkind history

---
+++ testsuite/tests/typing-layouts-or-null/custom_jkind_history.ml
@@ -0,0 +1,28 @@
+(* TEST
+ include ocamlcommon;
+ expect;
+*)
+
+let () =
+  let payload =
+    Btype.newgenvar
+      (Btype.Jkind0.Builtin.value ~why:Jkind_intf.History.Row_variable)
+  in
+  let result =
+    Btype.Jkind0.for_variant_with_null_result
+      (Path.Pident (Ident.create_local "custom_or_null"))
+      payload
+  in
+  Format.printf "%a@."
+    (Format_doc.compat
+       (Jkind.format_history
+          ~intro:(fun ppf ->
+            Format_doc.fprintf ppf "The kind of custom_or_null is")
+          Env.empty))
+    result
+;;
+
+[%%expect{|
+The kind of custom_or_null is
+  because the type argument of custom_or_null has kind value_or_null.
+|}]
