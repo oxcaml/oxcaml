2f056c4e35b677fa206611d951de6bb1f4111cf2
julesjacobs <julesjacobs@gmail.com>
2026-05-13 00:23:20 -0400

    Clarify effective modality hints

--- typing/mode.ml
+++ typing/mode.ml
@@ -4828,7 +4828,7 @@ module Report = struct
     | Staticity -> C.Staticity.print ppf monadic.staticity
 
   let print_modality_axis axis ppf modality =
-    Fmt.fprintf ppf " (with modality %a)"
+    Fmt.fprintf ppf " (with modality %a in effect)"
       (Misc.Style.as_inline_code (print_modality_on axis))
       modality
 
@@ -4858,11 +4858,11 @@ module Report = struct
           ( (fun ppf _ -> print_modality_axis axis ppf modality),
             (Location.none, Unknown : pinpoint) )
         | _ :: _ :: _ ->
-          ( (fun ppf _ -> Fmt.fprintf ppf " (with some modality)"),
+          ( (fun ppf _ -> Fmt.fprintf ppf " (with some modality in effect)"),
             (Location.none, Unknown : pinpoint) )
         end
       | Modality ->
-        ( (fun ppf _ -> Fmt.fprintf ppf " (with some modality)"),
+        ( (fun ppf _ -> Fmt.fprintf ppf " (with some modality in effect)"),
           (Location.none, Unknown : pinpoint) )
 
   let modality_if_relevant_for_containing ~fixpoint obj containing pp =
