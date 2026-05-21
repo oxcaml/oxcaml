95fca0a4463ec031c46b2481b32e5a536de620f5
Ryan Tjoa <rtjoa@janestreet.com>
2026-05-21 10:07:45 -0400

    Generated atomic tests

--- testsuite/tests/records-and-block-indices/do_test_generation.ml
+++ testsuite/tests/records-and-block-indices/do_test_generation.ml
@@ -45,6 +45,38 @@
       reference = "${test_source_directory}/generated_${test_name}_bytecode_test.ml";
       check-program-output;
     }}
+  { test_name = "record_atomic_idx_access";
+    {
+      arguments = "native ${test_name}";
+      output = "${test_source_directory}/generated_${test_name}_native_test.ml.corrected";
+      run;
+      output = "${test_source_directory}/generated_${test_name}_native_test.ml.corrected";
+      reference = "${test_source_directory}/generated_${test_name}_native_test.ml";
+      check-program-output;
+    }{
+      arguments = "bytecode ${test_name}";
+      output = "${test_source_directory}/generated_${test_name}_bytecode_test.ml.corrected";
+      run;
+      output = "${test_source_directory}/generated_${test_name}_bytecode_test.ml.corrected";
+      reference = "${test_source_directory}/generated_${test_name}_bytecode_test.ml";
+      check-program-output;
+    }}
+  { test_name = "record_atomic_idx_access_local";
+    {
+      arguments = "native ${test_name}";
+      output = "${test_source_directory}/generated_${test_name}_native_test.ml.corrected";
+      run;
+      output = "${test_source_directory}/generated_${test_name}_native_test.ml.corrected";
+      reference = "${test_source_directory}/generated_${test_name}_native_test.ml";
+      check-program-output;
+    }{
+      arguments = "bytecode ${test_name}";
+      output = "${test_source_directory}/generated_${test_name}_bytecode_test.ml.corrected";
+      run;
+      output = "${test_source_directory}/generated_${test_name}_bytecode_test.ml.corrected";
+      reference = "${test_source_directory}/generated_${test_name}_bytecode_test.ml";
+      check-program-output;
+    }}
   { test_name = "record_idx_deepening";
     {
       arguments = "native ${test_name}";
