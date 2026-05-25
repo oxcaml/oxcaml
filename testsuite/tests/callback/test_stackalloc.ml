112b05e486564c9a95e7c96ac5b456cb638a2ec7
Stephen Dolan <sdolan@janestreet.com>
2026-05-25 15:27:04 +0100

    Add caml_region_{begin/end} C API

---
+++ testsuite/tests/callback/test_stackalloc.ml
@@ -0,0 +1,16 @@
+(* TEST
+ modules = "test_stackalloc_.c";
+ stack-allocation;
+ runtime5;
+ native;
+*)
+
+external local_stack_offset : unit -> int = "caml_local_stack_offset"
+
+external apply10 : (int ref @ local -> unit) -> unit = "apply10"
+
+let to_words n = n / (Sys.word_size / 8)
+
+let () =
+  apply10 (fun r ->
+    Printf.printf "%d %d\n" !r (local_stack_offset () |> to_words))
