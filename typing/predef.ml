b57cd039911c11b79758f40723f8938b33b4a4fa
Luke Maurer <lmaurer@janestreet.com>
2026-05-27 16:03:35 +0100

    Fix formatting

--- typing/predef.ml
+++ typing/predef.ml
@@ -1285,8 +1285,8 @@ let builtin_type_constrs =
     (fun t -> let id = ident_of_type_constr t in (Ident.name id, id))
     all_type_constrs
 
-(* Externally, this is the jkind of an optional argument, as opposed to the jkind of the
-   argument to the [option] type. *)
+(* Externally, this is the jkind of an optional argument, as opposed to the
+   jkind of the argument to the [option] type. *)
 (* CR-soon lmaurer: Remove this when these are once again the same. *)
 let option_argument_jkind =
   Jkind.Builtin.value_or_null ~why:Optional_argument
