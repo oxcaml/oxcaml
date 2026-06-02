82f1fdbf9845688d956fd893b691dcc9181faa55
Ryan Tjoa <rtjoa@janestreet.com>
2026-06-02 13:32:15 -0400

    Simpler incompleteness test

--- testsuite/tests/typing-layouts-box/box_incompleteness.ml
+++ testsuite/tests/typing-layouts-box/box_incompleteness.ml
@@ -12,6 +12,32 @@ type (_, _ : any) boxes = Boxes : ('a : any). ('a box, 'a) boxes
 
 (* This function typechecks *)
 let f (type a) (type (au : float64))
+      (Boxes : (a, au) boxes) (Equal : (a, float) Type.eq)
+      (au : au) : au =
+  let z : a# = au in z
+[%%expect{|
+val f :
+  'a ('au : float64). ('a, 'au) boxes -> ('a, float) Type.eq -> 'au -> 'au =
+  <fun>
+|}]
+
+(* But not if you swap the order of the first two matches *)
+let f (type a) (type (au : float64))
+      (Equal : (a, float) Type.eq) (Boxes : (a, au) boxes)
+      (au : au) : au =
+  let z : a# = au in z
+[%%expect{|
+Line 4, characters 15-17:
+4 |   let z : a# = au in z
+                   ^^
+Error: This expression has type "au" but an expression was expected of type
+         "a#" = "float#"
+|}]
+
+(* Same example, using intermediate modules *)
+
+(* This function typechecks *)
+let f (type a) (type (au : float64))
       (eq : (a, float) Type.eq) (boxes : (a, au) boxes) (au : au) : au =
   let Boxes = boxes in
   let Equal = eq in
