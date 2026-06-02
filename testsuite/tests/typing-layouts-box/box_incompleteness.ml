2231303adbc8e3dc35aa2daf6c5dcf8224872e18
Ryan Tjoa <rtjoa@janestreet.com>
2026-06-01 23:26:57 -0400

    Add box incompleteness test

---
+++ testsuite/tests/typing-layouts-box/box_incompleteness.ml
@@ -0,0 +1,45 @@
+(* TEST
+   include stdlib_upstream_compatible;
+   expect;
+*)
+
+(* Typechecking for [box] is incomplete. *)
+
+type (_, _ : any) boxes = Boxes : ('a : any). ('a box, 'a) boxes
+[%%expect{|
+type (_, _ : any) boxes = Boxes : ('a : any). ('a box, 'a) boxes
+|}]
+
+(* This function typechecks *)
+let f (type a) (type (au : float64))
+      (eq : (a, float) Type.eq) (boxes : (a, au) boxes) (au : au) : au =
+  let Boxes = boxes in
+  let Equal = eq in
+  let module M = struct
+    type b = a
+    let z : b# = au
+  end in
+  M.z
+[%%expect{|
+val f :
+  'a ('au : float64). ('a, float) Type.eq -> ('a, 'au) boxes -> 'au -> 'au =
+  <fun>
+|}]
+
+(* But not if you swap the order of the first two [let]s *)
+let f (type a) (type (au : float64))
+      (eq : (a, float) Type.eq) (boxes : (a, au) boxes) (au : au) : au =
+  let Equal = eq in
+  let Boxes = boxes in
+  let module M = struct
+    type b = a
+    let z : b# = au
+  end in
+  M.z
+[%%expect{|
+Line 7, characters 17-19:
+7 |     let z : b# = au
+                     ^^
+Error: This expression has type "au" but an expression was expected of type
+         "b#" = "float#"
+|}]
