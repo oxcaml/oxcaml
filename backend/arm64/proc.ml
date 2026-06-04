8fd6bbb2cef5aea0e71b4ab5fcdd450b78e56047
Simon Spies <sspies@janestreet.com>
2026-06-04 12:01:58 +0100

    Revert "Revert "[5.4] Preserve the frame pointer register at noalloc C calls"" (#6183)
    Revert "Revert "[5.4] Preserve the frame pointer register at noalloc C calls"…"
    
    This reverts commit 66e2f59fada7a8317c56fad3ed30c0a2c244ef66.

--- backend/arm64/proc.ml
+++ backend/arm64/proc.ml
@@ -232,9 +232,9 @@ let domainstate_ptr_dwarf_register_number = 28
 (* Registers destroyed by operations *)
 
 let destroyed_at_c_noalloc_call =
-  (* x19-x28, d8-d15 preserved *)
+  (* x20-x28, d8-d15 preserved *)
   let int_regs_destroyed_at_c_noalloc_call =
-    Regs.[| X0;X1;X2;X3;X4;X5;X6;X7;X8;X9;X10;X11;X12;X13;X14;X15 |]
+    Regs.[| X0;X1;X2;X3;X4;X5;X6;X7;X8;X9;X10;X11;X12;X13;X14;X15;X19 |]
   in
   let float_regs_destroyed_at_c_noalloc_call =
     Regs.[|D0;D1;D2;D3;D4;D5;D6;D7;
