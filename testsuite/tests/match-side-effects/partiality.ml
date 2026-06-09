61e4555394e7dacba1f5d6ce8afdee7128bb1b3d
Ryan Tjoa <rtjoa@janestreet.com>
2026-06-09 15:03:37 -0400

    Test promotions

--- testsuite/tests/match-side-effects/partiality.ml
+++ testsuite/tests/match-side-effects/partiality.ml
@@ -88,7 +88,9 @@ let f r =
   (f/310 =
      (function {nlocal = 0} r/311 : int
        (region
-         (let (*match*/313 = (makelocalblock 0 (*) r/311))
+         (let
+           (*match*/313 =[value<(consts (0)) (non_consts ([0: *]))>]
+              (makelocalblock 0 (*) r/311))
            (catch
              (if *match*/313
                (let (*match*/315 =o? (field_mut 0 (field_imm 0 *match*/313)))
@@ -122,8 +124,14 @@ let test = function
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
   (test/321 =
-     (function {nlocal = 0} param/324 : int
-       (if param/324 (field_imm 0 (field_imm 0 param/324)) 0)))
+     (function {nlocal = 0}
+       param/324[value<
+                  (consts (0))
+                   (non_consts ([0:
+                                 value<
+                                  (consts ()) (non_consts ([1: value<int>]
+                                   [0: value<int>]))>]))>]
+       : int (if param/324 (field_imm 0 (field_imm 0 param/324)) 0)))
   (apply (field_imm 1 (global Toploop!)) "test" test/321))
 val test : int t option -> int = <fun>
 |}]
@@ -171,7 +179,12 @@ type _ t = Int : int -> int t | Bool : bool -> bool t
      (function {nlocal = 0} n/338? : int
        (region
          (let
-           (*match*/341 =
+           (*match*/341 =[value<
+                           (consts (0))
+                            (non_consts ([0:
+                                          value<
+                                           (consts ())
+                                            (non_consts ([0: *, *]))>]))>]
               (makelocalblock 0 (value<
                                   (consts ())
                                    (non_consts ([0: *,
