3730336abe0e82babf9aae1ca102e7fc0b0424bf
Liam Stevenson <lstevenson@janestreet.com>
2026-06-18 09:55:36 -0400

    Rename version from 5.4.0+ox1 to 5.4.0-ox1

--- external/merlin/src/ocaml/typing/magic_numbers.ml
+++ external/merlin/src/ocaml/typing/magic_numbers.ml
@@ -85,7 +85,7 @@ module Cmi = struct
     | "Caml1999I577" -> Some "5.2.0minus-37"
     | "Caml1999I578" -> Some "5.2.0minus-39"
     | "Caml1999I579" -> Some "5.2.0minus-40"
-    | "Caml1999I580" -> Some "5.4.0+ox1"
+    | "Caml1999I580" -> Some "5.4.0-ox1"
     | _ -> None
 
   let () = assert (to_version_opt Config.cmi_magic_number <> None)
