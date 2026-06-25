fe474e3b99769f4afcb80d692546d3f89d787705
Ryan Tjoa <51928404+rtjoa@users.noreply.github.com>
2026-06-25 03:24:38 -0600

    Add `int64_u`, `int32_u`, and `nativeint_u` to predef (#6321)
    Add the following types to the predef.
    ```ocaml
    type int64_u = int64#
    type int32_u = int32#
    type nativeint_u = nativeint#
    ```
    
    In the future, these types will be abstract, and we will delete
    `int64#`, `int32#`, and `nativeint#`, as they are not "truly" unboxed
    versions of `int64`, `int32`, and `nativeint` (which are custom blocks
    and thus should not have unboxed versions).
    
    **For review.** In `predef.ml`, we rename the parameter `tconstr` so it
    doesn't shadow the top-level function of the same name.

--- lambda/translquote.ml
+++ lambda/translquote.ml
@@ -2483,6 +2483,9 @@ let type_for_path loc env = function
         | "nativeint#" -> Identifier.Type.unboxed_nativeint
         | "int32#" -> Identifier.Type.unboxed_int32
         | "int64#" -> Identifier.Type.unboxed_int64
+        | "nativeint_u" -> Identifier.Type.unboxed_nativeint
+        | "int32_u" -> Identifier.Type.unboxed_int32
+        | "int64_u" -> Identifier.Type.unboxed_int64
         | "int8x16" -> Identifier.Type.int8x16
         | "int16x8" -> Identifier.Type.int16x8
         | "int32x4" -> Identifier.Type.int32x4
