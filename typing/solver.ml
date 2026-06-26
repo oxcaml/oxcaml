c906695cd3d90bc39102b967683ec629c8a498f2
Aina Linn Georges <ageorges@janestreet.com>
2026-06-26 17:11:09 +0100

    Bug fix: check_level and check_level_var

--- typing/solver.ml
+++ typing/solver.ml
@@ -582,9 +582,9 @@ module Solver_mono (H : Hint) (C : Lattices_mono) = struct
     | Amode _ -> true
     | Amodevar mv -> check_level_morphvar mv i
     | Amodemeet (_, _, mvs) ->
-      VarMap.fold (fun _ mv acc -> acc && check_level_morphvar mv i) mvs true
+      VarMap.fold (fun _ mv acc -> acc || check_level_morphvar mv i) mvs true
     | Amodejoin (_, _, mvs) ->
-      VarMap.fold (fun _ mv acc -> acc && check_level_morphvar mv i) mvs true
+      VarMap.fold (fun _ mv acc -> acc || check_level_morphvar mv i) mvs true
 
   let check_level_var : type a l r. (a, l * r) mode -> int -> bool =
    fun m i ->
@@ -592,15 +592,9 @@ module Solver_mono (H : Hint) (C : Lattices_mono) = struct
     | Amode _ -> false
     | Amodevar mv -> check_level_morphvar mv i
     | Amodemeet (_, _, mvs) ->
-      VarMap.fold
-        (fun _ mv acc -> acc && check_level_morphvar mv i)
-        mvs
-        (not (VarMap.is_empty mvs))
+      VarMap.fold (fun _ mv acc -> acc || check_level_morphvar mv i) mvs false
     | Amodejoin (_, _, mvs) ->
-      VarMap.fold
-        (fun _ mv acc -> acc && check_level_morphvar mv i)
-        mvs
-        (not (VarMap.is_empty mvs))
+      VarMap.fold (fun _ mv acc -> acc || check_level_morphvar mv i) mvs false
 
   type var_iterator =
     { iter : 'a. 'a C.obj -> ('a, allowed * allowed) mode -> unit }
