6669247f2c6b82a7b0af9afb03f98a8affa6eb82
Liam Stevenson <lstevenson@janestreet.com>
2026-06-26 17:30:28 -0400

    Fail gracefully if .merlin doesn't exist

--- external/merlin/src/utils/dot_merlin_finder.ml
+++ external/merlin/src/utils/dot_merlin_finder.ml
@@ -2,22 +2,24 @@ let file_exists path = Sys.file_exists path && not (Sys.is_directory path)
 
 let split_path path =
   let dir = Filename.dirname path in
-  let abs_dir = Unix.realpath dir in
-  let file_rel_to_dir = Filename.basename path in
-  let rec loop ~abs_dir ~file_rel_to_dir =
-    if file_exists (Filename.concat abs_dir "jenga.conf") then
-      Some (~workspace_root:abs_dir, ~rel_path:file_rel_to_dir)
-    else
-      let parent = Filename.dirname abs_dir in
-      if String.equal parent abs_dir then
-        (* We've reached the root directory. *)
-        None
+  if Sys.file_exists dir then
+    let abs_dir = Unix.realpath dir in
+    let file_rel_to_dir = Filename.basename path in
+    let rec loop ~abs_dir ~file_rel_to_dir =
+      if file_exists (Filename.concat abs_dir "jenga.conf") then
+        Some (~workspace_root:abs_dir, ~rel_path:file_rel_to_dir)
       else
-        loop ~abs_dir:parent
-          ~file_rel_to_dir:
-            (Filename.concat (Filename.basename abs_dir) file_rel_to_dir)
-  in
-  loop ~abs_dir ~file_rel_to_dir
+        let parent = Filename.dirname abs_dir in
+        if String.equal parent abs_dir then
+          (* We've reached the root directory. *)
+          None
+        else
+          loop ~abs_dir:parent
+            ~file_rel_to_dir:
+              (Filename.concat (Filename.basename abs_dir) file_rel_to_dir)
+    in
+    loop ~abs_dir ~file_rel_to_dir
+  else None
 
 let find_in_src_or_build_dir src_path =
   match split_path src_path with
