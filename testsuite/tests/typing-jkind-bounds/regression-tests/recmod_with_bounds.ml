5da28f11a90a179d843fa9e27d7d7160fb04d0bd
Ryan Tjoa <rtjoa@janestreet.com>
2026-07-01 14:06:41 -0400

    Promote regression test

--- testsuite/tests/typing-jkind-bounds/regression-tests/recmod_with_bounds.ml
+++ testsuite/tests/typing-jkind-bounds/regression-tests/recmod_with_bounds.ml
@@ -20,13 +20,6 @@ end =
   T2
 [%%expect{|
 type 'a require_value
-Line 9, characters 17-25:
-9 |   type t = { x : int T1.t require_value }
-                     ^^^^^^^^
-Error: This type "int T1.t" should be an instance of type "('a : value)"
-       The layout of int T1.t is any
-         because the compiler failed to deduce its exact kind
-         due to with-bound checking limitations.
-       But the layout of int T1.t must be a value layout
-         because of the definition of require_value at line 1, characters 0-31.
+module rec T1 : sig type 'a t : value mod portable with 'a end
+and T2 : sig type t = { x : int T1.t require_value; } end
 |}]
