dd75d817e3ef4c25436aff29b7d311eeae8ed26f
Ryan Tjoa <rtjoa@janestreet.com>
2026-07-01 18:37:36 -0400

    Add another bad case

--- testsuite/tests/typing-jkind-bounds/regression-tests/recmod_with_bounds_ikinds.ml
+++ testsuite/tests/typing-jkind-bounds/regression-tests/recmod_with_bounds_ikinds.ml
@@ -22,3 +22,23 @@ type 'a require_value
 module rec T1 : sig type 'a t : value mod portable with 'a end
 and T2 : sig type t = { x : int T1.t require_value; } end
 |}]
+
+(* CR layouts v2.8: fix this. Internal ticket 5127 *)
+module rec T1 : sig
+  type 'a t = A
+end =
+  T1
+
+and T2 : sig
+  type t : value mod portable = { x : int T1.t }
+end =
+  T2
+[%%expect{|
+Line 7, characters 2-48:
+7 |   type t : value mod portable = { x : int T1.t }
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: The kind of type "t" is immutable_data with int T1.t
+         because it's a boxed record type.
+       But the kind of type "t" must be a subkind of value mod portable
+         because of the annotation on the declaration of the type t.
+|}]
