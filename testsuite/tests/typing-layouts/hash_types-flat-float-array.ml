585def02418f007ca432bfd1ce35adbb424dd8fb
Ryan Tjoa <51928404+rtjoa@users.noreply.github.com>
2026-07-03 00:53:04 -0400

    Add `array#` and `iarray#` (#6361)
    Add unboxed versions of `array` and `iarray`, with kind `any`.
    ```ocaml
    type 'a t : any = 'a array#
    ```
    
    This is a step towards changing the types of indices to use `box`.

--- testsuite/tests/typing-layouts/hash_types-flat-float-array.ml
+++ testsuite/tests/typing-layouts/hash_types-flat-float-array.ml
@@ -5,7 +5,7 @@
 *)
 
 (* See hash_types.ml for the bulk of the tests. This file collects the
-   case that depends on the flat float array optimization being enabled. *)
+   cases that depend on the flat float array optimization being enabled. *)
 
 type 'a t = { i : 'a }
 and bad = P : 'a t# -> bad [@@unboxed]
@@ -13,6 +13,22 @@ and bad = P : 'a t# -> bad [@@unboxed]
 Line 2, characters 0-38:
 2 | and bad = P : 'a t# -> bad [@@unboxed]
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This type cannot be unboxed because
+       it might contain both float and non-float values,
+       depending on the instantiation of the existential variable "'a".
+       You should annotate it with "[@@ocaml.boxed]".
+|}]
+
+(* The negative counterpart of the [array#]/[iarray#] separability tests in
+   hash_types.ml: an abstract any-layout type's parameter gets the worst-case
+   mode, so the existential is rejected. *)
+type ('a : value) abstr : any
+type bad = B : 'a abstr -> bad [@@unboxed]
+[%%expect{|
+type 'a abstr : any
+Line 2, characters 0-42:
+2 | type bad = B : 'a abstr -> bad [@@unboxed]
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: This type cannot be unboxed because
        it might contain both float and non-float values,
        depending on the instantiation of the existential variable "'a".
