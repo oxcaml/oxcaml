be4b70e2319a804a7e6744a80b699d3e54b4bca1
TheNumbat <max@thenumb.at>
2026-07-13 11:32:30 -0400

    delete context

--- stdlib/dynamic.ml
+++ stdlib/dynamic.ml
@@ -14,14 +14,12 @@
 
 type 'a t : value mod everything with 'a @@ contended portable
 
-external make' : inherit_:bool -> 'a t @@ portable = "caml_dynamic_make"
+external make : unit -> 'a t @@ portable = "caml_dynamic_make"
 external get : 'a t -> 'a or_null @ contended portable @@ portable =
   "caml_dynamic_get" [@@noalloc]
 external push : 'a t -> 'a @ contended portable -> unit @@ portable =
   "caml_dynamic_push"
 external pop : 'a t -> unit @@ portable = "caml_dynamic_pop" [@@noalloc]
-
-let[@inline] make () = make' ~inherit_:false
 
 let[@inline] with_temporarily t v ~f = exclave_
   push t v;
@@ -31,12 +29,3 @@ let[@inline] with_temporarily t v ~f = exclave_
     let bt = Printexc.get_raw_backtrace () in
     pop t;
     Printexc.raise_with_backtrace exn bt
-
-module Context = struct
-  type nonrec 'a t = 'a t
-
-  let[@inline] make () = make' ~inherit_:true
-
-  let get = get
-  let with_temporarily = with_temporarily
-end
--- stdlib/dynamic.mli
+++ stdlib/dynamic.mli
@@ -47,34 +47,3 @@ val with_temporarily : ('b : value_or_null).
   'a @ contended portable ->
   f:(unit -> 'b @ local unique once) @ local once ->
   'b @ local unique once
-
-module Context : sig
-  (** ['a Context.t] is like ['a Dynamic.t], but fibers permanently inherit the
-      value bound at the time of their creation. *)
-  type 'a t : value mod everything with 'a @@ contended portable
-
-  (** [make ()] creates a new contextual variable. *)
-  val make : unit -> 'a t
-
-  (** [get t] retrieves the current value of the contextual variable [t] in the
-      current fiber.
-
-      Within a call to [with_temporarily], this returns the {i local} value
-      of the contextual variable. Outside of a call to [with_temporarily], this
-      returns [Null].
-
-      Because the contextual binding may be accessed concurrently, its contents
-      are contended. *)
-  val get : 'a t -> 'a or_null @ contended portable
-
-  (** [with_temporarily t v ~f] invokes [f] in a context where [t] is bound to
-      [v], then restores [t] to its previous state.
-
-      Because the contextual binding may be accessed concurrently, the new value
-      must be portable. *)
-  val with_temporarily : ('b : value_or_null).
-    'a t ->
-    'a @ contended portable ->
-    f:(unit -> 'b @ local unique once) @ local once ->
-    'b @ local unique once
-end
