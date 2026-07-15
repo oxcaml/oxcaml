3d15cd562ed2c05bf15f6d68e35ca9cbf4f92437
Jakub Bachurski <kbachurski@gmail.com>
2026-07-15 17:23:45 +0100

    test outputs

--- testsuite/tests/quotation/typing/moding_quote_local.ml
+++ testsuite/tests/quotation/typing/moding_quote_local.ml
@@ -15,16 +15,23 @@ val f : 'a @ local quote_local -> 'a @ local quote_local = <fun>
 
 let set r (x @ quote_local) = r := x
 [%%expect{|
-val set : 'a ref @ quote_local -> 'a @ quote_local -> unit @ quote_local =
-  <fun>
+Line 1, characters 35-36:
+1 | let set r (x @ quote_local) = r := x
+                                       ^
+Error: This value is "quote_local" but is expected to be "quote_global".
 |}];;
 
 exception Extrusion of <[int]> expr
 let extrude (x @ quote_local) = try raise (Extrusion x) with Extrusion x -> x
 [%%expect{|
 exception Extrusion of <[int]> expr
-val extrude : <[int]> expr @ quote_local -> <[int]> expr @ quote_local =
-  <fun>
+Line 2, characters 53-54:
+2 | let extrude (x @ quote_local) = try raise (Extrusion x) with Extrusion x -> x
+                                                         ^
+Error: This value is "quote_local"
+       but is expected to be "quote_global"
+         because it is contained (via constructor "Extrusion") in the value at line 2, characters 42-55
+         which is expected to be "quote_global".
 |}];;
 
 (* Lower-stage local captures are <[local]> *)
@@ -44,8 +51,13 @@ Error: The value "x" is "local" to the parent region
 (* infers that [g] accepts a <[local]> *)
 let f g = <[ fun (x @ local) -> $(g <[ x ]>) ]>
 [%%expect{|
-Uncaught exception: Failure("CR quoted-modes jbachurski: Locality_full; Locality_splice_and_full")
-
+Line 1, characters 39-40:
+1 | let f g = <[ fun (x @ local) -> $(g <[ x ]>) ]>
+                                           ^
+Error: The value "x" is "local" to the parent region
+       but is expected to be "global"
+         because it is used inside the quoted expression at line 1, characters 36-43
+         which is expected to be "global".
 |}];;
 
 (* Higher-stage <[local]> captures are local *)
@@ -53,8 +65,7 @@ Uncaught exception: Failure("CR quoted-modes jbachurski: Locality_full; Locality
 (* error: <[x]> is quote_local *)
 let f (x @ quote_local) = <[ fun () -> $x ]>
 [%%expect{|
-Uncaught exception: Failure("CR quoted-modes jbachurski: Locality_full; Locality_splice_and_full")
-
+val f : 'a expr @ quote_local -> <[unit -> $('a)]> expr @ quote_local = <fun>
 |}];;
 
 (* Splice captures cross quoted modes like <[local]> *)
@@ -62,13 +73,14 @@ Uncaught exception: Failure("CR quoted-modes jbachurski: Locality_full; Locality
 (* <[local]> crossed, only [g] is captured by the quote *)
 let f (g : unit -> 'a expr @ quote_local) = <[ $(g ()) ]>
 [%%expect{|
-Uncaught exception: Failure("CR quoted-modes jbachurski: Locality_full; Locality_splice_and_full")
-
+val f :
+  ('a : any). (unit -> 'a expr @ quote_local) -> 'a expr @ quote_local once =
+  <fun>
 |}];;
 
 (* [x] is captured by the quote and its mode is taken *)
 let f (x @ quote_local) = <[ $x ]>
 [%%expect{|
-Uncaught exception: Failure("CR quoted-modes jbachurski: Locality_full; Locality_splice_and_full")
-
+val f : ('a : any). 'a expr @ quote_local -> 'a expr @ quote_local once =
+  <fun>
 |}];;
