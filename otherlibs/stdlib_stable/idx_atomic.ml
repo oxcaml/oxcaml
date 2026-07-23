83585c291360cf0c8c516fa9c42436773ba9ee81
Joe Kerrigan <jkerrigan@janestreet.com>
2026-07-23 18:19:57 -0400

    Update documentation

--- otherlibs/stdlib_stable/idx_atomic.ml
+++ otherlibs/stdlib_stable/idx_atomic.ml
@@ -4,7 +4,7 @@
 (*                                                                        *)
 (*                 Joe Kerrigan, Jane Street, New York                    *)
 (*                                                                        *)
-(*   Copyright 2024 Jane Street Group LLC                                 *)
+(*   Copyright 2026 Jane Street Group LLC                                 *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
