ee4a28ee6b1c6f323327522830ffc5754a1babee
Stephen Dolan <sdolan@janestreet.com>
2026-07-27 13:49:21 +0100

    Add new -thin-library option
    Libraries built with -thin-library have much smaller .a and .cma files by
    referencing rather than including the corresponding .o and .cmo files.
    The .o and .cmo files must still be available when the final link occurs.

--- utils/clflags.ml
+++ utils/clflags.ml
@@ -214,6 +214,7 @@ let dump_linear = ref false             (* -dlinear *)
 let keep_startup_file = ref false       (* -dstartup *)
 let debug_ocaml = ref false             (* -debug-ocaml *)
 let llvm_backend = ref false            (* -llvm-backend *)
+let thin_library = ref false            (* -thin-library *)
 let ikinds = ref true                   (* -no-ikinds *)
 let ikinds_debug = ref false            (* -ikinds-debug *)
 let default_timings_precision  = 3
