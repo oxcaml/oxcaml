db5403ed18739218947a18fa60dc361505854162
Ryan Tjoa <rtjoa@janestreet.com>
2026-07-29 15:18:17 -0400

    Remove assume_inline_records_are_blocks

--- utils/breadcrumbs.ml
+++ utils/breadcrumbs.ml
@@ -1,3 +1,1 @@
 let until_kind_of = ()
-
-let assume_inline_records_are_blocks = ()
--- utils/breadcrumbs.mli
+++ utils/breadcrumbs.mli
@@ -15,15 +15,3 @@
     Some users of this breadcrumb may really care about [layout_of], but we
     expect these features to arrive ~together. *)
 val until_kind_of : unit
-
-(** Currently, all inline records are blocks. We plan to break this once we have
-    all-void inline records (internal ticket 4654) plus [inherit] (internal
-    ticket 7361).
-    {[
-      type t = A of { inherit u : unit# }
-    ]}
-
-    Tangentially, note that all-void constructors can be immediates with the
-    attribute [@immediate_all_void_constructor]. We plan to replace uses of
-    these constructors with [inherit] as well. *)
-val assume_inline_records_are_blocks : unit
