4b4dbaaa4e0f2da96982ba34a85b0f516ece77e9
James Rayman <209187487+jra4@users.noreply.github.com>
2026-07-29 15:21:57 -0400

    Add tests

---
+++ testsuite/tests/typing-zero-alloc/primitive_coercion_zero_alloc_all_in_mli.ml
@@ -0,0 +1,12 @@
+(* TEST
+ setup-ocamlopt.opt-build-env;
+ ocamlopt_opt_exit_status = "2";
+ ocamlopt.opt;
+ check-ocamlopt.opt-output;
+*)
+
+external add8 : int8# -> int8# -> int8# = "%int8#_add"
+
+external add32 : int32 -> int32 -> int32 = "%int32_add" (* Should error *)
+
+external add64 : int64 -> int64 -> int64 = "%int64_add"
