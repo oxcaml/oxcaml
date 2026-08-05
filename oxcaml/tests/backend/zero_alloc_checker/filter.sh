04ac52266499cd78930ce45968cd2b66dadbc62b
Nick Barnes <Nick.Barnes@pobox.com>
2026-08-05 10:27:26 +0100

    Promote test output. This test now seds out the ./ prefix that differs between dune versions, but does not replace the __ in symbols with a . (which was there for compatibility with outdated systems).

--- oxcaml/tests/backend/zero_alloc_checker/filter.sh
+++ oxcaml/tests/backend/zero_alloc_checker/filter.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sed -r 's/caml(.*)_[0-9]+_[0-9]+_code?/caml\1_HIDE_STAMP/'
+# The second sed strips the "./" prefix that dune >= 3.24 adds to source paths.
+sed -r 's/caml(.*)_[0-9]+_[0-9]+_code?/caml\1_HIDE_STAMP/' \
+| \
+    sed -e 's|\([("[/ ]\)\./|\1|g' -e 's|^\./||'
