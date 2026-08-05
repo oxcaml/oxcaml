829182833a71e9196a477ead9295eefedd9b17f1
Basile Clement <basile.clement@ocamlpro.com>
2026-08-05 09:11:28 +0200

    Store delayed renaming alongside name abstraction
    This gives us delayed renamings for result types and a little bit less
    delays to go through in expressions, since we no longer store delayed
    renamings for terminator expressions such as `apply_cont` or `switch`.
    
    The main motivation for this change, however, is that it enables us to
    implement an efficient "import renamed version of" in renamings and use
    it inside the name abstraction.

--- middle_end/flambda2/nominal/name_abstraction.ml
+++ middle_end/flambda2/nominal/name_abstraction.ml
@@ -22,25 +22,77 @@ module type Term = sig
   include Contains_ids.S with type t := t
 end
 
-type ('bindable, 'term) t = 'bindable * 'term
+(* This signature ensures absolutely that the insides of an expression cannot be
+   accessed before any necessary delayed renaming has been applied. *)
+module With_delayed_renaming : sig
+  type ('bindable, 'term) t
+
+  val create : 'bindable -> 'term -> ('bindable, 'term) t
+
+  val apply_renaming :
+    ('bindable, 'term) t -> Renaming.t -> ('bindable, 'term) t
+
+  val descr :
+    (module Bindable.S with type t = 'bindable) ->
+    ('bindable, 'term) t ->
+    apply_renaming_to_term:('term -> Renaming.t -> 'term) ->
+    'bindable * 'term
+end = struct
+  type ('bindable, 'term) t =
+    { mutable bindable : 'bindable;
+      mutable term : 'term;
+      mutable delayed_renaming : Renaming.t
+    }
+
+  let create bindable term =
+    { bindable; term; delayed_renaming = Renaming.empty }
+
+  let apply_renaming t renaming =
+    let delayed_renaming =
+      Renaming.compose ~second:renaming ~first:t.delayed_renaming
+    in
+    { t with delayed_renaming }
+
+  let[@inline always] descr (type bindable)
+      (module Bindable : Bindable.S with type t = bindable) t
+      ~apply_renaming_to_term =
+    if Renaming.is_identity t.delayed_renaming
+    then t.bindable, t.term
+    else
+      let bindable = Bindable.apply_renaming t.bindable t.delayed_renaming in
+      let term = apply_renaming_to_term t.term t.delayed_renaming in
+      t.bindable <- bindable;
+      t.term <- term;
+      t.delayed_renaming <- Renaming.empty;
+      bindable, term
+end
+
+type ('bindable, 'term) t = ('bindable, 'term) With_delayed_renaming.t
+
+let descr = With_delayed_renaming.descr
 
 let[@inline always] pattern_match (type bindable)
-    (module Bindable : Bindable.S with type t = bindable) (bindable, term)
+    (module Bindable : Bindable.S with type t = bindable) t
     ~apply_renaming_to_term ~f =
+  let bindable, term = descr (module Bindable) t ~apply_renaming_to_term in
   let fresh_bindable = Bindable.rename bindable in
   let renaming = Bindable.renaming bindable ~guaranteed_fresh:fresh_bindable in
   let fresh_term = apply_renaming_to_term term renaming in
   f fresh_bindable fresh_term
 
-let[@inline always] pattern_match_for_printing bindable_impl
-    ((bindable, term) as t) ~apply_renaming_to_term ~f =
+let[@inline always] pattern_match_for_printing bindable_impl t
+    ~apply_renaming_to_term ~f =
   if Flambda_features.freshen_when_printing ()
   then pattern_match bindable_impl t ~apply_renaming_to_term ~f
-  else f bindable term
+  else
+    let bindable, term = descr bindable_impl t ~apply_renaming_to_term in
+    f bindable term
 
 let[@inline always] pattern_match_pair (type bindable)
-    (module Bindable : Bindable.S with type t = bindable) (bindable0, term0)
-    (bindable1, term1) ~apply_renaming_to_term ~f =
+    (module Bindable : Bindable.S with type t = bindable) t0 t1
+    ~apply_renaming_to_term ~f =
+  let bindable0, term0 = descr (module Bindable) t0 ~apply_renaming_to_term in
+  let bindable1, term1 = descr (module Bindable) t1 ~apply_renaming_to_term in
   let fresh_bindable = Bindable.rename bindable0 in
   let renaming0 =
     Bindable.renaming bindable0 ~guaranteed_fresh:fresh_bindable
@@ -53,24 +105,21 @@ let[@inline always] pattern_match_pair (type bindable)
   f fresh_bindable fresh_term0 fresh_term1
 
 let apply_renaming (type bindable)
-    (module Bindable : Bindable.S with type t = bindable)
-    ((bindable, term) as t) renaming ~apply_renaming_to_term =
-  if Renaming.is_identity renaming
-  then t
-  else
-    let bindable' = Bindable.apply_renaming bindable renaming in
-    let term' = apply_renaming_to_term term renaming in
-    if bindable == bindable' && term == term' then t else bindable', term'
+    (module _ : Bindable.S with type t = bindable) t renaming
+    ~apply_renaming_to_term:_ =
+  With_delayed_renaming.apply_renaming t renaming
 
 let free_names (type bindable)
-    (module Bindable : Bindable.S with type t = bindable) (bindable, term)
-    ~free_names_of_term =
+    (module Bindable : Bindable.S with type t = bindable) t ~free_names_of_term
+    ~apply_renaming_to_term =
+  let bindable, term = descr (module Bindable) t ~apply_renaming_to_term in
   Name_occurrences.diff (free_names_of_term term)
     ~without:(Bindable.free_names bindable)
 
 let ids_for_export (type bindable)
-    (module Bindable : Bindable.S with type t = bindable) (bindable, term)
-    ~ids_for_export_of_term =
+    (module Bindable : Bindable.S with type t = bindable) t
+    ~ids_for_export_of_term ~apply_renaming_to_term =
+  let bindable, term = descr (module Bindable) t ~apply_renaming_to_term in
   Ids_for_export.union
     (Bindable.ids_for_export bindable)
     (ids_for_export_of_term term)
@@ -78,24 +127,22 @@ let ids_for_export (type bindable)
 module Make (Bindable : Bindable.S) (Term : Term) = struct
   type nonrec t = (Bindable.t, Term.t) t
 
-  let create bindable term = bindable, term
+  let create bindable term = With_delayed_renaming.create bindable term
 
-  let[@inline always] pattern_match (bindable, term) ~f =
+  let[@inline always] pattern_match t ~f =
     pattern_match
       (module Bindable)
-      (bindable, term) ~f ~apply_renaming_to_term:Term.apply_renaming
+      t ~f ~apply_renaming_to_term:Term.apply_renaming
 
-  let[@inline always] pattern_match_for_printing (bindable, term) ~f =
+  let[@inline always] pattern_match_for_printing t ~f =
     pattern_match_for_printing
       (module Bindable)
-      (bindable, term) ~f ~apply_renaming_to_term:Term.apply_renaming
+      t ~f ~apply_renaming_to_term:Term.apply_renaming
 
-  let[@inline always] pattern_match_pair (bindable0, term0) (bindable1, term1)
-      ~f =
+  let[@inline always] pattern_match_pair t0 t1 ~f =
     pattern_match_pair
       (module Bindable)
-      (bindable0, term0) (bindable1, term1) ~f
-      ~apply_renaming_to_term:Term.apply_renaming
+      t0 t1 ~f ~apply_renaming_to_term:Term.apply_renaming
 
   let apply_renaming t renaming =
     apply_renaming
@@ -109,5 +156,6 @@ module Make (Bindable : Bindable.S) (Term : Term) = struct
     ids_for_export
       (module Bindable)
       t ~ids_for_export_of_term:Term.ids_for_export
+      ~apply_renaming_to_term:Term.apply_renaming
 end
 [@@inline always]
