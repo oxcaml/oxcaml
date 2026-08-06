377d339b4a52507819e590a3e6d0738e4efafdff
Miriam Vellacott <mvellacott@janestreet.com>
2026-08-06 12:10:55 +0100

    add plumbing for the reaper solve entrypoint

--- driver/boot_ocamlopt.ml
+++ driver/boot_ocamlopt.ml
@@ -2,4 +2,5 @@ let () =
   exit (Optmaindriver.main (module Unix : Compiler_owee.Unix_intf.S) Sys.argv
     Format.err_formatter
     ~flambda2:Flambda2.lambda_to_cmm
-    ~reaped_flambda2_to_cmm:Flambda2.reaped_flambda2_to_cmm)
+    ~reaped_flambda2_to_cmm:Flambda2.reaped_flambda2_to_cmm
+    ~reaper_lto_solve:Flambda2.reaper_lto_solve)
