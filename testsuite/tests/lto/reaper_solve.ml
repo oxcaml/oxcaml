8b0af8243ded31e3dd0745575dab10a788de8a95
Miriam Vellacott <mvellacott@janestreet.com>
2026-08-06 12:13:43 +0100

    add tests for reaper solve plumbing

---
+++ testsuite/tests/lto/reaper_solve.ml
@@ -0,0 +1,28 @@
+(* TEST
+ modules = "reaper_solve_dependency.ml";
+ flambda2;
+ setup-ocamlopt.opt-build-env;
+
+ flags = "-flambda2-reaper -support-lto";
+ compile_only = "true";
+ ocamlopt.opt;
+
+ file = "reaper_solve_dependency.cmr";
+ file-exists;
+
+ file = "reaper_solve.cmr";
+ file-exists;
+
+ compile_only = "false";
+ flags = "-reaper-solve reaper_solve_dependency.cmr reaper_solve.cmr";
+ last_flags = "-o reaper_solve.ltosol";
+ all_modules = "";
+ ocamlopt.opt;
+
+ check-ocamlopt.opt-output;
+*)
+
+(* CR mvellacott: currently reaper_lto_solve just prints the file names it was
+   given, when the solve stage is implemented this test will need updating. *)
+
+let () = assert (Reaper_solve_dependency.used 41 = 42)
