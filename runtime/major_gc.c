38a58bf7f755e1814d512de1d7c60fba76d21252
Stephen Dolan <sdolan@janestreet.com>
2026-08-11 09:26:41 +0100

    Fix a GC pacing bug with large off-heap data
    When there's a lot of off-heap garbage created (relative to the GC heap size),
    a huge amount of GC work is incurred. Sometimes, this work is more than needed
    to complete a whole GC cycle and persists for future cycles, causing too much
    GC to be done overall.
    
    This patch adds a clamp: any work incurred before marking starts is necessarily
    done before marking starts the next time, so any work that remains outstanding
    longer than that can be cancelled.

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -815,6 +815,17 @@ static intnat Sweepwork_markwork(intnat mark_work)
 static atomic_uintnat total_work_incurred;
 static atomic_uintnat total_work_completed;
 
+/* We store the total work incurred when marking last started.
+
+   This is used to avoid some pathological behaviour where far more
+   work is incurred than can be done in a cycle, which can happen with
+   off-heap allocations that vastly exceed the heap size. We know that
+   any work incurred before marking last started is done by the time
+   marking next starts, so we can cancel it if it remains outstanding.
+
+   (Nonatomic: only accessed during STW) */
+static uintnat total_work_incurred_at_mark_start;
+
 static inline intnat max2 (intnat a, intnat b)
 {
   if (a > b){
@@ -1686,6 +1697,13 @@ void caml_mark_roots_stw (int participant_count, caml_domain_state** barrier_par
        orphaned in [Phase_sweep_main], so they must come from last
        cycle, so will have status [UNMARKED] now). */
     adopt_orphaned_work (caml_global_heap_state.UNMARKED);
+
+    /* Any work incurred before the start of the marking phase prior
+       to this one is definitely done by now. */
+    uintnat definitely_done = total_work_incurred_at_mark_start;
+    if (total_work_completed < definitely_done)
+      total_work_completed = definitely_done;
+    total_work_incurred_at_mark_start = total_work_incurred;
   }
 
   caml_domain_state* domain = Caml_state;
