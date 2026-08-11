17700beda0388f2a4c6e7cc788573eb1fc485173
Shixin Song <shsong@janestreet.com>
2026-08-11 13:48:54 -0400

    Fix format

--- typing/mode.ml
+++ typing/mode.ml
@@ -4818,8 +4818,7 @@ module Report = struct
     | Allocation -> Some (print_article_noun Vowel "allocation")
     | Zero_alloc_func_appl ->
       Some
-        (print_article_noun Consonant
-           "return value of a zero_alloc function")
+        (print_article_noun Consonant "return value of a zero_alloc function")
     | Class -> Some (print_article_noun Consonant "class")
     | Object -> Some (print_article_noun Vowel "object")
     | Loop -> Some (print_article_noun Consonant "loop")
