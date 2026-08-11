990acc1b751d777b865b537baba7d8668fe7d131
Miriam Vellacott <mvellacott@janestreet.com>
2026-08-11 16:30:00 +0100

    squashed commits from #6731

--- middle_end/flambda2/identifiers/int_ids.mli
+++ middle_end/flambda2/identifiers/int_ids.mli
@@ -114,6 +114,13 @@ module Variable : sig
 
   val create : ?user_visible:unit -> string -> Flambda_kind.t -> t
 
+  val create_in_compilation_unit :
+    compilation_unit:Compilation_unit.t ->
+    ?user_visible:unit ->
+    string ->
+    Flambda_kind.t ->
+    t
+
   val compilation_unit : t -> Compilation_unit.t
 
   val name : t -> string
