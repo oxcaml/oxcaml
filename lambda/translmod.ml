4f5f839405b717dfc3d91f0783a86cf0629be2f9
James Rayman <209187487+jra4@users.noreply.github.com>
2026-08-13 21:10:32 +0000

    Allow coercions from `external`s to `[@@zero_alloc] val`s (#6651)
    This is done by adding a `pc_zero_alloc : Zero_alloc.check option` field
    to `Tcoerce_primitive`, representing the zero_alloc check that must be
    performed to ensure the coercion is valid. This field is used in
    `transl_primitive` similar to how `transl_function` handles zero_alloc.

--- lambda/translmod.ml
+++ lambda/translmod.ml
@@ -145,11 +145,12 @@ let rec apply_coercion loc strict restr arg =
           attributes = Lambda.default_param_attribute; mode = alloc_heap}]
         [carg] yielding cc_res
   | Tcoerce_primitive { pc_desc; pc_env; pc_type; pc_poly_mode; pc_poly_sort;
-                        pc_yielding } ->
+                        pc_yielding; pc_zero_alloc_check } ->
       Translprim.transl_primitive loc pc_desc pc_env pc_type
         ~poly_mode:pc_poly_mode
         ~poly_sort:pc_poly_sort
         ~yielding:pc_yielding
+        ~zero_alloc_check:pc_zero_alloc_check
         None
   | Tcoerce_alias (env, path, cc) ->
       let lam = transl_module_path loc env path in
@@ -741,6 +742,7 @@ and transl_structure ~scopes loc
                             ~poly_mode:p.pc_poly_mode
                             ~poly_sort:p.pc_poly_sort
                             ~yielding:p.pc_yielding
+                            ~zero_alloc_check:p.pc_zero_alloc_check
                             None
                       | _ -> apply_coercion loc Strict cc (get_field pos))
                     pos_cc_list, loc)
--- lambda/translprim.ml
+++ lambda/translprim.ml
@@ -2445,7 +2445,8 @@ let transl_primitive_common loc ~poly_mode ~poly_sort
   else
     prim
 
-let transl_primitive loc p env ty ~poly_mode ~poly_sort ~yielding path =
+let transl_primitive
+      loc p env ty ~poly_mode ~poly_sort ~yielding ~zero_alloc_check path =
   let prim =
     transl_primitive_common loc
       ~poly_mode ~poly_sort Rc_normal p env ty path []
@@ -2541,12 +2542,20 @@ let transl_primitive loc p env ty ~poly_mode ~poly_sort ~yielding path =
        | Alloc_heap :: args -> count_nlocal args
        | (Alloc_local :: _) as args -> List.length args
      in
-     let nlocal = count_nlocal (List.map to_locality p.prim_native_repr_args)
-     in lfunction
+     let nlocal = count_nlocal (List.map to_locality p.prim_native_repr_args) in
+     let zero_alloc : Lambda.zero_alloc_attribute =
+       match (zero_alloc_check : Zero_alloc.check option) with
+       | None -> Default_zero_alloc
+       | Some { strict; opt; arity = _; loc; custom_error_msg } ->
+         if Builtin_attributes.is_zero_alloc_check_enabled ~opt
+         then Check { strict; loc; custom_error_msg }
+         else Default_zero_alloc
+     in
+     lfunction
        ~kind:(Curried {nlocal})
        ~params
        ~return
-       ~attr:default_stub_attribute
+       ~attr:{ default_stub_attribute with zero_alloc }
        ~loc
        ~body
        ~mode:alloc_heap
