1bf65e1386857ab05457b420fcd71335ba5f93f1
David Allsopp <david.allsopp@metastack.com>
2026-08-13 16:39:16 +0100

    Update Merlin

--- external/merlin/src/ocaml/utils/clflags.mli
+++ external/merlin/src/ocaml/utils/clflags.mli
@@ -41,23 +41,6 @@ val zero_alloc_check     : Zero_alloc_annotations.Check.t ref
 val zero_alloc_assert    : Zero_alloc_annotations.Assert.t ref
 val infer_with_bounds    : bool ref
 val kind_verbosity : int ref
-<<<<<<< Merlin:stedolan/thin-libbie
-||||||| Compiler:last-imported
-val emit_optimized_probes : bool ref
-val supports_optimized_probes : bool
-
-val llvm_backend : bool ref
-
-=======
-val emit_optimized_probes : bool ref
-val supports_optimized_probes : bool
-
-val llvm_backend : bool ref
-
-(* Build .a files as thin archives (option declared in Oxcaml_args). *)
-val thin_library : bool ref
-
->>>>>>> Compiler:HEAD
 (* Dedicated flag for the ikinds kind checker (enabled by default). *)
 val ikinds : bool ref
 
