6d5770c524c3772c8199dc0718fa6c318035e61e
Gavin Gray <gavinleroy@gmail.com>
2026-08-16 17:01:40 -0400

    Add module-type implementations query

--- external/merlin/src/frontend/query_protocol.ml
+++ external/merlin/src/frontend/query_protocol.ml
@@ -218,6 +218,47 @@ module Locate_types_result = struct
   type t = Success of Tree.t | Invalid_context
 end
 
+module Module_type_impls = struct
+  type impl_kind = Whole_unit | Ascription_sites
+
+  type impl_site = { impl_loc : Location.t; impl_kind : impl_kind }
+
+  type target = Own_interface | Modtype of string
+
+  type check_kind = Ascription | Argument | Package | Interface
+
+  type implementation =
+    { target : target;
+      target_loc : Location.t option;
+      instance : string option;
+      site : impl_site;
+      check : check_kind option;
+      check_site : Location.t option
+    }
+
+  type status = Complete | Partial | Unavailable
+
+  type reason =
+    | No_index_files
+    | Channel_absent
+    | Reader_problem of string
+    | Omission of { family : string option; reason : string }
+    | Unresolved_implementation of
+        { target : string;
+          witness : string;
+          implementation : string;
+          site : string option
+        }
+    | Unresolved_check_site of
+        { target : string; witness : string; site : string }
+
+  type response =
+    { status : status;
+      reasons : reason list;
+      implementations : implementation list
+    }
+end
+
 type _ t =
   | Type_expr (* *) : string * Msource.position -> string t
   | Stack_or_heap_enclosing (* *) :
@@ -307,6 +348,7 @@ type _ t =
       Msource.position * Msource.position
       -> (Location.t * string) t
   | Holes (* *) : (Location.t * string) list t
+  | Module_type_impls (* *) : Module_type_impls.response t
   | Construct :
       Msource.position * [ `None | `Local ] option * int option
       -> (Location.t * string list) t
