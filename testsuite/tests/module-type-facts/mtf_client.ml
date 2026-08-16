f455909010062df2c35788f31472da8922475388
Gavin Gray <gavinleroy@gmail.com>
2026-08-16 17:01:40 -0400

    Make module type facts reproducible and minimize tests

--- testsuite/tests/module-type-facts/mtf_client.ml
+++ testsuite/tests/module-type-facts/mtf_client.ml
@@ -1,21 +1,12 @@
-(* A unit whose facts mention the module types and the functor parameter of
-   [Mtf_aux], all loaded from [mtf_aux.cmi]. *)
+(* A unit whose expectations are all loaded from [mtf_aux.cmi]: the module
+   type of an ascription and the module type expected of the parameter of a
+   functor of another unit. *)
 
 module A = struct type t = int end
 
 module FA = Mtf_aux.F (A)
 
 module M : Mtf_aux.S = struct type t = char end
-
-module N : Mtf_aux.Wrapper.Inner = struct type t = int end
-
-module type Local = Mtf_aux.S with type t = int
-
-module P : Local = struct type t = int end
-
-module Q : Mtf_aux.T = struct type t = int end
-
-module R : Mtf_aux.S with Mtf_aux.M = Mtf_aux.M
 
 (* The members of [Mtf_aux.Container] are declared in [mtf_aux.mli], so the
    member checks of this ascription cross the unit boundary; they must not be
