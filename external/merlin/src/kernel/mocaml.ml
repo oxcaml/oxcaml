8264b5f2879c24278c66cd180f39e106da3af607
Gavin Gray <gavinleroy@gmail.com>
2026-08-18 15:00:38 -0400

    updated index readers

--- external/merlin/src/kernel/mocaml.ml
+++ external/merlin/src/kernel/mocaml.ml
@@ -155,4 +155,5 @@ let clear_caches () =
 let flush_caches ?older_than () =
   Cmi_cache.flush ?older_than ();
   Cmt_cache.flush ?older_than ();
-  Merlin_index_format.Index_cache.flush ?older_than ()
+  Merlin_index_format.Index_cache.flush ?older_than ();
+  Merlin_index_format.Module_facts_reader.flush ?older_than ()
