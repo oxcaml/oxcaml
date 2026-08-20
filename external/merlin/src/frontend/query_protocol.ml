8b2ab44bccd85257164215a064c1caf47426607d
Gavin Gray <gavinleroy@gmail.com>
2026-08-20 18:19:42 -0400

    Serve structured mode diagnostics from Merlin

--- external/merlin/src/frontend/query_protocol.ml
+++ external/merlin/src/frontend/query_protocol.ml
@@ -220,6 +220,9 @@ end
 
 type _ t =
   | Type_expr (* *) : string * Msource.position -> string t
+  | Structured_errors (* *) :
+      { pronouns : bool }
+      -> Structured_diagnostic.t list t
   | Stack_or_heap_enclosing (* *) :
       Msource.position * bool * int option
       -> (Location.t * [ `String of string | `Index of int ]) list t
