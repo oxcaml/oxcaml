5a8c6f3b1325070a76bcbd86f967d2abbcdc4bdd
Robin Ricard <rricard@janestreet.com>
2026-08-20 21:49:10 +0100

    js_of_ocaml: fix wasm string builtins and preserve leading BOMs
    Fix runtime bugs:
    - jsstring.wat: fix wasm string builtins
    - Preserve leading BOMs when converting OCaml strings to JavaScript strings
    
    (cherry picked from commit 112978855461a6a6865940a659e5f7b71d2c027c)

--- external/js_of_ocaml/runtime/wasm/runtime.js
+++ external/js_of_ocaml/runtime/wasm/runtime.js
@@ -165,7 +165,7 @@
       : f;
   }
 
-  const decoder = new TextDecoder("utf-8", { ignoreBOM: 1 });
+  const decoder = new TextDecoder("utf-8", { ignoreBOM: true });
   const encoder = new TextEncoder();
 
   // View on the scratch area used to convert between OCaml and JavaScript
