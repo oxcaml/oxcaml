dd60144c1b274542c550b0f591ca7c5b3916d305
Gavin Gray <gavinleroy@gmail.com>
2026-08-21 12:46:08 -0400

    Expose mode documentation helpers

--- external/merlin/src/analysis/syntax_doc.mli
+++ external/merlin/src/analysis/syntax_doc.mli
@@ -1,3 +1,9 @@
+val get_mode_doc :
+  Mode.Alloc.atom -> Query_protocol.Syntax_doc_result.t option
+
+val get_modality_doc :
+  Mode.Modality.atom -> Query_protocol.Syntax_doc_result.t option
+
 val get_syntax_doc :
   Lexing.position ->
   (Env.t * Browse_raw.node) list ->
