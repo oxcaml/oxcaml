966b49aa85db337318a39c86d838c918c9aae369
Gavin Gray <gavinleroy@gmail.com>
2026-08-24 14:06:43 -0400

    Handle functorized artifacts in module facts

--- typing/typemod.ml
+++ typing/typemod.ml
@@ -4628,7 +4628,8 @@ let module_implementation_facts ~unit_interface ~argument_interface
     compilation_unit (annots : Cmt_format.binary_annots)
     declaration_dependencies =
   match annots with
-  | Cmt_format.Packed _ | Partial_implementation _ | Partial_interface _ ->
+  | Cmt_format.Packed _ | Partial_implementation _ | Partial_interface _
+  | Functorize ->
     None
   | Interface signature ->
     let argument_interface =
@@ -5015,9 +5016,9 @@ let functorize_signature ~params ~modules : Types.signature =
         let sign, _ = swg.sign in
         let param_type = Mty_signature (Subst.Lazy.force_signature sign) in
         Mty_functor
-          (Named (Some param_id, param_type, With_locality.legacy),
-           body,
-           With_locality.legacy))
+          ( Named (Some param_id, param_type, None, With_locality.legacy),
+            body,
+            With_locality.legacy ))
       params body
   in
   let body =
