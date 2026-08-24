b182299a40c921f38610e73c26403720da870541
Gavin Gray <gavinleroy@gmail.com>
2026-08-24 12:42:19 -0400

    Automated commit: Import compiler changes from 259bbb860f40a4b0e5260894a35cd2fdccfa5389

--- external/merlin/src/ocaml/typing/typemode.mli
+++ external/merlin/src/ocaml/typing/typemode.mli
@@ -95,6 +95,7 @@ val close_implied_mod_bounds : Jkind.Mod_bounds.t -> Jkind.Mod_bounds.t
 val untransl_mod_bounds : ?verbose:bool -> Jkind.Mod_bounds.t -> Parsetree.modes
 
 val idx_expected_modalities : mut:bool -> Mode.Modality.Const.t
+<<<<<<< Merlin:ggray/ete/dev
 
 (* Merlin-only: This is exposed for Merlin (for syntax_doc.ml). *)
 
@@ -103,3 +104,24 @@ module Modifier_axis_pair : sig
 
   val of_string : string -> t
 end
+||||||| Compiler:last-imported
+=======
+
+type 'ax annot_type =
+  | Modifier : 'a Jkind_axis.Axis.t annot_type
+  | Mode : 'a Mode.Alloc.Axis.t annot_type
+  | Modality : 'a Mode.Modality.Axis.t annot_type
+
+type forbidden_modality_kind = Global_and_unique
+
+type error =
+  | Forbidden_modality : 'a annot_type * forbidden_modality_kind -> error
+  | Duplicated_axis : 'a annot_type * 'a -> error
+  | Unrecognized_modifier : 'a annot_type * string -> error
+
+exception Error of Location.t * error
+
+val print_annot_type : Format_doc.formatter -> _ annot_type -> unit
+
+val print_annot_axis : 'a annot_type -> Format_doc.formatter -> 'a -> unit
+>>>>>>> Compiler:HEAD
