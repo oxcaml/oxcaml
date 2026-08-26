898df732aaf509cf1281ebba1097a8e8b5554b0a
Stephen Dolan <sdolan@janestreet.com>
2026-08-26 10:52:09 +0000

    Allow Parsetree compiler hooks to edit the AST (#6826)
    The Compiler_hooks module doesn't allow hooks to edit the IRs, because
    to do so will in general break internal invariants. It's fine for
    parsetree hooks, though, since that's how ppxs work.

--- driver/compile.ml
+++ driver/compile.ml
@@ -28,8 +28,8 @@ let interface ~source_file ~output_prefix =
   in
   with_info ~dump_ext:"cmi" unit_info @@ fun info ->
   Compile_common.interface
-    ~hook_parse_tree:(fun _ -> ())
-    ~hook_typed_tree:(fun _ -> ())
+    ~hook_parse_tree:Fun.id
+    ~hook_typed_tree:ignore
     info
 
 (** Bytecode compilation backend for .ml files. *)
@@ -139,8 +139,8 @@ let implementation_aux ~start_from ~source_file ~output_prefix
       emit_bytecode info bytecode
     in
     Compile_common.implementation
-      ~hook_parse_tree:(fun _ -> ())
-      ~hook_typed_tree:(fun _ -> ())
+      ~hook_parse_tree:Fun.id
+      ~hook_typed_tree:ignore
       info ~backend
   | Instantiation { runtime_args; main_module_block_repr; arg_descr } ->
     begin
