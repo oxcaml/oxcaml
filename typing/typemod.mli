139cae819f86a7b8010d8ac53d548712e89df7e5
Gavin Gray <gavinleroy@gmail.com>
2026-08-28 18:10:09 -0400

    add Typedtree.interface

--- typing/typemod.mli
+++ typing/typemod.mli
@@ -48,8 +48,7 @@ val type_implementation:
   Parsetree.structure -> Typedtree.implementation
 val type_interface:
   sourcefile:string -> Compilation_unit.t -> Env.t ->
-  Parsetree.signature ->
-  Typedtree.signature * Typedtree.argument_interface option
+  Parsetree.signature -> Typedtree.interface
 
 (* If the [.mli] file has any file-level staticity modality (whether
    [@@ static] or [@@ dynamic]), the module is [Static]; otherwise [Dynamic].
@@ -71,12 +70,10 @@ val modtype_of_package:
 
 val path_of_module : Typedtree.module_expr -> Path.t option
 
-(** [save_signature ~argument_interface unit_info comp_unit typedtree env cmi]
-    writes the typed interface artifacts. [argument_interface] records the
-    additional parameter interface used by [-as-argument-for], when present. *)
+(** [save_signature unit_info comp_unit intf env cmi]
+    writes the typed interface artifacts. *)
 val save_signature:
-  argument_interface:Typedtree.argument_interface option ->
-  Unit_info.t -> Compilation_unit.t -> Typedtree.signature ->
+  Unit_info.t -> Compilation_unit.t -> Typedtree.interface ->
   Env.t -> Cmi_format.cmi_infos_lazy -> unit
 
 val package_units:
