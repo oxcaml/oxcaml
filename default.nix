2460da920dbe5567f826fbb3395fb1b133dda45a
Robin Ricard <rricard@janestreet.com>
2026-09-01 12:58:45 +0100

    ppxlib: build as an external lib
    We also pull in a few dependencies using nix and build these deps in make (and ci)
    
    This build currenlty fails

--- default.nix
+++ default.nix
@@ -265,6 +265,28 @@ let
   # testOcaml argument (it only feeds the merlin package's check phase).
   merlinDev = (mkMerlinPackages ocaml_5_4_0).merlin;
 
+  unpackSourceArchive =
+    name: archive:
+    pkgs.runCommand name { } ''
+      mkdir "$out"
+      tar --extract --file=${archive} --directory="$out" --strip-components=1
+    '';
+
+  ppxDeriversSrc = unpackSourceArchive "ppx-derivers-1.2.1-source" (pkgs.fetchurl {
+    url = "https://github.com/ocaml-ppx/ppx_derivers/archive/1.2.1.tar.gz";
+    hash = "sha256-tlle4Yfep5KzH8VKDhUkqx5IvGBo0wZsRSFaE4zHO5U=";
+  });
+
+  sexplib0Src = unpackSourceArchive "sexplib0-v0.17.0-source" (pkgs.fetchurl {
+    url = "https://github.com/janestreet/sexplib0/archive/refs/tags/v0.17.0.tar.gz";
+    hash = "sha512-rTh+QHif5woRRz236F/gF7gBWSYkQU6QMHMLLpLqCPmAlftukjZDDzPIAWBevuCipihOD2GKJqfaRZnU/Z05XQ==";
+  });
+
+  stdlibShimsSrc = unpackSourceArchive "stdlib-shims-0.3.0-source" (pkgs.fetchurl {
+    url = "https://github.com/ocaml/stdlib-shims/releases/download/0.3.0/stdlib-shims-0.3.0.tbz";
+    hash = "sha256-ur9y05F7hvcHiF8MVSjjbGP8y2mPS0bPK6tcfM3W2Eo=";
+  });
+
   gfortran =
     # we require fortran for some bigarray tests, but adding `pkgs.gfortran`
     # directly to `nativeBuildInputs` overrides many `$PATH` entries from
@@ -358,6 +380,9 @@ stdenv.mkDerivation {
 
   OXCAML_LLDB = if oxcamlLldb then "${lldb}/bin/lldb" else null;
   OXCAML_CLANG = if oxcamlClang then "${clang}/bin/clang" else null;
+  PPXLIB_PPX_DERIVERS_SRC = ppxDeriversSrc;
+  PPXLIB_SEXPLIB0_SRC = sexplib0Src;
+  PPXLIB_STDLIB_SHIMS_SRC = stdlibShimsSrc;
 
   enableParallelBuilding = true;
   separateDebugInfo = false;
@@ -461,6 +486,7 @@ stdenv.mkDerivation {
         make install             - Install
         make test                - Run all tests
         make test-one TEST=...   - Run a single test
+        make external-libs-build - Builds vendored external libraries depending on oxcaml
       ${merlinCommands}EOF
     '';
 
