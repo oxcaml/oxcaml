d5652df4c936fbc2f1e891e08194c9940b98ce2c
Robin Ricard <rricard@janestreet.com>
2026-09-01 15:27:09 +0100

    ppxlib: astlib/stdlib0.ml
    Adapt astlib/stdlib0.ml for the OxCaml AST.

--- external/ppxlib/astlib/stdlib0.ml
+++ external/ppxlib/astlib/stdlib0.ml
@@ -3,5 +3,7 @@ module Int = struct
 end
 
 module Option = struct
+  let is_none o = match o with None -> true | Some _ -> false
+  let is_some o = match o with None -> false | Some _ -> true
   let map f o = match o with None -> None | Some v -> Some (f v)
 end
