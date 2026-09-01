873868252e5bf271119d863c2e9ffaae95fe8768
Gavin Gray <gavinleroy@gmail.com>
2026-09-01 18:22:42 -0400

    remove Typecore.Mode_mismatch

--- typing/typecore.mli
+++ typing/typecore.mli
@@ -379,7 +379,6 @@ type error =
   | Submode_failed of Mode.Value.error * submode_reason
   | Curried_application_complete of
       arg_label * Mode.Alloc.error * [`Prefix|`Single_arg|`Entire_apply]
-  | Mode_mismatch of mode_mismatch_kind * Mode.Alloc.equate_error
   | Uncurried_function_escapes_comonadic of Mode.Alloc.Comonadic.error
   | Uncurried_function_escapes_locality
   | Function_returns_local
