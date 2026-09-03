8b7b359cd8b74eca289ba161f410cc0a52743bba
Aina Linn Georges <ageorges@janestreet.com>
2026-09-03 10:50:04 +0100

    Regression test: modalities on include and on the modality itself

--- testsuite/tests/typing-modes/curry_mode_modality.ml
+++ testsuite/tests/typing-modes/curry_mode_modality.ml
@@ -95,3 +95,100 @@ Line 1, characters 22-36:
                           ^^^^^^^^^^^^^^
 Error: This value is "nonportable" but is expected to be "portable".
 |}]
+
+module type Module_modality = sig
+  module M : No_modality @@ portable
+end
+
+module Module_modality : Module_modality = struct
+  module M = No_modality
+end
+[%%expect{|
+module type Module_modality =
+  sig
+    module M :
+      sig val f : 'a @ portable contended -> (unit -> unit) @@ portable end
+  end
+module Module_modality : Module_modality
+|}]
+
+let () = use_portable Module_modality.M.f
+[%%expect{|
+|}]
+
+let () = use_portable (Module_modality.M.f 42)
+[%%expect{|
+Line 1, characters 22-46:
+1 | let () = use_portable (Module_modality.M.f 42)
+                          ^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This value is "nonportable" but is expected to be "portable".
+|}]
+
+module type Module_modality_prefix = sig
+  module (M @@ portable) : No_modality
+end
+
+module Module_modality_prefix : Module_modality_prefix = struct
+  module M = No_modality
+end
+[%%expect{|
+module type Module_modality_prefix =
+  sig
+    module M :
+      sig val f : 'a @ portable contended -> (unit -> unit) @@ portable end
+  end
+module Module_modality_prefix : Module_modality_prefix
+|}]
+
+let () = use_portable (Module_modality_prefix.M.f 42)
+[%%expect{|
+Line 1, characters 22-53:
+1 | let () = use_portable (Module_modality_prefix.M.f 42)
+                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This value is "nonportable" but is expected to be "portable".
+|}]
+
+module type Default_module = sig @@ portable
+  module M : sig
+    val f : 'a @ portable contended -> unit -> unit
+  end
+end
+
+module Default_module : Default_module = struct
+  module M = No_modality
+end
+[%%expect{|
+module type Default_module =
+  sig
+    module M :
+      sig val f : 'a @ portable contended -> (unit -> unit) @@ portable end
+  end
+module Default_module : Default_module
+|}]
+
+let () = use_portable (Default_module.M.f 42)
+[%%expect{|
+Line 1, characters 22-45:
+1 | let () = use_portable (Default_module.M.f 42)
+                          ^^^^^^^^^^^^^^^^^^^^^^^
+Error: This value is "nonportable" but is expected to be "portable".
+|}]
+
+module type Include_modality = sig
+  include No_modality @@ portable
+end
+
+module Include_modality : Include_modality = No_modality
+[%%expect{|
+module type Include_modality =
+  sig val f : 'a @ portable contended -> (unit -> unit) @@ portable end
+module Include_modality : Include_modality
+|}]
+
+let () = use_portable (Include_modality.f 42)
+[%%expect{|
+Line 1, characters 22-45:
+1 | let () = use_portable (Include_modality.f 42)
+                          ^^^^^^^^^^^^^^^^^^^^^^^
+Error: This value is "nonportable" but is expected to be "portable".
+|}]
