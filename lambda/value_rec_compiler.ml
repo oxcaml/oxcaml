495544e27b6e93897451da7c31ba5a5c3e0867e2
Will Sturgeon <willstrgn@gmail.com>
2026-09-04 18:53:01 -0400

    doc: more detailed comment
    Co-authored-by: Ryan Tjoa <51928404+rtjoa@users.noreply.github.com>

--- lambda/value_rec_compiler.ml
+++ lambda/value_rec_compiler.ml
@@ -345,7 +345,7 @@ let compute_static_size lam =
         | Record_inlined (_, Constructor_uniform_value, Variant_boxed _) ->
             Block (Regular_block size)
         | Record_inlined (_, Constructor_uniform_value, Variant_extensible) ->
-            (* Extensible variants need a byte for constructor identity *)
+            (* Extensible variants use an extra word to identify the constructor *)
             Block (Regular_block (size + 1))
         | Record_float ->
             Block (Float_record size)
