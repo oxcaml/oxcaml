e8a1951e61bf26f816955cbd4d51d0587c717105
Matthew Dempsky <mdempsky@janestreet.com>
2026-09-08 10:16:02 -0400

    Remove XCRs

--- testsuite/tests/typing-jkind-bounds/right_kinds.ml
+++ testsuite/tests/typing-jkind-bounds/right_kinds.ml
@@ -5,8 +5,6 @@
 
 (* Test that "with" syntax isn't allowed in locations that expect a right-kind *)
 
-(* XCR layouts v2.8: these errors should talk about kinds, not modes. Internal
-   ticket 5128 *)
 type ('a : immutable_data with int) t
 [%%expect {|
 Line 1, characters 31-34:
