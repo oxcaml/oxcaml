277fb805050940bafec80772ce2b00153b75096c
Will Sturgeon <willstrgn@gmail.com>
2026-09-08 14:54:22 -0400

    doc: clarify erasing mutability of zero-size blocks

--- middle_end/flambda2/from_lambda/lambda_to_flambda_primitives.ml
+++ middle_end/flambda2/from_lambda/lambda_to_flambda_primitives.ml
@@ -1928,9 +1928,10 @@ let convert_lprim ~(machine_width : Target_system.Machine_width.t) ~big_endian
         ~current_region
     in
     let tag = Tag.Scannable.create_exn tag in
-    (* Mutable zero-size blocks are meaningless after typechecking, and they are
-       fully unsupported by the garbage collector. If `args` is empty, we need
-       to convert mutable zero-size blocks to immutable zero-size blocks. *)
+    (* Mutable zero-size blocks cannot be heap-allocated,
+       so we treat them as immutable.
+       This is fine because their mutability is
+       meaningless after typechecking. *)
     let mutability =
       if List.is_empty args
       then Mutability.Immutable
