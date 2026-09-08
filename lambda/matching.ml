2948a80c8b6223703d31fa50a156d4579d6348f3
Zack Eisbach <zeisbach@janestreet.com>
2026-09-08 11:48:15 -0400

    document tuple matcher list length check

--- lambda/matching.ml
+++ lambda/matching.ml
@@ -488,12 +488,13 @@ let matcher discr (p : Simple.pattern) rem =
   | Array (am1, _, n1), Array (am2, _, n2) -> yesif (am1 = am2 && n1 = n2)
   | Unboxed_unit, Unboxed_unit -> yes ()
   | Unboxed_bool b1, Unboxed_bool b2 -> yesif (Bool.equal b1 b2)
-  | Tuple l1, Tuple l2
-  | Unboxed_tuple l1, Unboxed_tuple l2 ->
-      (* CR zeisbach: sometimes the length of these two lists can be different?
-        see robustmatch issue... come back and diagnose this. *)
+  | Tuple l1, Tuple l2 ->
+      (* List lengths can differ with GADT refinements.
+         see [basic-more/robustmatch.ml] module [M7] for an example *)
       yesif (List.length l1 = List.length l2 &&
-            List.for_all2 (fun (lbl1, _) (lbl2, _) -> lbl1 = lbl2) l1 l2)
+             List.for_all2 (fun (lbl1, _) (lbl2, _) -> lbl1 = lbl2) l1 l2)
+  | Unboxed_tuple l1, Unboxed_tuple l2 ->
+      yesif (List.for_all2 (fun (lbl1, _) (lbl2, _) -> lbl1 = lbl2) l1 l2)
   | Record (l, _), Record (l', _) ->
       (* we already expanded the record fully *)
       yesif (List.length l = List.length l')
