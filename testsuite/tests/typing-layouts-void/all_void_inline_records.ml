c8781066d1c46fa0e7694bcdb8fc39baa5259cc2
Will Sturgeon <willstrgn@gmail.com>
2026-09-08 15:35:03 -0400

    test: exact tag of inline all-void record variant

--- testsuite/tests/typing-layouts-void/all_void_inline_records.ml
+++ testsuite/tests/typing-layouts-void/all_void_inline_records.ml
@@ -6,17 +6,39 @@
  { flags += " -O3"; expect.opt; }
 *)
 
-(* An empty inline payload must not renumber later block constructors. *)
+(* All-void inline records have the right tag and play well with other variants. *)
 type t = A of { x : unit# } | B of int
 let[@inline never] id (x : t) = x
 let selected =
   match id (B 42) with
   | A _ -> "A"
   | B n -> "B " ^ string_of_int n
+let tag_a = Obj.tag (Obj.repr (A { x = #() }))
+let tag_b = Obj.tag (Obj.repr (B 42))
 [%%expect{|
 type t = A of { x : unit#; } | B of int
 val id : t -> t = <fun>
 val selected : string = "B 42"
+val tag_a : int = 0
+val tag_b : int = 1
+|}]
+
+
+(* Same as above, but the all-void inline record has a nonzero tag. *)
+type t = A of int | B of { x : unit# }
+let[@inline never] id (x : t) = x
+let selected =
+  match id (B { x = #() }) with
+  | A n -> "A " ^ string_of_int n
+  | B _ -> "B"
+let tag_a = Obj.tag (Obj.repr (A 42))
+let tag_b = Obj.tag (Obj.repr (B { x = #() }))
+[%%expect{|
+type t = A of int | B of { x : unit#; }
+val id : t -> t = <fun>
+val selected : string = "B"
+val tag_a : int = 0
+val tag_b : int = 1
 |}]
 
 (* Specialization and mutation preserve both block and constant tags. *)
