0812f71a65d413ba97c44086b133ddb89ad4c889
Will Sturgeon <willstrgn@gmail.com>
2026-09-09 17:06:14 -0400

    brevity: `|> List.split`
    Co-authored-by: Ryan Tjoa <51928404+rtjoa@users.noreply.github.com>

--- typing/typedecl.ml
+++ typing/typedecl.ml
@@ -1933,8 +1933,7 @@ let update_constructor_arguments_sorts env loc cd_args =
     (Misc.Stdlib.List.map_option (fun arg -> arg.ca_sort) args)
       |> Option.map Array.of_list
   | Types.Cstr_record lbls ->
-    let lbls_and_jkinds = update_label_sorts env loc lbls ~form:Legacy in
-    let lbls, jkinds = List.split lbls_and_jkinds in
+    let lbls, jkinds = update_label_sorts env loc lbls ~form:Legacy |> List.split in
     Types.Cstr_record lbls, false, jkinds, Some [| Jkind.Sort.Const.scannable |]
 
 let assert_mixed_product_support =
