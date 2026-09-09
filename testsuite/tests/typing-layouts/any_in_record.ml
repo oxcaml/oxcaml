4ce53a57670992194ee815b68c712f0e0c74e02b
Ryan Tjoa <rtjoa@janestreet.com>
2026-09-09 17:08:40 -0400

    Promote tests for functional updates that change the representation
    A functional update whose source representation cannot be determined is
    now rejected, and an inferred source type parameter now gets a sort
    variable (defaulted to value).

--- testsuite/tests/typing-layouts/any_in_record.ml
+++ testsuite/tests/typing-layouts/any_in_record.ml
@@ -601,11 +601,18 @@ val f : int fu -> float# fu = <fun>
 
 let f (r : 'a fu) = { r with a = "hi" }
 [%%expect{|
-val f : ('a : any). 'a fu -> string fu = <fun>
+val f : 'a fu -> string fu = <fun>
 |}]
 
 (* The original record isn't representable *)
 let bad (type a : any) (r : a fu) = { r with a = "hi" }
 [%%expect{|
-val bad : ('a : any). 'a fu -> string fu = <fun>
+Line 1, characters 38-39:
+1 | let bad (type a : any) (r : a fu) = { r with a = "hi" }
+                                          ^
+Error: Record element types must have a representable layout.
+       The layout of a is any
+         because of the annotation on the abstract type declaration for a.
+       But the layout of a must be representable
+         because it's the type of a field involved in a functional update.
 |}]
