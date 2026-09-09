0fd4691790946260918e6ef68f72b5c4059e10cb
Robin Ricard <rricard@janestreet.com>
2026-09-09 07:51:50 +0100

    js_of_ocaml: mark Wasm dynlink stub parameters unused
    Allow the placeholder stubs to compile with -Wunused-parameter -Werror.

--- external/js_of_ocaml/compiler/lib-dynlink-wasm/stubs.c
+++ external/js_of_ocaml/compiler/lib-dynlink-wasm/stubs.c
@@ -2,23 +2,31 @@
 #include <caml/fail.h>
 
 CAMLprim value wasm_toplevel_init_compile(value f) {
+  (void)f;
   caml_failwith("wasm_toplevel_init_compile: only available in wasm_of_ocaml");
 }
 CAMLprim value wasm_get_bytecode_sections(value unit) {
+  (void)unit;
   caml_failwith("wasm_get_bytecode_sections: only available in wasm_of_ocaml");
 }
 CAMLprim value wasm_toplevel_init_reloc(value f) {
+  (void)f;
   caml_failwith("wasm_toplevel_init_reloc: only available in wasm_of_ocaml");
 }
 CAMLprim value caml_wasm_load_module(value bytes) {
+  (void)bytes;
   caml_failwith("caml_wasm_load_module: only available in wasm_of_ocaml");
 }
 CAMLprim value caml_wasm_load_wasmo(value bytes) {
+  (void)bytes;
   caml_failwith("caml_wasm_load_wasmo: only available in wasm_of_ocaml");
 }
 CAMLprim value caml_wasm_register_fragments(value unit_name, value source) {
+  (void)unit_name;
+  (void)source;
   caml_failwith("caml_wasm_register_fragments: only available in wasm_of_ocaml");
 }
 CAMLprim value wasm_get_runtime_aliases(value unit) {
+  (void)unit;
   caml_failwith("wasm_get_runtime_aliases: only available in wasm_of_ocaml");
 }
