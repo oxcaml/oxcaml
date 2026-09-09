94a4155e81db7179145326ab4c37d23cca3563ea
Diana Kalinichenko <dkalinichenko@janestreet.com>
2026-09-09 19:26:43 -0400

    Apply formatting to the kind-bound diagnostic fix

--- typing/jkind.ml
+++ typing/jkind.ml
@@ -3599,8 +3599,7 @@ module Violation = struct
                              |> With_bounds.modalities_of_bounds_mask
                            in
                            let modalities =
-                             !outcometree_of_modalities Types.Immutable
-                               modality
+                             !outcometree_of_modalities Types.Immutable modality
                            in
                            Some
                              (List.map
