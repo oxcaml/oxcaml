d05067043c953a703aea7f85eba845e61cb6b22e
Ryan Tjoa <rtjoa@janestreet.com>
2026-09-15 22:13:34 -0400

    Automated commit: Import compiler changes from 0f07fb0964af439191a357f4fb665b24a71b3855

--- external/merlin/src/ocaml/typing/btype.ml
+++ external/merlin/src/ocaml/typing/btype.ml
@@ -1338,14 +1338,6 @@ module Jkind0 = struct
           name = "any mod everything"
         }
 
-      let scannable =
-        { jkind =
-            mk_jkind (base Scannable Scannable_axes.max)
-              ~crossing:Mode.Crossing.max
-              ~externality:Mod_bounds.Externality.max;
-          name = "scannable"
-        }
-
       let value_or_null =
         { jkind =
             mk_jkind
@@ -1913,7 +1905,6 @@ module Jkind0 = struct
     module Builtin = struct
       let any = max
 
-      let scannable = of_const Const.Builtin.scannable.jkind
 
       let value_or_null = of_const Const.Builtin.value_or_null.jkind
 
@@ -2109,10 +2100,6 @@ module Jkind0 = struct
         fresh_jkind Jkind_desc.Builtin.void ~annotation:(mk_annot "void")
           ~why:(Void_creation why)
         |> mark_best
-
-      let scannable ~why =
-        fresh_jkind Jkind_desc.Builtin.scannable
-          ~annotation:(mk_annot "scannable") ~why:(Scannable_creation why)
 
       let value_or_null ~why =
         match (why : Jkind_intf.History.value_or_null_creation_reason) with
