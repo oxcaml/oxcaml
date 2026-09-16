5c9fa28e6ca5a89e776378863f648cd00808391b
David Allsopp <david.allsopp@metastack.com>
2026-09-16 09:21:37 +0100

    Bump magic numbers

--- external/merlin/src/ocaml/utils/config.ml
+++ external/merlin/src/ocaml/utils/config.ml
@@ -31,15 +31,15 @@
 let version = Sys.ocaml_version
 
 (* When bumping this number, be sure to also update ../typing/magic_numbers.ml *)
-let cmi_magic_number = "Caml1999I586"
+let cmi_magic_number = "Caml1999I587"
 
 let as_debug_prefix_map_flag = ""
 
-let ast_impl_magic_number = "Caml1999M586"
-let ast_intf_magic_number = "Caml1999N586"
-let cmt_magic_number = "Caml1999T586"
-let cms_magic_number = "Caml1999S586"
-let index_magic_number = "Merl2023I586"
+let ast_impl_magic_number = "Caml1999M587"
+let ast_intf_magic_number = "Caml1999N587"
+let cmt_magic_number = "Caml1999T587"
+let cms_magic_number = "Caml1999S587"
+let index_magic_number = "Merl2023I587"
 
 let interface_suffix = ref ".mli"
 
