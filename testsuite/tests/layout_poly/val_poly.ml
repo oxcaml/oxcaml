1f0b0e1a1a26b794a4f997463491170bf09b1fc7
Jakub Bachurski <kbachurski@gmail.com>
2026-09-17 17:12:19 +0100

    Add `moregen` tests for generalisation

--- testsuite/tests/layout_poly/val_poly.ml
+++ testsuite/tests/layout_poly/val_poly.ml
@@ -227,6 +227,198 @@ Error: Signature mismatch:
        which is not supported yet.
 |}]
 
+(* Examples with weak sort variables *)
+
+module M1 : sig
+  val poly_ f : 'a -> 'a
+end = struct
+  let f x = x
+end
+[%%expect{|
+Lines 3-5, characters 6-3:
+3 | ......struct
+4 |   let f x = x
+5 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : 'a -> 'a end
+       is not included in
+         sig val poly_ f : 'a -> 'a end
+       Values do not match:
+         val f : 'a -> 'a
+       is not included in
+         val poly_ f : 'a -> 'a
+       The type "'a -> 'a" is not compatible with the type "'b -> 'b"
+       The kind of 'a is 's2 separable non_null
+         because of the definition of f at line 2, characters 2-24.
+       But the kind of 'a must be representable
+         because of the definition of f at line 4, characters 8-13.
+|}]
+
+module M2 : sig
+  val poly_ f : 'a -> 'b -> 'a
+end = struct
+  let f x y = x
+end
+[%%expect{|
+Lines 3-5, characters 6-3:
+3 | ......struct
+4 |   let f x y = x
+5 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : 'a -> 'b -> 'a end
+       is not included in
+         sig val poly_ f : 'a -> 'b -> 'a end
+       Values do not match:
+         val f : 'a -> 'b -> 'a
+       is not included in
+         val poly_ f : 'a -> 'b -> 'a
+       The type "'a -> 'b -> 'a" is not compatible with the type "'c -> 'd -> 'c"
+       The kind of 'a is 's5 separable non_null
+         because of the definition of f at line 2, characters 2-30.
+       But the kind of 'a must be representable
+         because of the definition of f at line 4, characters 8-15.
+|}]
+
+module M2' : sig
+  val f : layout_ x. ('a : x). 'a -> 'b -> 'a
+end = struct
+  let f x y = x
+end
+[%%expect{|
+Lines 3-5, characters 6-3:
+3 | ......struct
+4 |   let f x y = x
+5 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : 'a -> 'b -> 'a end
+       is not included in
+         sig val poly_ f : 'b. 'a -> 'b -> 'a end
+       Values do not match:
+         val f : 'a -> 'b -> 'a
+       is not included in
+         val poly_ f : 'b. 'a -> 'b -> 'a
+       The type "'a -> 'b -> 'a" is not compatible with the type "'c -> 'd -> 'c"
+       The kind of 'a is 's6
+         because of the definition of f at line 2, characters 2-45.
+       But the kind of 'a must be representable
+         because of the definition of f at line 4, characters 8-15.
+|}]
+
+module M2'' : sig
+  val f : layout_ x. ('b : x). 'a -> 'b -> 'a
+end = struct
+  let f x y = x
+end
+[%%expect{|
+Lines 3-5, characters 6-3:
+3 | ......struct
+4 |   let f x y = x
+5 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig val f : 'a -> 'b -> 'a end
+       is not included in
+         sig val poly_ f : 'a. 'a -> 'b -> 'a end
+       Values do not match:
+         val f : 'a -> 'b -> 'a
+       is not included in
+         val poly_ f : 'a. 'a -> 'b -> 'a
+       The type "'a -> 'b -> 'a" is not compatible with the type "'a -> 'c -> 'a"
+       The kind of 'a is 's7
+         because of the definition of f at line 2, characters 2-45.
+       But the kind of 'a must be representable
+         because of the definition of f at line 4, characters 8-15.
+|}]
+
+(* CR-soon jbachurski: This is fine, though we should check ['_weak1]'s layout
+   does not end up generalised with the level fixes. *)
+module Weak_type : sig
+  val f : 'a list -> 'a list
+end = struct
+  let cell = ref []
+  let poly_ f _ = !cell
+end
+[%%expect{|
+Lines 3-6, characters 6-3:
+3 | ......struct
+4 |   let cell = ref []
+5 |   let poly_ f _ = !cell
+6 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig val cell : '_weak1 list ref val poly_ f : 'a -> '_weak1 list end
+       is not included in
+         sig val f : 'a list -> 'a list end
+       Values do not match:
+         val poly_ f : 'a -> '_weak1 list
+       is not included in
+         val f : 'a list -> 'a list
+       The type "'a list -> '_weak1 list" is not compatible with the type
+         "'a list -> 'a list"
+       Type "'_weak1" is not compatible with type "'a"
+|}]
+
+(* CR-soon jbachurski: [x]'s layout is unsoundly generalised. *)
+module Weak_prim : sig
+  val f : layout_ x. ('a : x). 'a -> 'a -> 'a
+end = struct
+  external[@layout_poly] id : ('a : any). 'a -> 'a = "%opaque"
+  let poly_ f x y = id x
+end
+[%%expect{|
+Lines 3-6, characters 6-3:
+3 | ......struct
+4 |   external[@layout_poly] id : ('a : any). 'a -> 'a = "%opaque"
+5 |   let poly_ f x y = id x
+6 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig
+           external id : ('a : any). 'a -> 'a = "%opaque" [@@layout_poly]
+           val poly_ f : 'a -> 'b -> 'a
+         end
+       is not included in
+         sig val poly_ f : 'a -> 'a -> 'a end
+       Values do not match:
+         val poly_ f : 'a -> 'b -> 'a
+       is not included in
+         val poly_ f : 'a -> 'a -> 'a
+       the first has 1 more layout parameter that is not used,
+       which is not supported yet.
+|}]
+
+(* CR-soon jbachurski: [x]'s layout is unsoundly generalised. *)
+module Weak_sort : sig
+  val f : layout_ x. ('a : x). 'a -> 'a -> 'a
+end = struct
+  let g x = x
+  let poly_ f x y = g x
+end
+[%%expect{|
+Lines 3-6, characters 6-3:
+3 | ......struct
+4 |   let g x = x
+5 |   let poly_ f x y = g x
+6 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig
+           val g : ('a : <genvar>). 'a -> 'a
+           val poly_ f : 'a -> 'b -> 'a
+         end
+       is not included in
+         sig val poly_ f : 'a -> 'a -> 'a end
+       Values do not match:
+         val poly_ f : 'a -> 'b -> 'a
+       is not included in
+         val poly_ f : 'a -> 'a -> 'a
+       the first has 1 more layout parameter that is not used,
+       which is not supported yet.
+|}]
+
 (* Ordering: both use first var on both sides - same position, should succeed *)
 (* CR-soon zqian: same issue; should pass with coercion. *)
 module FO1 (M : sig
