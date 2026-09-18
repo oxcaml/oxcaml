b482d446dabab48e11250ce98cbdb3404598f095
Zack Eisbach <zeisbach@janestreet.com>
2026-09-18 13:53:16 -0400

    overkill tests

--- testsuite/tests/typing-layouts-box/unbox_primitive.ml
+++ testsuite/tests/typing-layouts-box/unbox_primitive.ml
@@ -144,3 +144,321 @@ let () =
   assert (int8_equal (unbox n) #42s)
 [%%expect{|
 |}]
+
+(* Unboxed products. The box is laid out like a record with one field per
+   component, with values moved to a prefix and flat data to a suffix, so
+   unboxing must undo that permutation. *)
+
+(* Unboxed tuples of values only *)
+
+let () =
+  let #(a, b) = unbox (box #(42, s)) in
+  assert (a = 42 && b == s);
+  let o = Some s in
+  let #(a, b, c) = unbox (box #(s, o, 7)) in
+  assert (a == s && b == o && c = 7);
+  (* not statically known *)
+  let #(a, b) = unbox (Sys.opaque_identity (box #(42, s))) in
+  assert (a = 42 && b == s);
+  let #(a, b, c) = unbox (Sys.opaque_identity (box #(s, o, 7))) in
+  assert (a == s && b == o && c = 7)
+[%%expect{|
+|}]
+
+(* CR zeisbach: add tests for mixed tuples after rebasing onto them *)
+(*
+(* Unboxed tuples mixing values and flat data, in various orders *)
+
+let () =
+  let #(a, b) = unbox (box #(#42L, s)) in
+  assert (Int64.equal (box_int64 a) 42L && b == s);
+  let #(a, b) = unbox (box #(s, #42L)) in
+  assert (a == s && Int64.equal (box_int64 b) 42L);
+  let #(a, b, c, d, e) = unbox (box #(#1L, #2.5, s, 3, #4L)) in
+  assert (Int64.equal (box_int64 a) 1L);
+  assert (Float.equal (box_float b) 2.5);
+  assert (c == s && d = 3);
+  assert (Int64.equal (box_int64 e) 4L);
+  (* not statically known *)
+  let #(a, b) = unbox (Sys.opaque_identity (box #(#42L, s))) in
+  assert (Int64.equal (box_int64 a) 42L && b == s);
+  let #(a, b) = unbox (Sys.opaque_identity (box #(s, #42L))) in
+  assert (a == s && Int64.equal (box_int64 b) 42L);
+  let #(a, b, c, d, e) =
+    unbox (Sys.opaque_identity (box #(#1L, #2.5, s, 3, #4L)))
+  in
+  assert (Int64.equal (box_int64 a) 1L);
+  assert (Float.equal (box_float b) 2.5);
+  assert (c == s && d = 3);
+  assert (Int64.equal (box_int64 e) 4L)
+[%%expect{|
+|}]
+*)
+
+(*
+(* Nested unboxed tuples: the nesting is flattened away in the box and must be
+   rebuilt by unboxing. *)
+
+let () =
+  let #(#(a, b), c) = unbox (box #(#(#42L, s), #2.5)) in
+  assert (Int64.equal (box_int64 a) 42L && b == s);
+  assert (Float.equal (box_float c) 2.5);
+  let #(a, #(b, #(c, d)), e) = unbox (box #(s, #(#1L, #(2, #3.25s)), #4l)) in
+  assert (a == s && Int64.equal (box_int64 b) 1L && c = 2);
+  assert (box_float32 d = box_float32 #3.25s);
+  assert (Int32.equal (box_int32 e) 4l);
+  let #(#(a, b), #(c, d)) = unbox (box #(#(1, #2L), #(#3L, 4))) in
+  assert (a = 1 && Int64.equal (box_int64 b) 2L);
+  assert (Int64.equal (box_int64 c) 3L && d = 4);
+  (* not statically known *)
+  let #(#(a, b), c) = unbox (Sys.opaque_identity (box #(#(#42L, s), #2.5))) in
+  assert (Int64.equal (box_int64 a) 42L && b == s);
+  assert (Float.equal (box_float c) 2.5);
+  let #(a, #(b, #(c, d)), e) =
+    unbox (Sys.opaque_identity (box #(s, #(#1L, #(2, #3.25s)), #4l)))
+  in
+  assert (a == s && Int64.equal (box_int64 b) 1L && c = 2);
+  assert (box_float32 d = box_float32 #3.25s);
+  assert (Int32.equal (box_int32 e) 4l);
+  let #(#(a, b), #(c, d)) =
+    unbox (Sys.opaque_identity (box #(#(1, #2L), #(#3L, 4))))
+  in
+  assert (a = 1 && Int64.equal (box_int64 b) 2L);
+  assert (Int64.equal (box_int64 c) 3L && d = 4)
+[%%expect{|
+|}]
+*)
+
+(* Unboxed records. [t# box = t], so we can also unbox a directly constructed
+   record. *)
+
+type p_flat_first = { c : int64_u; d : string }
+type p_value_first = { e : string; f : int64_u }
+type p_many = { g : int64_u; h : float#; k : string; l : int; m : int64_u }
+type p_nested = { n1 : #(int64_u * string); n2 : float# }
+type ur = { u1 : int64_u; u2 : string; u3 : int }
+
+let () =
+  let #{ c; d } = unbox (box #{ c = #42L; d = s }) in
+  assert (Int64.equal (box_int64 c) 42L && d == s);
+  let #{ e; f } = unbox (box #{ e = s; f = #42L }) in
+  assert (e == s && Int64.equal (box_int64 f) 42L);
+  let #{ g; h; k; l; m } = unbox (box #{ g = #1L; h = #2.5; k = s; l = 3; m = #4L }) in
+  assert (Int64.equal (box_int64 g) 1L);
+  assert (Float.equal (box_float h) 2.5);
+  assert (k == s && l = 3);
+  assert (Int64.equal (box_int64 m) 4L);
+  let #{ n1 = #(n1a, n1b); n2 } = unbox (box #{ n1 = #(#42L, s); n2 = #2.5 }) in
+  assert (Int64.equal (box_int64 n1a) 42L && n1b == s);
+  assert (Float.equal (box_float n2) 2.5);
+  let #{ u1; u2; u3 } = unbox (box #{ u1 = #42L; u2 = s; u3 = 7 }) in
+  assert (Int64.equal (box_int64 u1) 42L && u2 == s && u3 = 7);
+  (* not statically known *)
+  let #{ c; d } = unbox (Sys.opaque_identity (box #{ c = #42L; d = s })) in
+  assert (Int64.equal (box_int64 c) 42L && d == s);
+  let #{ e; f } = unbox (Sys.opaque_identity (box #{ e = s; f = #42L })) in
+  assert (e == s && Int64.equal (box_int64 f) 42L);
+  let #{ g; h; k; l; m } =
+    unbox (Sys.opaque_identity (box #{ g = #1L; h = #2.5; k = s; l = 3; m = #4L }))
+  in
+  assert (Int64.equal (box_int64 g) 1L);
+  assert (Float.equal (box_float h) 2.5);
+  assert (k == s && l = 3);
+  assert (Int64.equal (box_int64 m) 4L);
+  let #{ n1 = #(n1a, n1b); n2 } =
+    unbox (Sys.opaque_identity (box #{ n1 = #(#42L, s); n2 = #2.5 }))
+  in
+  assert (Int64.equal (box_int64 n1a) 42L && n1b == s);
+  assert (Float.equal (box_float n2) 2.5)
+[%%expect{|
+type p_flat_first = { c : int64_u; d : string; }
+type p_value_first = { e : string; f : int64_u; }
+type p_many = { g : int64_u; h : float#; k : string; l : int; m : int64_u; }
+type p_nested = { n1 : #(int64_u * string); n2 : float#; }
+type ur = { u1 : int64_u; u2 : string; u3 : int; }
+|}]
+
+(* Unboxing a directly constructed record *)
+
+let () =
+  let r : p_many = { g = #7L; h = #0.5; k = s; l = 9; m = #8L } in
+  let #{ g; h; k; l; m } = unbox r in
+  assert (Int64.equal (box_int64 g) 7L);
+  assert (Float.equal (box_float h) 0.5);
+  assert (k == s && l = 9);
+  assert (Int64.equal (box_int64 m) 8L);
+  let r : p_nested = { n1 = #(#6L, s); n2 = #1.25 } in
+  let #{ n1 = #(n1a, n1b); n2 } = unbox r in
+  assert (Int64.equal (box_int64 n1a) 6L && n1b == s);
+  assert (Float.equal (box_float n2) 1.25);
+  (* not statically known *)
+  let r : p_many = Sys.opaque_identity { g = #7L; h = #0.5; k = s; l = 9; m = #8L } in
+  let #{ g; h; k; l; m } = unbox r in
+  assert (Int64.equal (box_int64 g) 7L);
+  assert (Float.equal (box_float h) 0.5);
+  assert (k == s && l = 9);
+  assert (Int64.equal (box_int64 m) 8L);
+  let r : ur = Sys.opaque_identity { u1 = #5L; u2 = s; u3 = 6 } in
+  let #{ u1; u2; u3 } = unbox r in
+  assert (Int64.equal (box_int64 u1) 5L && u2 == s && u3 = 6)
+[%%expect{|
+|}]
+
+(* Unboxed records nested inside unboxed records and tuples *)
+
+type inner_u = #{ ix : int64_u; iy : string }
+type outer_u = #{ o1 : inner_u; o2 : float#; o3 : int }
+type wrap_u = #{ w1 : int; w2 : #(inner_u * int32_u); w3 : string }
+
+let () =
+  let #{ o1 = #{ ix; iy }; o2; o3 } =
+    unbox (box #{ o1 = #{ ix = #1L; iy = s }; o2 = #2.5; o3 = 3 })
+  in
+  assert (Int64.equal (box_int64 ix) 1L && iy == s);
+  assert (Float.equal (box_float o2) 2.5 && o3 = 3);
+  let #{ w1; w2 = #(#{ ix; iy }, w2b); w3 } =
+    unbox (box #{ w1 = 1; w2 = #(#{ ix = #2L; iy = s }, #3l); w3 = s })
+  in
+  assert (w1 = 1 && Int64.equal (box_int64 ix) 2L && iy == s);
+  assert (Int32.equal (box_int32 w2b) 3l && w3 == s);
+  (* CR zeisbach: mixed tuple, enable after rebasing onto mixed tuples
+  let #(#{ ix; iy }, b) = unbox (box #(#{ ix = #4L; iy = s }, #5.5)) in
+  assert (Int64.equal (box_int64 ix) 4L && iy == s);
+  assert (Float.equal (box_float b) 5.5);
+  *)
+  (* not statically known *)
+  let #{ o1 = #{ ix; iy }; o2; o3 } =
+    unbox
+      (Sys.opaque_identity
+         (box #{ o1 = #{ ix = #1L; iy = s }; o2 = #2.5; o3 = 3 }))
+  in
+  assert (Int64.equal (box_int64 ix) 1L && iy == s);
+  assert (Float.equal (box_float o2) 2.5 && o3 = 3);
+  let #{ w1; w2 = #(#{ ix; iy }, w2b); w3 } =
+    unbox
+      (Sys.opaque_identity
+         (box #{ w1 = 1; w2 = #(#{ ix = #2L; iy = s }, #3l); w3 = s }))
+  in
+  assert (w1 = 1 && Int64.equal (box_int64 ix) 2L && iy == s);
+  assert (Int32.equal (box_int32 w2b) 3l && w3 == s)
+  (* CR zeisbach: mixed tuple, enable after rebasing onto mixed tuples
+  ;
+  let #(#{ ix; iy }, b) =
+    unbox (Sys.opaque_identity (box #(#{ ix = #4L; iy = s }, #5.5)))
+  in
+  assert (Int64.equal (box_int64 ix) 4L && iy == s);
+  assert (Float.equal (box_float b) 5.5)
+  *)
+[%%expect{|
+type inner_u = #{ ix : int64_u; iy : string; }
+type outer_u = #{ o1 : inner_u; o2 : float#; o3 : int; }
+type wrap_u = #{ w1 : int; w2 : #(inner_u * int32_u); w3 : string; }
+|}]
+
+(* Records with narrow data and deep nesting *)
+
+type narrow =
+  { na : int8#; nb : string; nc : int16#; nd : float32_u; ne : int32_u;
+    nf : int#; ng : nativeint_u }
+type deep = { d1 : string; d2 : #(int64_u * #(int * float32_u)); d3 : int32_u }
+
+let () =
+  let #{ na; nb; nc; nd; ne; nf; ng } =
+    unbox (box #{ na = #42s; nb = s; nc = #42S; nd = #3.25s; ne = #42l;
+                  nf = untag_int 42; ng = #42n })
+  in
+  assert (int8_equal na #42s && nb == s && int16_equal nc #42S);
+  assert (box_float32 nd = box_float32 #3.25s);
+  assert (Int32.equal (box_int32 ne) 42l);
+  assert (int_equal nf (untag_int 42));
+  assert (Nativeint.equal (box_nativeint ng) 42n);
+  let #{ d1; d2 = #(d2a, #(d2b, d2c)); d3 } =
+    unbox (box #{ d1 = s; d2 = #(#1L, #(2, #3.25s)); d3 = #4l })
+  in
+  assert (d1 == s && Int64.equal (box_int64 d2a) 1L && d2b = 2);
+  assert (box_float32 d2c = box_float32 #3.25s);
+  assert (Int32.equal (box_int32 d3) 4l);
+  (* not statically known *)
+  let #{ na; nb; nc; nd; ne; nf; ng } =
+    unbox
+      (Sys.opaque_identity
+         (box #{ na = #42s; nb = s; nc = #42S; nd = #3.25s; ne = #42l;
+                 nf = untag_int 42; ng = #42n }))
+  in
+  assert (int8_equal na #42s && nb == s && int16_equal nc #42S);
+  assert (box_float32 nd = box_float32 #3.25s);
+  assert (Int32.equal (box_int32 ne) 42l);
+  assert (int_equal nf (untag_int 42));
+  assert (Nativeint.equal (box_nativeint ng) 42n);
+  let #{ d1; d2 = #(d2a, #(d2b, d2c)); d3 } =
+    unbox
+      (Sys.opaque_identity (box #{ d1 = s; d2 = #(#1L, #(2, #3.25s)); d3 = #4l }))
+  in
+  assert (d1 == s && Int64.equal (box_int64 d2a) 1L && d2b = 2);
+  assert (box_float32 d2c = box_float32 #3.25s);
+  assert (Int32.equal (box_int32 d3) 4l);
+[%%expect{|
+type narrow = {
+  na : int8#;
+  nb : string;
+  nc : int16#;
+  nd : float32_u;
+  ne : int32_u;
+  nf : int#;
+  ng : nativeint_u;
+}
+type deep = {
+  d1 : string;
+  d2 : #(int64_u * #(int * float32_u));
+  d3 : int32_u;
+}
+|}]
+
+(* Void components contribute no fields to the box and no data to the unboxed
+   result. *)
+
+type all_void = { x : unit#; kept : unit# }
+type void_mixed = { v1 : #(unit# * int64_u); v2 : string; v3 : unit# }
+
+let () =
+  let #{ x = _; kept = _ } = unbox (box #{ x = #(); kept = #() }) in
+  (* not statically known *)
+  let #{ v1 = #(_, a); v2 = b; v3 = _ } =
+    unbox (Sys.opaque_identity (box #{ v1 = #(#(), #1L); v2 = s; v3 = #() }))
+  in
+  assert (Int64.equal (box_int64 a) 1L && b == s);
+  let #{ x = _; kept = _ } =
+    unbox (Sys.opaque_identity (box #{ x = #(); kept = #() }))
+  in
+  let r : all_void = Sys.opaque_identity { x = #(); kept = #() } in
+  let #{ x = _; kept = _ } = unbox r in
+  ()
+[%%expect{|
+type all_void = { x : unit#; kept : unit#; }
+type void_mixed = { v1 : #(unit# * int64_u); v2 : string; v3 : unit#; }
+|}]
+
+(* Local allocation. Unboxing a local box yields local components. *)
+
+let () =
+  let local_ b = box #(42, s) in
+  let #(a, c) = unbox b in
+  assert (a = 42 && c == s);
+  let local_ r = box #{ g = #1L; h = #2.5; k = s; l = 3; m = #4L } in
+  let #{ g; h; k; l; m } = unbox r in
+  assert (Int64.equal (box_int64 g) 1L);
+  assert (Float.equal (box_float h) 2.5);
+  assert (k == s && l = 3);
+  assert (Int64.equal (box_int64 m) 4L)
+  (* CR zeisbach: mixed tuples, enable after rebasing onto mixed tuples
+  ;
+  let local_ b = box #(#1L, s) in
+  let #(a, c) = unbox b in
+  assert (Int64.equal (box_int64 a) 1L && c == s);
+  let local_ n = box #(s, #(#2L, #(3, #4.5s))) in
+  let #(a, #(c, #(d, e))) = unbox n in
+  assert (a == s && Int64.equal (box_int64 c) 2L && d = 3);
+  assert (box_float32 e = box_float32 #4.5s)
+  *)
+[%%expect{|
+|}]
