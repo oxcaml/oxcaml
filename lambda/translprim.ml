05a51fdb60b48c3cd42eba752019c47f054a2549
Ryan Tjoa <rtjoa@janestreet.com>
2026-09-22 12:37:04 -0400

    Expand boxed index bases before specializing array access

--- lambda/translprim.ml
+++ lambda/translprim.ml
@@ -2053,8 +2053,8 @@ let specialize_primitive env loc ty ~has_constant_constructor prim =
         array_ty, elt_ty
       | _ -> err ()
     in
-    let array_ty = Btype.new_box_ty array_ty in
-    (match Types.get_desc (Ctype.expand_head env array_ty) with
+    let array_ty = Ctype.expand_head env (Btype.new_box_ty array_ty) in
+    (match Types.get_desc array_ty with
      | Tconstr (p, _, _)
        when Path.same p Predef.path_array
          || Path.same p Predef.path_iarray -> ()
