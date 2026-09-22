ac067be7264480fb9f9077e2a132efcc1d5ed358
Zack Eisbach <zeisbach@janestreet.com>
2026-09-22 17:45:59 +0100

    fix failing test

--- testsuite/tests/typing-modes/crossing_principal.ml
+++ testsuite/tests/typing-modes/crossing_principal.ml
@@ -13,20 +13,4 @@ end = struct
 end
 [%%expect{|
 module M : sig val f : ('a : value mod portable). 'a -> 'a @ portable end
-|}, Principal{|
-Lines 3-5, characters 6-3:
-3 | ......struct
-4 |   let f x = (x : _ @ nonportable)
-5 | end
-Error: Signature mismatch:
-       Modules do not match:
-         sig val f : 'a -> 'a end
-       is not included in
-         sig val f : ('a : value mod portable). 'a -> 'a @ portable end
-       Values do not match:
-         val f : 'a -> 'a
-       is not included in
-         val f : ('a : value mod portable). 'a -> 'a @ portable
-       The type "'a -> 'a" is not compatible with the type "'a -> 'a @ portable"
-       The return mode was expected to be "portable" but is "nonportable"
 |}]
