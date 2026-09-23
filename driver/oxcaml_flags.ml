1aaf81a845ac8c31c96614339e4e15129ba830cf
Mark Shinwell <mshinwell@pm.me>
2026-09-23 14:15:40 +0100

    small functor size -> 1250

--- driver/oxcaml_flags.ml
+++ driver/oxcaml_flags.ml
@@ -556,7 +556,7 @@ module Flambda2 = struct
       poly_compare_cost = 3.0 *. Default.default_arguments.poly_compare_cost;
       small_function_size = 10 * Default.default_arguments.small_function_size;
       large_function_size = 50 * Default.default_arguments.large_function_size;
-      small_functor_size = 10 * Default.default_arguments.small_functor_size;
+      small_functor_size = 1250; (* 10 * Default.default_arguments.small_functor_size; *)
       (* This allows functors 50% larger than those in [Stdlib.Map] and
          [Stdlib.Set] to be eligible for speculative inlining. *)
       large_functor_size =
