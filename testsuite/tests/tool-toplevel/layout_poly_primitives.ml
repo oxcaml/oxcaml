42d9e46fe1c0c89a95231891b8acf9c635624cfa
Ryan Tjoa <rtjoa@janestreet.com>
2026-09-22 22:20:20 -0400

    Cover kind annotations on unnamed toplevel functions

--- testsuite/tests/tool-toplevel/layout_poly_primitives.ml
+++ testsuite/tests/tool-toplevel/layout_poly_primitives.ml
@@ -25,3 +25,11 @@ unbox (box #("product", #4.5));;
 external opaque : ('a : any). 'a -> 'a = "%opaque" [@@layout_poly];;
 opaque;;
 opaque #("opaque", #5L);;
+
+let (_ : (_ : float64) -> _) = box;;
+let f (x : (_ : float64)) = x;;
+(box : (_ : float64) -> _);;
+let (_ : (_ : float64) box -> _) = unbox;;
+let (_ : (_ : value & float64) -> _) = box;;
+let (_ : (_ : float64) -> _ -> _) = fun _ x -> x;;
+let _ = fun x -> x;;
