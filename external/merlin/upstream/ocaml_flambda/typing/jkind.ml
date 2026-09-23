0358d989c9c2a05f74cd47d45b6177968a396340
Ryan Tjoa <rtjoa@janestreet.com>
2026-09-23 00:27:15 -0400

    Automated commit: Import compiler changes from 2725077433dd73671163defd7546b29f0ce3d924

--- external/merlin/upstream/ocaml_flambda/typing/jkind.ml
+++ external/merlin/upstream/ocaml_flambda/typing/jkind.ml
@@ -3724,6 +3724,8 @@ module Format_history = struct
       fprintf ppf
         "it's the element type of an array that is iterated over in a \
          comprehension"
+    | List_comprehension_element ->
+      fprintf ppf "it's the element type of list comprehension"
     | Optional_argument -> fprintf ppf "it's the type of an optional argument"
 
   let format_value_creation_reason ppf ~layout_or_kind :
@@ -4903,6 +4905,7 @@ module Debug_printers = struct
     | Array_comprehension_element -> fprintf ppf "Array_comprehension_element"
     | Array_comprehension_iterator_element ->
       fprintf ppf "Array_comprehension_iterator_element"
+    | List_comprehension_element -> fprintf ppf "List_comprehension_element"
     | Optional_argument -> fprintf ppf "Optional_argument"
 
   let value_creation_reason ppf : History.value_creation_reason -> _ = function
--- external/merlin/upstream/ocaml_flambda/typing/primitive.ml
+++ external/merlin/upstream/ocaml_flambda/typing/primitive.ml
@@ -52,6 +52,10 @@ type mode =
   | Prim_global
   | Prim_poly
 
+(* CR-someday lmaurer: We should consider moving [prim_alloc], [prim_effects],
+   and [prim_coeffects] into a variant to replace the [bool] in
+   [prim_c_builtin]. They're ignored for compiler primitives (for which we
+   already know the correct values - see [Lambda.primitive_may_allocate]). *)
 type 'repr description_gen =
   { prim_name: string;         (* Name of primitive  or C function *)
     prim_arity: int;           (* Number of arguments *)
@@ -1170,6 +1174,19 @@ let prim_has_valid_reprs ~loc prim =
       check [
         is (Same_as_ocaml_repr C.scannable);
         any;
+      ]
+    | "%makeblock" ->
+      check [any; (is (Same_as_ocaml_repr C.scannable))]
+    | "%makemutable" ->
+      check [any; (is (Same_as_ocaml_repr C.scannable))]
+    | "%field0_of_1"
+    | "%field0_of_1_immut" ->
+      check [is (Same_as_ocaml_repr C.scannable); any]
+    | "%setfield0_of_1" ->
+      check [
+        is (Same_as_ocaml_repr C.scannable);
+        any;
+        is (Same_as_ocaml_repr C.scannable)
       ]
 
     | name -> (
