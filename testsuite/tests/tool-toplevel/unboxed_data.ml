4c9cb221d33ac7f5cf417a8759fae15d13203ead
Ryan Tjoa <rtjoa@janestreet.com>
2026-09-22 21:08:29 -0400

    Cover native toplevel printing of nested unboxed products

--- testsuite/tests/tool-toplevel/unboxed_data.ml
+++ testsuite/tests/tool-toplevel/unboxed_data.ml
@@ -45,3 +45,37 @@ let one_slot_record = { payload = #(#(), 42) };;
 
 type inherited_one_slot = { inherit payload : #(unit# * int) };;
 let inherited_one_slot = { payload = #(#(), 42) };;
+
+type scalar_record = #{ scalar : float# };;
+let scalar_record = #{ scalar = #3.5 };;
+let labeled = #(~left:#1.25, ~name:"tuple", ~empty:#(), ~right:#2L);;
+let numbers = #(-#12s, -#300S, -#4l, #5n, #6.5s, -#7.5, #8L);;
+
+type nested = #{ a : t; b : #(unit# * string * float#); c : unit# };;
+let nested =
+  #{ a = #{ x = 7; y = #8L }; b = #(#(), "nested", #9.5); c = #() };;
+
+type ('a : any) poly = #{ data : 'a; empty : unit# };;
+let poly = #{ data = #("poly", #10.5); empty = #() };;
+
+let boxed_tuple = (#(#1.5, "tuple"), #2L, nested);;
+type variant = Product of #(float# * string) | Record of { data : nested };;
+let variant = Product #(#2.5, "variant");;
+let inline = Record { data = nested };;
+
+module M = struct
+  let product = #(#3.5, "module", #4L)
+end;;
+M.product;;
+
+type cycle = { mutable next : #(int * cycle) option };;
+let cycle = { next = None };;
+cycle.next <- Some #(42, cycle);;
+cycle;;
+
+#print_depth 2;;
+nested;;
+#print_depth 100;;
+#print_length 2;;
+poly;;
+#print_length 100;;
